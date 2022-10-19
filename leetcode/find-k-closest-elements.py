a646b70aee05005121c5c9f9ca569abc8cd7faec
joric <joric@users.noreply.github.com>
2022-10-20 01:56:05 +0500

    updated files

--- leetcode/find-k-closest-elements.py
+++ leetcode/find-k-closest-elements.py
@@ -18,18 +18,25 @@ class Solution1:
 
             return arr[low:high + 1]
 
-class Solution:
+class Solution2:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         return sorted(sorted(arr, key = lambda e: abs(x-e))[:k])
 
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        return sorted(nsmallest(k, arr, key=lambda n:(abs(n-x),n)))
+
 test(Solution,'''
+658. Find K Closest Elements
+
+Medium
+
 Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
 
 An integer a is closer to x than an integer b if:
 
 |a - x| < |b - x|, or
 |a - x| == |b - x| and a < b
- 
 
 Example 1:
 
@@ -39,12 +46,11 @@ Example 2:
 
 Input: arr = [1,2,3,4,5], k = 4, x = -1
 Output: [1,2,3,4]
- 
 
 Constraints:
 
 1 <= k <= arr.length
-1 <= arr.length <= 104
+1 <= arr.length <= 10**4
 arr is sorted in ascending order.
--104 <= arr[i], x <= 104
+-104 <= arr[i], x <= 10**4
 ''')

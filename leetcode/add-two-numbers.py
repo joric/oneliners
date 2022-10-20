bf2ad534cf310e695bba78f84de5fcb2524ad40c
joric <joric@users.noreply.github.com>
2022-10-20 17:09:49 +0500

    updated files

--- leetcode/add-two-numbers.py
+++ leetcode/add-two-numbers.py
@@ -15,14 +15,24 @@ class Solution1:
 
 class Solution(object):
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        return (i:=lambda x: x.val + i(x.next)*10 if x else 0) and (l:=
-            lambda n: ListNode(n%10, l(n//10) if n>9 else None))(i(l1) + i(l2))
+        return (i:=lambda x: x.val + i(x.next)*10 if x else 0) and (l:=lambda n: ListNode(n%10, l(n//10) if n>9 else None))(i(l1) + i(l2))
 
 
 test(Solution, '''
+2. Add Two Numbers
+
+Medium
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
 Input: l1 = [2,4,3], l2 = [5,6,4]
 Output: [7,0,8]
 Explanation: 342 + 465 = 807.
+
 Example 2:
 
 Input: l1 = [0], l2 = [0]
@@ -31,4 +41,11 @@ Example 3:
 
 Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 Output: [8,9,9,9,0,0,0,1]
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+
 ''')

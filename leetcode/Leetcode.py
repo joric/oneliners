39cb6b9dac14958308f020986d6725974d35620d
joric <joric@users.noreply.github.com>
2022-10-24 23:51:01 +0500

    updated files

--- leetcode/Leetcode.py
+++ leetcode/Leetcode.py
@@ -9,6 +9,7 @@ from heapq import *
 from bisect import *
 from random import *
 from re import *
+import collections
 import bisect
 
 class TreeNode(object):
@@ -61,33 +62,53 @@ class ListNode:
         return str(a)==str(b)
 
     def parse(val):
-        if len(val) == 0:
+        if len(val)==0:
             return None
         sub_nodes = ListNode.parse(val[1:])
         list_node = ListNode(val[0], sub_nodes)
         return list_node
 
-def test(Solution, s, init=None, check=None):
+def test(classname, text, init=None, check=None):
     import re
 
+    def vp(s):
+        return eval(s.replace('null','None').replace('true','True').replace('false','False'))
+
+    def vc(func, vname, val):
+        is_seq = lambda v: type(v) in (dict, tuple, set, Generator)
+        to_list = lambda v: v if not is_seq(v) else [to_list(x) for x in v]
+        tname = str(get_type_hints(func).get(vname, None))
+        if 'ListNode' in tname and type(val)!=ListNode:
+            val = ListNode.parse(val)
+        elif 'TreeNode' in tname and type(val)!=TreeNode:
+            val = TreeNode.parse(val)
+        elif 'List' in tname or is_seq(val):
+            val = to_list(val)
+        return val
+
+    def print_res(passed, res, expected, *args):
+        c = lambda c,t,w=60: '\x1b[{1}m{2}\x1b[0m'.format(s:=str(t), 30+c, s[:w]+'...' if len(s)>=w else s)
+        e = 2 if passed else 1
+        print('%s args %s result %s expected %s' % (c(e,'PASSED' if passed else 'FAILED'), c(e,args), c(e,res), c(e,expected)))
+
+    if not check:
+        def check(res, expected, *args):
+            return res==expected
+
+    # Solution tests
+
     tests = []
-    for s in s.splitlines():
+    for s in text.splitlines():
         if s.startswith('Input:'):
             tests.append([[],[]])
             if m:=re.split(r'[\, ]*\w+ = ',s):
                 for i in range(1, len(m)):
-                    tests[-1][0].append(m[i])
+                    tests[-1][0].append(vp(m[i]))
         elif s.startswith('Output:'):
-            tests[-1][1].append(s[8:])
+            tests[-1][1] = vp(s[8:])
 
     for args, expected in tests:
-        def vparse(s):
-            return eval(s.replace('null','None').replace('true','True').replace('false','False'))
-
-        args = list(map(vparse,args))
-        expected = list(map(vparse,expected))[0]
-
-        func = getattr(Solution(), dir(Solution)[-1])
+        func = getattr(classname(), dir(classname)[-1])
         argc = func.__code__.co_argcount - 1
 
         if init:
@@ -95,32 +116,51 @@ def test(Solution, s, init=None, check=None):
 
         args = args[:argc]
 
-        is_seq = lambda v: type(v) in (dict, tuple, set, Generator)
-        to_list = lambda v: v if not is_seq(v) else [to_list(x) for x in v]
-
-        def vcast(func, vname, val):
-            tname = str(get_type_hints(func).get(vname, None))
-            if 'ListNode' in tname:
-                val = ListNode.parse(val)
-            elif 'TreeNode' in tname:
-                val = TreeNode.parse(val)
-            elif 'List' in tname or is_seq(val):
-                val = to_list(val)
-            return val
-
         for i in range(argc):
-            args[i] = vcast(func, func.__code__.co_varnames[i+1], args[i])
+            args[i] = vc(func, func.__code__.co_varnames[i+1], args[i])
 
-        res = vcast(func, 'return', func(*args))
+        res = func(*args)
+        res = vc(func, 'return', res)
 
         if type(expected) != type(res):
             res, expected = map(str, (res, expected))
 
-        if not check:
-            def check(res, expected, *args):
-                return res==expected
+        print_res(check(res, expected, *args), res, expected, *args)
 
-        c = lambda c,t,w=60: '\x1b[{1}m{2}\x1b[0m'.format(s:=str(t), 30+c, s[:w]+'...' if len(s)>=w else s)
-        passed = check(res, expected, *args)
-        e = 2 if passed else 1
-        print('%s args %s result %s expected %s' % (c(e,'PASSED' if passed else 'FAILED'), c(e,args), c(e,res), c(e,expected)))
+    # Class implementation tests
+
+    param = []
+    state = 0
+    for s in text.splitlines():
+        if s=='Input':
+            state = 1
+        elif state == 1:
+            param.append([[],[],[]])
+            param[-1][0] = vp(s)
+            state = 2
+        elif state == 2:
+            param[-1][1] = vp(s)
+            state = 0
+        elif s=='Output':
+            state = 3
+        elif state == 3:
+            param[-1][2] = vp(s)
+            state = 0
+
+    for calls, args, expected in param:
+        passed = True
+        res = [None]
+        instance = None
+        for i in range(len(calls)):
+            if calls[i] == type(classname()).__name__:
+                instance = classname()
+            else:
+                func = getattr(instance, calls[i])
+                r = func(*args[i])
+                if type(r) is float:
+                    r = round(r,5)
+                res.append(r)
+
+        passed = res == expected
+        print_res(passed, res, expected, args)
+
---
+++ leetcode/design-underground-system.py
@@ -0,0 +1,112 @@
+from Leetcode import *
+
+class UndergroundSystem:
+
+    def getSid(self,a,b):
+        return a+' to '+b
+
+    def __init__(self):
+        self.ids = {}
+        self.times = collections.defaultdict(list)
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.ids[id] = (stationName, t)
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        startStation, startTime = self.ids[id]
+        del self.ids[id]
+        sid = self.getSid(startStation,stationName)
+        tm = t - startTime
+        self.times[sid].append(tm)
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        sid = self.getSid(startStation, endStation)
+        count = len(self.times[sid])
+        t = 0
+        for x in self.times[sid]:
+            t += x
+        return t/count
+
+
+test(UndergroundSystem,'''
+1396. Design Underground System
+Medium
+
+2280
+
+111
+
+Add to List
+
+Share
+An underground railway system is keeping track of customer travel times between different stations. They are using this data to calculate the average time it takes to travel from one station to another.
+
+Implement the UndergroundSystem class:
+
+void checkIn(int id, string stationName, int t)
+A customer with a card ID equal to id, checks in at the station stationName at time t.
+A customer can only be checked into one place at a time.
+void checkOut(int id, string stationName, int t)
+A customer with a card ID equal to id, checks out from the station stationName at time t.
+double getAverageTime(string startStation, string endStation)
+Returns the average time it takes to travel from startStation to endStation.
+The average time is computed from all the previous traveling times from startStation to endStation that happened directly, meaning a check in at startStation followed by a check out from endStation.
+The time it takes to travel from startStation to endStation may be different from the time it takes to travel from endStation to startStation.
+There will be at least one customer that has traveled from startStation to endStation before getAverageTime is called.
+You may assume all calls to the checkIn and checkOut methods are consistent. If a customer checks in at time t1 then checks out at time t2, then t1 < t2. All events happen in chronological order.
+
+ 
+
+Example 1:
+
+Input
+["UndergroundSystem","checkIn","checkIn","checkIn","checkOut","checkOut","checkOut","getAverageTime","getAverageTime","checkIn","getAverageTime","checkOut","getAverageTime"]
+[[],[45,"Leyton",3],[32,"Paradise",8],[27,"Leyton",10],[45,"Waterloo",15],[27,"Waterloo",20],[32,"Cambridge",22],["Paradise","Cambridge"],["Leyton","Waterloo"],[10,"Leyton",24],["Leyton","Waterloo"],[10,"Waterloo",38],["Leyton","Waterloo"]]
+
+Output
+[null,null,null,null,null,null,null,14.00000,11.00000,null,11.00000,null,12.00000]
+
+Explanation
+UndergroundSystem undergroundSystem = new UndergroundSystem();
+undergroundSystem.checkIn(45, "Leyton", 3);
+undergroundSystem.checkIn(32, "Paradise", 8);
+undergroundSystem.checkIn(27, "Leyton", 10);
+undergroundSystem.checkOut(45, "Waterloo", 15);  // Customer 45 "Leyton" -> "Waterloo" in 15-3 = 12
+undergroundSystem.checkOut(27, "Waterloo", 20);  // Customer 27 "Leyton" -> "Waterloo" in 20-10 = 10
+undergroundSystem.checkOut(32, "Cambridge", 22); // Customer 32 "Paradise" -> "Cambridge" in 22-8 = 14
+undergroundSystem.getAverageTime("Paradise", "Cambridge"); // return 14.00000. One trip "Paradise" -> "Cambridge", (14) / 1 = 14
+undergroundSystem.getAverageTime("Leyton", "Waterloo");    // return 11.00000. Two trips "Leyton" -> "Waterloo", (10 + 12) / 2 = 11
+undergroundSystem.checkIn(10, "Leyton", 24);
+undergroundSystem.getAverageTime("Leyton", "Waterloo");    // return 11.00000
+undergroundSystem.checkOut(10, "Waterloo", 38);  // Customer 10 "Leyton" -> "Waterloo" in 38-24 = 14
+undergroundSystem.getAverageTime("Leyton", "Waterloo");    // return 12.00000. Three trips "Leyton" -> "Waterloo", (10 + 12 + 14) / 3 = 12
+Example 2:
+
+Input
+["UndergroundSystem","checkIn","checkOut","getAverageTime","checkIn","checkOut","getAverageTime","checkIn","checkOut","getAverageTime"]
+[[],[10,"Leyton",3],[10,"Paradise",8],["Leyton","Paradise"],[5,"Leyton",10],[5,"Paradise",16],["Leyton","Paradise"],[2,"Leyton",21],[2,"Paradise",30],["Leyton","Paradise"]]
+
+Output
+[null,null,null,5.00000,null,null,5.50000,null,null,6.66667]
+
+Explanation
+UndergroundSystem undergroundSystem = new UndergroundSystem();
+undergroundSystem.checkIn(10, "Leyton", 3);
+undergroundSystem.checkOut(10, "Paradise", 8); // Customer 10 "Leyton" -> "Paradise" in 8-3 = 5
+undergroundSystem.getAverageTime("Leyton", "Paradise"); // return 5.00000, (5) / 1 = 5
+undergroundSystem.checkIn(5, "Leyton", 10);
+undergroundSystem.checkOut(5, "Paradise", 16); // Customer 5 "Leyton" -> "Paradise" in 16-10 = 6
+undergroundSystem.getAverageTime("Leyton", "Paradise"); // return 5.50000, (5 + 6) / 2 = 5.5
+undergroundSystem.checkIn(2, "Leyton", 21);
+undergroundSystem.checkOut(2, "Paradise", 30); // Customer 2 "Leyton" -> "Paradise" in 30-21 = 9
+undergroundSystem.getAverageTime("Leyton", "Paradise"); // return 6.66667, (5 + 6 + 9) / 3 = 6.66667
+ 
+
+Constraints:
+
+1 <= id, t <= 106
+1 <= stationName.length, startStation.length, endStation.length <= 10
+All strings consist of uppercase and lowercase English letters and digits.
+There will be at most 2 * 104 calls in total to checkIn, checkOut, and getAverageTime.
+Answers within 10-5 of the actual value will be accepted.
+''')

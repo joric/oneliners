ba7dd06baffb9c0792a74139de056aaaf57110ec
joric <joric@users.noreply.github.com>
2022-10-27 14:13:44 +0500

    updated files

---
+++ leetcode/image-overlap.py
@@ -0,0 +1,62 @@
+from Leetcode import *
+
+class Solution:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        n = len(img1)
+        a = [i // n * 50 + i % n for i in range(n * n) if img1[i // n][i % n]]
+        b = [i // n * 50 + i % n for i in range(n * n) if img2[i // n][i % n]]
+        return max(Counter(i - j for i in a for j in b).values() or [0])
+
+class Solution:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        return (f:=lambda v:(n:=len(v)) and [i//n*50+i%n for i in range(n*n) if v[i//n][i%n]],
+            a:=f(img1), b:=f(img2)) and max(Counter(i-j for i in a for j in b).values() or [0])
+
+test(Solution, '''
+835. Image Overlap
+Medium
+
+588
+
+182
+
+Add to List
+
+Share
+You are given two images, img1 and img2, represented as binary, square matrices of size n x n. A binary matrix has only 0s and 1s as values.
+
+We translate one image however we choose by sliding all the 1 bits left, right, up, and/or down any number of units. We then place it on top of the other image. We can then calculate the overlap by counting the number of positions that have a 1 in both images.
+
+Note also that a translation does not include any kind of rotation. Any 1 bits that are translated outside of the matrix borders are erased.
+
+Return the largest possible overlap.
+
+ 
+
+Example 1:
+
+
+Input: img1 = [[1,1,0],[0,1,0],[0,1,0]], img2 = [[0,0,0],[0,1,1],[0,0,1]]
+Output: 3
+Explanation: We translate img1 to right by 1 unit and down by 1 unit.
+
+The number of positions that have a 1 in both images is 3 (shown in red).
+
+Example 2:
+
+Input: img1 = [[1]], img2 = [[1]]
+Output: 1
+Example 3:
+
+Input: img1 = [[0]], img2 = [[0]]
+Output: 0
+ 
+
+Constraints:
+
+n == img1.length == img1[i].length
+n == img2.length == img2[i].length
+1 <= n <= 30
+img1[i][j] is either 0 or 1.
+img2[i][j] is either 0 or 1.
+''')

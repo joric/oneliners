5886fb3104dab0b56f3b3b3a8422159a5733da08
joric <joric@users.noreply.github.com>
2022-10-27 14:26:24 +0500

    updated files

--- leetcode/image-overlap.py
+++ leetcode/image-overlap.py
@@ -14,10 +14,14 @@ class Solution2:
         return (f:=lambda v:(n:=len(v)) and [i//n*50+i%n for i in range(n*n) if v[i//n][i%n]],
             a:=f(img1), b:=f(img2)) and max(Counter(i-j for i in a for j in b).values() or [0])
 
+class Solution(object):
+    def largestOverlap(self, a, b):
+        return (f:=lambda v:(n:=len(v)) and [i//n*50+i%n for i in range(n*n)
+            if v[i//n][i%n]]) and max(Counter(i-j for i in f(a) for j in f(b)).values() or [0])
 
 # https://leetcode.com/problems/image-overlap/discuss/832472/Python-by-offset-vector-counting-w-Comment
 
-class Solution3:
+class Solution4:
     def largestOverlap(self, A, B):
         N = len(A)
         pos_of_1_in_A = [(xi, yi) for xi in range(N) for yi in range(N) if A[xi][yi]]
@@ -25,7 +29,7 @@ class Solution3:
         offset_vector_counting = Counter([(x1 - x2, y1 - y2) for (x1, y1) in pos_of_1_in_A for (x2, y2) in pos_of_1_in_B ])
         return max(offset_vector_counting.values() or [0])
 
-class Solution:
+class Solution5:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
         return (f:=lambda v:(n:=len(v)) and [(i,j) for i in range(n) for j in range(n) if v[i][j]],
             a:=f(img1), b:=f(img2)) and max(Counter([(x1-x2, y1-y2) for (x1,y1) in a for (x2,y2) in b]).values() or [0])

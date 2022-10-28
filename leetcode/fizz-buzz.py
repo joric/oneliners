137f99276c883eec3dce7a537467c85717b2c8ef
joric <joric@users.noreply.github.com>
2022-10-29 04:00:35 +0500

    updated files

---
+++ leetcode/fizz-buzz.py
@@ -0,0 +1,47 @@
+from lc import *
+
+class Solution1:
+    def fizzBuzz(self, n: int) -> List[str]:
+        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1,n+1)]
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        return['FizzBuzz'[i%-3&-4:i%-5&8^12] or str(i) for i in range(1,n+1)]
+
+test('''
+412. Fizz Buzz
+Easy
+
+1147
+
+181
+
+Add to List
+
+Share
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+Constraints:
+
+1 <= n <= 10^4
+''')

f556f0371e71ea305d07a4aaee301f2c27ce6575
joric <joric@users.noreply.github.com>
2022-10-29 10:09:06 +0500

    updated files

---
+++ leetcode/duplicate-zeros.py
@@ -0,0 +1,41 @@
+from lc import *
+
+def check(res, expected, arr):
+    return arr==expected
+
+class Solution1:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        [arr.insert(i,0) or arr.pop() for i in range(len(arr)-1,-1,-1) if not arr[i]]
+
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        arr[:]=[x for a in arr for x in ([a] if a else [0,0])][:len(arr)]
+
+
+test('''
+
+1089. Duplicate Zeros
+Easy
+
+Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+
+Example 1:
+
+Input: arr = [1,0,2,3,0,4,5,0]
+Output: [1,0,0,2,3,0,0,4]
+Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+Example 2:
+
+Input: arr = [1,2,3]
+Output: [1,2,3]
+Explanation: After calling your function, the input array is modified to: [1,2,3]
+
+Constraints:
+
+1 <= arr.length <= 10^4
+0 <= arr[i] <= 9
+
+''', check=check)

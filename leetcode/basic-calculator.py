0f9f0445c6d64bcf5a53c5ce58c476c82919b138
joric <joric@users.noreply.github.com>
2022-11-20 14:40:33 +0500

    updated files

--- leetcode/basic-calculator.py
+++ leetcode/basic-calculator.py
@@ -32,35 +32,37 @@ class Solution:
 
 
 test('''
+224. Basic Calculator
+Hard
 
-Description
-Implement a basic calculator to evaluate a simple expression string.
+Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
 
-The expression string contains only non-negative integers, +, -, *, / operators , open ( and closing parentheses ) and empty spaces . The integer division should truncate toward zero.
-
-You may assume that the given expression is always valid. All intermediate results will be in the range of [-2147483648, 2147483647]
-
-Wechat reply 【Google】 get the latest requent Interview questions. (wechat id : jiuzhang0607)
-
-
-Do not use the eval built-in library function.
+Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
 
 Example 1:
 
 Input: s = "1 + 1"
 Output: 2
-Explanation:1 + 1 = 2
 
 Example 2:
 
-Input: s = "6-4 / 2"
-Output: 4
-Explanation:4/2=2，6-2=4
+Input: s = " 2-1 + 2 "
+Output: 3
 
 Example 3:
 
-Input: s = "(2+4)*2"
-Output: 12
+Input: s = "(1+(4+5+2)-3)+(6+8)"
+Output: 23
+
+Constraints:
+
+1 <= s.length <= 3 * 10^5
+s consists of digits, '+', '-', '(', ')', and ' '.
+s represents a valid expression.
+'+' is not used as a unary operation (i.e., "+1" and "+(2 + 3)" is invalid).
+'-' could be used as a unary operation (i.e., "-1" and "-(2 + 3)" is valid).
+There will be no two consecutive operators in the input.
+Every number and running calculation will fit in a signed 32-bit integer.
 
 ''')
 

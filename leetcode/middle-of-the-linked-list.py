3aaf403072d34c3ecc2351edea9ad76955a3dc96
joric <joric@users.noreply.github.com>
2022-12-05 17:24:32 +0500

    updated files

---
+++ leetcode/middle-of-the-linked-list.py
@@ -0,0 +1,55 @@
+from lc import *
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast, slow = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return next((s for _ in count() if not(f and f.next and (f:=f.next.next,s:=s.next))),(f:=head,s:=head))
+
+test('''
+
+876. Middle of the Linked List
+Easy
+
+8012
+
+220
+
+Add to List
+
+Share
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+
+''')
+
+
+

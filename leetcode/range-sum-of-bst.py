95b5a4ddba9d4d39926bac269033df6d6277bd8a
joric <joric@users.noreply.github.com>
2022-12-07 05:07:49 +0500

    updated files

---
+++ leetcode/range-sum-of-bst.py
@@ -0,0 +1,51 @@
+from lc import *
+
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        if not root:
+            return 0
+        elif root.val < low:
+            return self.rangeSumBST(root.right, low, high)
+        elif root.val > high:
+            return self.rangeSumBST(root.left, low, high)
+        return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+
+
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        return 0 if not root else self.rangeSumBST(root.right, low, high) if root.val<low else self.rangeSumBST(root.left, low, high) if root.val>high else root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+
+test('''
+
+938. Range Sum of BST
+Easy
+4.8K
+340
+Companies
+Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+ 
+
+Example 1:
+
+
+Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+Output: 32
+Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
+Example 2:
+
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+Output: 23
+Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 2 * 10^4].
+1 <= Node.val <= 10^5
+1 <= low <= high <= 10^5
+All Node.val are unique.
+
+''')
+

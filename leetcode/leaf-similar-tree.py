8f69ac9ffc92dabefd55d6c2747ea60db3789794
joric <joric@users.noreply.github.com>
2022-12-08 05:14:08 +0500

    updated files

---
+++ leetcode/leaf-similar-tree.py
@@ -0,0 +1,43 @@
+from lc import *
+
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        return (f:=lambda l,r: l if not r else l+[r.val] if not r.left and not r.right else f(f(l,r.left),r.right))([], root1)==f([], root2)
+
+test('''
+
+872. Leaf-Similar Trees
+Easy
+2K
+59
+Companies
+Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+
+
+For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+ 
+
+Example 1:
+
+
+Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+Output: true
+Example 2:
+
+
+Input: root1 = [1,2,3], root2 = [1,3,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in each tree will be in the range [1, 200].
+Both of the given trees will have values in the range [0, 200].
+
+''')

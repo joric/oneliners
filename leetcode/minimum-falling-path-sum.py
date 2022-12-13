b418b9603b1dffbb124ba9748619b072c10dfd4b
joric <joric@users.noreply.github.com>
2022-12-13 23:28:49 +0500

    updated files

--- leetcode/minimum-falling-path-sum.py
+++ leetcode/minimum-falling-path-sum.py
@@ -13,6 +13,8 @@ class Solution:
     def minFallingPathSum(self, m: List[List[int]]) -> int:
         return any(m[r].__setitem__(c,m[r][c]+min(m[r-1][max(0,c-1):c+2])) for r in range(1,len(m)) for c in range(len(m[0]))) or min(m[-1])
 
+# https://leetcode.com/problems/minimum-falling-path-sum/solutions/407613/python3-in-1-line
+
 class Solution:
     def minFallingPathSum(self, m: List[List[int]]) -> int:
         return min(reduce(lambda a,b:[min(a[max(i-1,0):i+2])+b[i] for i in range(len(m))],m))

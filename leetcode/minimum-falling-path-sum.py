c46f4f5efc62eb5e36d64ee3042f33e1dbe61cb1
joric <joric@users.noreply.github.com>
2022-12-13 07:55:21 +0500

    updated files

--- leetcode/minimum-falling-path-sum.py
+++ leetcode/minimum-falling-path-sum.py
@@ -14,34 +14,30 @@ class Solution:
         return any(m[r].__setitem__(c,m[r][c]+min(m[r-1][max(0,c-1):c+2])) for r in range(1,len(m)) for c in range(len(m[0]))) or min(m[-1])
 
 test('''
+
 931. Minimum Falling Path Sum
 Medium
-3.3K
-103
-Companies
+
 Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
 
 A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
 
- 
-
 Example 1:
-
 
 Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
 Output: 13
 Explanation: There are two falling paths with a minimum sum as shown.
-Example 2:
 
+Example 2:
 
 Input: matrix = [[-19,57],[-40,-5]]
 Output: -59
 Explanation: The falling path with a minimum sum is shown.
- 
 
 Constraints:
 
 n == matrix.length == matrix[i].length
 1 <= n <= 100
 -100 <= matrix[i][j] <= 100
+
 ''')

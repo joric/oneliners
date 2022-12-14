4fd85c4fdb5de4f110a69adbec9e325c3cb46183
joric <joric@users.noreply.github.com>
2022-12-14 05:02:24 +0500

    updated files

---
+++ leetcode/house-robber.py
@@ -0,0 +1,40 @@
+from lc import *
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return reduce(lambda x,y:[x[1],max(x[0]+y,x[1])],nums,[0, 0])[1]
+
+test('''
+
+198. House Robber
+Medium
+15.5K
+305
+Companies
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 400
+
+''')
+

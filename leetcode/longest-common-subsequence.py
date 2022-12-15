c754aa54097fc3889b5660cb0fc6fad1121fff6e
joric <joric@users.noreply.github.com>
2022-12-15 05:21:31 +0500

    updated files

---
+++ leetcode/longest-common-subsequence.py
@@ -0,0 +1,70 @@
+from lc import *
+
+class Solution:
+    def longestCommonSubsequence(self, a: str, b: str) -> int:
+        m, n = len(a), len(b)
+        dp = [[0]*(n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = dp[i - 1][j - 1] + 1 if a[i - 1] == b[j - 1] else max(dp[i][j - 1], dp[i - 1][j])
+        return dp[-1][-1]
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        @cache
+        def f(a,b):
+            if len(a)==0 or len(b)==0:
+                return 0
+            if a[0]==b[0]:
+                return 1 + f(a[1:], text2[1:])
+            else:
+                return max(f(a, b[1:]), f(a[1:], b))
+        return f(text1, text2)
+
+# https://leetcode.com/problems/longest-common-subsequence/solutions/2912811/python-3-one-line/
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        return (f:=cache(lambda a,b:0 if len(a)==0 or len(b)==0 else 1+f(a[1:],b[1:]) if a[0]==b[0] else max(f(a,b[1:]),f(a[1:],b))))(text1,text2)
+
+test('''
+
+1143. Longest Common Subsequence
+Medium
+9K
+103
+Companies
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+ 
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+ 
+
+Constraints:
+
+1 <= text1.length, text2.length <= 1000
+text1 and text2 consist of only lowercase English characters.
+
+''')
+

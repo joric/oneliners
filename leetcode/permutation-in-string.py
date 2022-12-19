095abca2dd0f0feda01a4326602b488369942a4c
joric <joric@users.noreply.github.com>
2022-12-20 00:42:27 +0500

    updated files

---
+++ leetcode/permutation-in-string.py
@@ -0,0 +1,57 @@
+from lc import *
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1, n2 = len(s1), len(s2)
+        need = Counter(s1)
+        have = Counter(s2[:n1])
+        for i in range(n2-n1):
+            if need == have: return True
+            have[s2[i]] -= 1
+            have[s2[i+n1]] += 1
+            if not have[s2[i]]: del have[s2[i]]
+        return need == have
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        return any(Counter(s1)==Counter(s2[i:i+len(s1)]) for i in range(len(s2)-len(s1)+1))
+
+test('''
+
+567. Permutation in String
+Medium
+
+7748
+
+258
+
+Add to List
+
+Share
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+ 
+
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+ 
+
+Constraints:
+
+1 <= s1.length, s2.length <= 10^4
+s1 and s2 consist of lowercase English letters.
+Accepted
+522,152
+Submissions
+1,197,635
+
+''')

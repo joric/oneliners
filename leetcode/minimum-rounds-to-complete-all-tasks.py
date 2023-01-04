2b9881fad5df9ed21e2d2f2110b6c20b28fe3831
joric <joric@users.noreply.github.com>
2023-01-04 05:31:38 +0500

    updated files

---
+++ leetcode/minimum-rounds-to-complete-all-tasks.py
@@ -0,0 +1,46 @@
+from lc import *
+
+class Solution:
+    def minimumRounds(self, tasks: List[int]) -> int:
+        return -1 if 1 in (freq:=Counter(tasks).values()) else sum((a + 2) // 3 for a in freq)
+
+test('''
+
+2244. Minimum Rounds to Complete All Tasks
+Medium
+
+545
+
+14
+
+Add to List
+
+Share
+You are given a 0-indexed integer array tasks, where tasks[i] represents the difficulty level of a task. In each round, you can complete either 2 or 3 tasks of the same difficulty level.
+
+Return the minimum rounds required to complete all the tasks, or -1 if it is not possible to complete all the tasks.
+
+ 
+
+Example 1:
+
+Input: tasks = [2,2,3,3,2,4,4,4,4,4]
+Output: 4
+Explanation: To complete all the tasks, a possible plan is:
+- In the first round, you complete 3 tasks of difficulty level 2. 
+- In the second round, you complete 2 tasks of difficulty level 3. 
+- In the third round, you complete 3 tasks of difficulty level 4. 
+- In the fourth round, you complete 2 tasks of difficulty level 4.  
+It can be shown that all the tasks cannot be completed in fewer than 4 rounds, so the answer is 4.
+Example 2:
+
+Input: tasks = [2,3,3]
+Output: -1
+Explanation: There is only 1 task of difficulty level 2, but in each round, you can only complete either 2 or 3 tasks of the same difficulty level. Hence, you cannot complete all the tasks, and the answer is -1.
+ 
+
+Constraints:
+
+1 <= tasks.length <= 10^5
+1 <= tasks[i] <= 10^9
+''')

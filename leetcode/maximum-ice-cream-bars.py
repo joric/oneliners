97698a3e8bdcd9a400967c653a74a23b25a852ab
joric <joric@users.noreply.github.com>
2023-01-06 14:21:04 +0500

    updated files

--- leetcode/maximum-ice-cream-bars.py
+++ leetcode/maximum-ice-cream-bars.py
@@ -23,11 +23,12 @@ class Solution:
 
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        return bisect_right(list(accumulate(sorted(costs))), coins)
+        return sum(1 for c in sorted(costs) if (coins:=coins-c)>=0)
 
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        return sum(1 for c in sorted(costs) if (coins:=coins-c)>=0)
+        return bisect_right(list(accumulate(sorted(costs))), coins)
+
 
 test('''
 

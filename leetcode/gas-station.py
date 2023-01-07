1dd05b0ecd73a571c64815add6b5d2cd7d8fc0b4
joric <joric@users.noreply.github.com>
2023-01-07 06:54:07 +0500

    updated files

--- leetcode/gas-station.py
+++ leetcode/gas-station.py
@@ -20,6 +20,7 @@ class Solution:
                 
         return -1 if dist < n else max_so_far
 
+
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         t = b = 0
@@ -30,6 +31,7 @@ class Solution:
                 b = i + 1
         return -1 if sum(gas) < sum(cost) else b
 
+
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         return -1 if sum(gas)<sum(cost) else reduce(lambda p,i:p.__setitem__(0, p[0]+gas[i]-cost[i]) or ([0,i+1] if p[0]<0 else p), range(len(gas)),[0,0])[1]
@@ -37,7 +39,7 @@ class Solution:
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        return -1 if sum(gas)<sum(cost) else reduce(lambda p,d:(0,p[2]+1,p[2]+1) if p[0]+d<0 else p+(0,0,1),map(sub,gas,cost),[0,0,0])[1]
+        return -1 if sum(gas)<sum(cost) else reduce(lambda p,d:(0,p[2]+1,p[2]+1) if p[0]+d<0 else (p[0]+d,p[1],p[2]+1),map(sub,gas,cost),(0,0,0))[1]
 
 
 test('''
@@ -64,6 +66,7 @@ Example 1:
 
 Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
 Output: 3
+
 Explanation:
 Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
 Travel to station 4. Your tank = 4 - 1 + 5 = 8
@@ -72,10 +75,12 @@ Travel to station 1. Your tank = 7 - 3 + 2 = 6
 Travel to station 2. Your tank = 6 - 4 + 3 = 5
 Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
 Therefore, return 3 as the starting index.
+
 Example 2:
 
 Input: gas = [2,3,4], cost = [3,4,3]
 Output: -1
+
 Explanation:
 You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
 Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
@@ -83,7 +88,12 @@ Travel to station 0. Your tank = 4 - 3 + 2 = 3
 Travel to station 1. Your tank = 3 - 3 + 3 = 3
 You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
 Therefore, you can't travel around the circuit once no matter where you start.
- 
+
+
+Example 3:
+
+Input: gas = [5,1,2,3,4], cost = [4,4,1,5,1]
+Output: 4
 
 Constraints:
 

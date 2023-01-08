e0829ee4d43c6d0644931714515ab8e0a1fa7b0f
joric <joric@users.noreply.github.com>
2023-01-09 04:22:12 +0500

    updated files

--- leetcode/design-underground-system.py
+++ leetcode/design-underground-system.py
@@ -30,6 +30,24 @@ class UndergroundSystem:
             t += x
         return t/count
 
+# https://leetcode.com/problems/design-underground-system/discuss/1118415/Python-Dictionary-%2B-Counters-solution-explained
+
+class UndergroundSystem:
+    def __init__(self):
+        self.ids = {}
+        self.pairs = Counter()
+        self.freqs = Counter()
+        
+    def checkIn(self, id, stationName, t):
+        self.ids[id] = (stationName, t)
+
+    def checkOut(self, id, stationName, t):
+        Name2, t2 = self.ids.pop(id)
+        self.pairs[(Name2, stationName)] += t-t2
+        self.freqs[(Name2, stationName)] += 1
+        
+    def getAverageTime(self, startStation, endStation):
+        return self.pairs[startStation, endStation]/self.freqs[startStation, endStation]
 
 test('''
 1396. Design Underground System

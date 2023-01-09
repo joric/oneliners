f54f818ecf1f84a9ff86b93c22d463ae193b8293
joric <joric@users.noreply.github.com>
2023-01-09 05:27:59 +0500

    updated files

---
+++ leetcode/binary-tree-preorder-traversal.py
@@ -0,0 +1,42 @@
+from lc import *
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right) if root else []
+
+test('''
+
+144. Binary Tree Preorder Traversal
+Easy
+
+5579
+
+150
+
+Add to List
+
+Share
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+''')

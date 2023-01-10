1356f311dc26dde851115d59b49877be1e98375f
joric <joric@users.noreply.github.com>
2023-01-10 11:54:52 +0500

    updated files

--- leetcode/same-tree.py
+++ leetcode/same-tree.py
@@ -2,7 +2,7 @@ from lc import *
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) if p and q else p is q
+        return p.val==q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) if p and q else p is q
 
 test('''
 

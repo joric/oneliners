050720c6654c8f622ab8ae9fc5a8aab302216b3a
joric <joric@users.noreply.github.com>
2023-01-14 14:52:52 +0500

    updated files

--- leetcode/lexicographically-smallest-equivalent-string.py
+++ leetcode/lexicographically-smallest-equivalent-string.py
@@ -10,8 +10,7 @@ class Solution:
 
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        return (p:={c:c for c in set(s1+s2)}) and any(p.__setitem__(max((f:=lambda c:c if c not in p or p[c]==c
-            else f(p[c]))(a),f(b)),min(f(a),f(b))) for a,b in zip(s1,s2)) or ''.join(f(c) for c in baseStr)
+        return (p:={c:c for c in set(s1+s2)}) and any(p.__setitem__(max((f:=lambda c:c if c not in p or p[c]==c else f(p[c]))(a),f(b)),min(f(a),f(b))) for a,b in zip(s1,s2)) or ''.join(f(c) for c in baseStr)
 
 
 class Solution:
@@ -44,6 +43,16 @@ class Solution:
         return (p:={},f:=lambda x: p.setdefault(x,x) and (p[x] if x==p[x] else f(p[x])),u:=lambda a,b:
         p.__setitem__(x if (x:=f(a))>(y:=f(b)) else y, y if x>y else x),any(u(a,b) for a,b in zip(s1,s2))) and ''.join(f(c) for c in baseStr)
 
+
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        p = {}
+        def f(c):
+            p.setdefault(c,c)
+            return p[x] if x==p[x] else f(p[x])
+        for a,b in zip(s1,s2):
+            p[max(f(a),f(b))] = min(f(a),f(b))
+        return ''.join(f(c) for c in baseStr)
 
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:

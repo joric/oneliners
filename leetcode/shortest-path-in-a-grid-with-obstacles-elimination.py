59a480f42e76d5a643f40e1aa1d67f648e13d6fe
joric <joric@users.noreply.github.com>
2023-02-12 16:58:21 +0500

    updated files

--- leetcode/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ leetcode/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,6 +1,6 @@
 from lc import *
 
-class Solution1:
+class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         q, d = deque([(0,0)]), {(0,0):(0,0)}
         while q:
@@ -15,7 +15,7 @@ class Solution1:
                 return r
         return -1
 
-class Solution2:
+class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         m, n = len(grid), len(grid[0])
         q, v = deque([(0, 0, 0, k)]), set()
@@ -33,7 +33,7 @@ class Solution2:
                         q.append((steps + 1,) + new)
         return -1
 
-class Solution3:
+class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         pq = [(0, 0, 0, 0)]
         m, n = len(grid), len(grid[0])
@@ -73,6 +73,66 @@ class Solution:
                     else:
                         q.put((steps + 1, ni, nj, num))
         return -1
+
+
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        def f(i, j, k, c):
+            if i < 0 or j < 0 or i > len(grid) - 1 or j > len(grid[0]) - 1:
+                return
+            if k == 0 and grid[i][j] == 1:
+                return
+            if grid[i][j] == 2:
+                return
+
+            if self.r == len(grid) + len(grid[0]) - 2:
+                return
+            if i == len(grid) - 1 and j == len(grid[0]) - 1:
+                self.r = min(self.r, c)
+                return
+
+            p = grid[i][j]
+            grid[i][j] = 2
+            if p == 1:
+                k -= 1
+
+            f(i + 1, j, k, c+1)
+            f(i, j + 1, k, c+1)
+            f(i - 1, j, k, c+1)
+            f(i, j - 1, k, c+1)
+
+            grid[i][j] = p
+
+        self.r = inf
+        f(0, 0, k, 0)
+        return self.r if self.r!=inf else -1
+
+
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        g = {i+j*1j:x for i,r in enumerate(grid) for j,x in enumerate(r)}
+        n = len(grid)
+        m = len(grid[0])
+        @cache
+        def f(z, k, c):
+            x = g.get(z,-1)
+            if x==-1 or (k==0 and x==1) or x==2 or self.r==m+n-2:
+                return
+            if z==(n-1)+(m-1)*1j:
+                self.r = min(self.r, c)
+                return
+            g[z] = 2
+            if x==1:
+                k -= 1
+            [f(z + 1j**i,k,c+1) for i in range(4)]
+            g[z] = x
+        self.r = inf
+        f(0, k, 0)
+        return self.r if self.r!=inf else -1
+
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        return ((g:={i+j*1j:x for i,r in enumerate(grid) for j,x in enumerate(r)},n:=len(grid),m:=len(grid[0]),r:=[0]),setitem(r,0,inf),(f:=cache(lambda z,k,c:not((x:=g.get(z,-1))==-1 or (k==0 and x==1) or x==2 or r[0]==m+n-2) and not(z==(n-1)+(m-1)*1j and setitem(r,0,min(r[0],c))) and (setitem(g,z,2), (x==1 and (k:=k-1)), [f(z + 1j**i,k,c+1) for i in range(4)],setitem(g,z,x))))(0,k,0),r[0] if r[0]!=inf else -1)[-1]
 
 test('''
 1293. Shortest Path in a Grid with Obstacles Elimination

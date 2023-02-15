5a1758fd8d458e4bdc243db690be9fb1388b7f10
joric <joric@users.noreply.github.com>
2023-02-15 10:49:07 +0500

    updated files

---
+++ leetcode/add-to-array-form-of-integer.py
@@ -0,0 +1,50 @@
+from lc import *
+
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        return map(int,list(str(int("".join(map(str,num)))+k)))
+
+test('''
+
+989. Add to Array-Form of Integer
+Easy
+
+2098
+
+189
+
+Add to List
+
+Share
+The array-form of an integer num is an array representing its digits in left to right order.
+
+For example, for num = 1321, the array form is [1,3,2,1].
+Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
+
+ 
+
+Example 1:
+
+Input: num = [1,2,0,0], k = 34
+Output: [1,2,3,4]
+Explanation: 1200 + 34 = 1234
+Example 2:
+
+Input: num = [2,7,4], k = 181
+Output: [4,5,5]
+Explanation: 274 + 181 = 455
+Example 3:
+
+Input: num = [2,1,5], k = 806
+Output: [1,0,2,1]
+Explanation: 215 + 806 = 1021
+ 
+
+Constraints:
+
+1 <= num.length <= 10^4
+0 <= num[i] <= 9
+num does not contain any leading zeros except for the zero itself.
+1 <= k <= 10^4
+''')
+

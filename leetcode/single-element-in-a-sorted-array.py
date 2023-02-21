c53ec36c3a04d72af660647d56fae387cfef6c31
joric <joric@users.noreply.github.com>
2023-02-21 06:13:01 +0500

    updated files

--- leetcode/single-element-in-a-sorted-array.py
+++ leetcode/single-element-in-a-sorted-array.py
@@ -2,10 +2,10 @@ from lc import *
 
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
+        l,r = 0,len(nums)-1
         while l < r:
-            m = (l + r) // 2
-            if nums[m] == nums[m^1]:
+            m = (l + r)//2
+            if nums[m]==nums[m^1]:
                 l = m + 1
             else:
                 r = m

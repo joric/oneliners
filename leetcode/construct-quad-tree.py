84940a7a92359ce528202fe6dc2dd97db7cd2634
joric <joric@users.noreply.github.com>
2023-02-27 23:47:30 +0500

    updated files

--- leetcode/construct-quad-tree.py
+++ leetcode/construct-quad-tree.py
@@ -93,6 +93,13 @@ class Solution:
         return (h:=len(g)//2,Node(bool(g[0][0]),True,*([None]*4)) if all(all(i==g[0][0] for i in j) for j in g) else Node(bool(g[0][0]),
         False,*map(self.construct,[[[g[i][j] for j in range(k%2*h,(k%2+1)*h)] for i in range(k//2*h,(k//2+1)*h)] for k in range(4)])))[1]
 
+# https://leetcode.com/problems/construct-quad-tree/discuss/3235773/Python-one-liner
+
+class Solution:
+    def construct(self, g: List[List[int]]) -> 'Node':
+        return (not(s:=sum(sum(r) for r in g)) or s==(n:=len(g))*n) and Node(bool(s),1,*([None]*4)) or Node(bool(s),0,*map(self.construct,[[r[k%2*n//2:(k%2+1)*n//2] for r in g[k//2*n//2:(k//2+1)*n//2]] for k in range(4)]))
+
+
 test('''
 
 427. Construct Quad Tree

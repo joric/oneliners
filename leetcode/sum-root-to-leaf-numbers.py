c649df260022b7a2b2ec0e84886b5e92501295b5
joric <joric@users.noreply.github.com>
2023-03-14 05:35:14 +0500

    updated files

--- leetcode/sum-root-to-leaf-numbers.py
+++ leetcode/sum-root-to-leaf-numbers.py
@@ -33,11 +33,11 @@ class Solution:
 
 class Solution:
     def sumNumbers(self, root: TreeNode) -> int:
-        return (f:=lambda r,v:r and (v:=10*v+r.val,f(r.left,v)+f(r.right,v) if (r.left or r.right) else v)[1] or 0)(root,0)
+        return (f:=lambda r,v:bool(r) and (lambda l,r,v:f(l,v)+f(r,v) if (l or r) else v)(r.left,r.right,10*v+r.val))(root,0)
 
 class Solution:
     def sumNumbers(self, root: TreeNode) -> int:
-        return (f:=lambda r,v:bool(r)and(lambda l,r,v:f(l,v)+f(r,v)if(l or r)else v)(r.left,r.right,10*v+r.val))(root,0)
+        return (f:=lambda r,v:r and (v:=10*v+r.val,f(r.left,v)+f(r.right,v) if (r.left or r.right) else v)[1] or 0)(root,0)
 
 test('''
 

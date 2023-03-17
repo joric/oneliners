f909c5706de88398adc5a0c1d9c2a4ffe9e7468a
joric <joric@users.noreply.github.com>
2023-03-18 00:32:24 +0500

    updated files

--- leetcode/rotate-array.py
+++ leetcode/rotate-array.py
@@ -52,11 +52,15 @@ class Solution:
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        nums[:]= nums[len(nums)-k:]+nums[:len(nums)-k]
+        nums[:] = nums[-(k-len(nums)):]+nums[:-(k-len(nums))]
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        [nums.insert(0,nums.pop()) for i in range(k)]
+        nums[:]=nums[-k%len(nums):]+nums[:-k%len(nums)]
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        [nums.insert(0,nums.pop()) for _ in range(k)]
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
@@ -82,18 +86,25 @@ Example 1:
 
 Input: nums = [1,2,3,4,5,6,7], k = 3
 Output: [5,6,7,1,2,3,4]
+
 Explanation:
 rotate 1 steps to the right: [7,1,2,3,4,5,6]
 rotate 2 steps to the right: [6,7,1,2,3,4,5]
 rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
 Example 2:
 
 Input: nums = [-1,-100,3,99], k = 2
 Output: [3,99,-1,-100]
+
 Explanation: 
 rotate 1 steps to the right: [99,-1,-100,3]
 rotate 2 steps to the right: [3,99,-1,-100]
- 
+
+Example 3:
+
+Input: nums = [1,2], k = 5
+Output: [2,1]
 
 Constraints:
 

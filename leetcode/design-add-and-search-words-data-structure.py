23fb7fb23ca5e861036d890d095ca5388b016942
joric <joric@users.noreply.github.com>
2023-03-19 07:24:52 +0500

    updated files

--- leetcode/design-add-and-search-words-data-structure.py
+++ leetcode/design-add-and-search-words-data-structure.py
@@ -13,9 +13,7 @@ class WordDictionary:
                 return '' in n
             if word[i] == '.':
                 return any(f(x,i+1) for x in n.values())
-            if word[i] not in n:
-                return False
-            return f(n[word[i]],i+1)
+            return word[i] in n and f(n[word[i]],i+1)
         return f(self.t, 0)
 
 WordDictionary = type('',(),{'__init__':lambda s:setattr(s,'t',{}),'addWord':lambda s,w:reduce(lambda n,c:n.setdefault(c,{}),list(w)+[''],s.t) or None,'search':lambda s,w:(f:=lambda n,i:'' in n if i>=len(w) else any(f(x,i+1) for x in n.values()) if w[i]=='.' else w[i] in n and f(n[w[i]],i+1))(s.t,0)})

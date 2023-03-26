8c802e775f9153b0aafdc8a4a6e346f59209eacd
joric <joric@users.noreply.github.com>
2023-03-26 07:04:40 +0500

    updated files

--- leetcode/longest-cycle-in-a-graph.py
+++ leetcode/longest-cycle-in-a-graph.py
@@ -1,5 +1,24 @@
 from lc import *
 
+# fastest (100%)
+class Solution:
+    def longestCycle(self, edges: List[int]) -> int:
+        time = [0] * len(edges)
+        clock, ans = 1, -1
+        for x,t in enumerate(time):
+            if t:
+                continue
+            start_time = clock
+            while x >= 0:
+                if time[x]:
+                    if time[x] >= start_time:
+                        ans = max(ans, clock - time[x])
+                    break
+                time[x] = clock
+                clock += 1
+                x = edges[x]
+        return ans
+
 class Solution:
     def longestCycle(self, edges: List[int]) -> int:
         n = len(edges)

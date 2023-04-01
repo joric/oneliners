6f94785ebd7a05e947b00aad14ec5af133ca257f
joric <joric@users.noreply.github.com>
2023-04-01 06:49:02 +0500

    updated files

--- leetcode/binary-search.py
+++ leetcode/binary-search.py
@@ -20,6 +20,10 @@ class Solution:
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        return i if nums[i:=bisect_right(nums,target)-1]==target else -1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         return nums.index(target) if target in nums else -1
 
 test('''
@@ -37,19 +41,23 @@ Given an array of integers nums which is sorted in ascending order, and an integ
 
 You must write an algorithm with O(log n) runtime complexity.
 
- 
-
 Example 1:
 
 Input: nums = [-1,0,3,5,9,12], target = 9
 Output: 4
 Explanation: 9 exists in nums and its index is 4
+
 Example 2:
 
 Input: nums = [-1,0,3,5,9,12], target = 2
 Output: -1
 Explanation: 2 does not exist in nums so return -1
- 
+
+
+Example 3:
+
+Input: nums = [-1,0,3,5,9,12], target = 13
+Output: -1
 
 Constraints:
 

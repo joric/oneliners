6f23b6fad38026fc0fa9f2100f8cc2a6d6d84436
joric <joric@users.noreply.github.com>
2023-05-08 05:09:17 +0500

    updated files

--- leetcode/matrix-diagonal-sum.py
+++ leetcode/matrix-diagonal-sum.py
@@ -2,7 +2,7 @@ from lc import *
 
 class Solution:
     def diagonalSum(self, m: List[List[int]]) -> int:
-        n=len(m);return sum(m[i][i]+m[i][n-i-1] for i in range(n))-(n%2 and m[n//2][n//2])
+        n=len(m);return sum(m[i][i]+m[i][n-i-1]for i in range(n))-(n%2and m[n//2][n//2])
 
 test('''
 1572. Matrix Diagonal Sum

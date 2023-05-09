202f90d6f629d7ec33b9a15f68e4ddf288d65716
joric <joric@users.noreply.github.com>
2023-05-09 13:32:13 +0500

    updated files

--- leetcode/spiral-matrix.py
+++ leetcode/spiral-matrix.py
@@ -4,6 +4,10 @@ class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
 
+class Solution:
+    def spiralOrder(self, m: List[List[int]]) -> List[int]:
+        return m and[*m.pop(0)]+self.spiralOrder([*zip(*m)][::-1])
+
 test('''
 54. Spiral Matrix
 Medium

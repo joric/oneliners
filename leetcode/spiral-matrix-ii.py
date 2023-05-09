30a2791485fe58dc5deff4fccd9d214ea328695f
joric <joric@users.noreply.github.com>
2023-05-09 13:39:46 +0500

    updated files

--- leetcode/spiral-matrix-ii.py
+++ leetcode/spiral-matrix-ii.py
@@ -1,19 +1,19 @@
 from lc import *
 
 class Solution:
-    def generateMatrix(self, n):
-        A = [[0] * n for _ in range(n)]
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        m = [[0] * n for _ in range(n)]
         i, j, di, dj = 0, 0, 0, 1
         for k in range(n*n):
-            A[i][j] = k + 1
-            if A[(i+di)%n][(j+dj)%n]:
+            m[i][j] = k + 1
+            if m[(i+di)%n][(j+dj)%n]:
                 di, dj = dj, -di
             i += di
             j += dj
-        return A
+        return m
 
 class Solution:
-    def generateMatrix(self, n):
+    def generateMatrix(self, n: int) -> List[List[int]]:
         def f(a,b):
             m, i, j, di, dj = a
             m[i][j] = b + 1
@@ -25,14 +25,14 @@ class Solution:
         return reduce(f, range(n*n), [[[0] * n for _ in range(n)], 0, 0, 0, 1])[0]
 
 class Solution:
-    def generateMatrix(self, n):
+    def generateMatrix(self, n: int) -> List[List[int]]:
         def f(a,b):
             m, i, j, di, dj = a
             return [[m[h] if i!=h else m[h][:j]+[b+1]+m[h][j+1:] for h in range(len(m))]] + ([i+dj, j+-di, dj, -di] if m[(i+di)%n][(j+dj)%n] else [i+di, j+dj, di, dj])
         return reduce(f, range(n*n), [[[0] * n for _ in range(n)], 0, 0, 0, 1])[0]
 
 class Solution:
-    def generateMatrix(self, n):
+    def generateMatrix(self, n: int) -> List[List[int]]:
         return reduce(lambda a,b:[[a[0][h]if a[1]!=h else a[0][h][:a[2]]+[b+1]+a[0][h][a[2]+1:]for h in range(len(a[0]))]]+([a[1]+a[4],a[2]+-a[3],a[4],-a[3]]if a[0][(a[1]+a[3])%n][(a[2]+a[4])%n]else[a[1]+a[3],a[2]+a[4],a[3],a[4]]),range(n*n),[[[0]*n for _ in range(n)],0,0,0,1])[0]
 
 class Solution:
@@ -45,7 +45,7 @@ class Solution:
 
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        return (f:=lambda l,w,b:l*[[]]and[list(range(b,b+l))]+list(map(list,zip(*f(w-1,l,b+l)[::-1]))))(n,n,1)
+        return (f:=lambda l,w,b:l*[[]]and[[*range(b,b+l)]]+[*map(list,zip(*f(w-1,l,b+l)[::-1]))])(n,n,1)
 
 test('''
 59. Spiral Matrix II

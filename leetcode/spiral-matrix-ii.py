192c87ebdcd3906f7ac7127b869296dbd298c185
joric <joric@users.noreply.github.com>
2023-05-11 02:11:08 +0500

    updated files

--- leetcode/spiral-matrix-ii.py
+++ leetcode/spiral-matrix-ii.py
@@ -73,7 +73,7 @@ class Solution:
 
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        r=range(n);return[[4*(n-(a:=min(i,j,n+~i,n+~j)))*a+(i+j-2*a+1,4*n-6*a-i-j-3)[i>j]for j in r]for i in r]
+        r=range(n);return[[4*(n-(a:=min(i,j,~i+n,~j+n)))*a+(i+j-2*a+1,4*n-6*a-i-j-3)[i>j]for j in r]for i in r]
 
 test('''
 59. Spiral Matrix II

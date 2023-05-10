99c4706cbe1de4408b0cc09160d287c98cf03dce
joric <joric@users.noreply.github.com>
2023-05-11 02:33:16 +0500

    updated files

--- leetcode/spiral-matrix-ii.py
+++ leetcode/spiral-matrix-ii.py
@@ -1,48 +1,5 @@
 from lc import *
 
-class Solution:
-    def generateMatrix(self, n: int) -> List[List[int]]:
-        m = [[0] * n for _ in range(n)]
-        i, j, di, dj = 0, 0, 0, 1
-        for k in range(n*n):
-            m[i][j] = k + 1
-            if m[(i+di)%n][(j+dj)%n]:
-                di, dj = dj, -di
-            i += di
-            j += dj
-        return m
-
-class Solution:
-    def generateMatrix(self, n: int) -> List[List[int]]:
-        def f(a,b):
-            m, i, j, di, dj = a
-            m[i][j] = b + 1
-            di, dj = (dj, -di) if m[(i+di)%n][(j+dj)%n] else (di, dj)
-            i += di
-            j += dj
-            a = m, i, j, di, dj
-            return a
-        return reduce(f, range(n*n), [[[0] * n for _ in range(n)], 0, 0, 0, 1])[0]
-
-class Solution:
-    def generateMatrix(self, n: int) -> List[List[int]]:
-        def f(a,b):
-            m, i, j, di, dj = a
-            return [[m[h] if i!=h else m[h][:j]+[b+1]+m[h][j+1:] for h in range(len(m))]] + ([i+dj, j+-di, dj, -di] if m[(i+di)%n][(j+dj)%n] else [i+di, j+dj, di, dj])
-        return reduce(f, range(n*n), [[[0] * n for _ in range(n)], 0, 0, 0, 1])[0]
-
-class Solution:
-    def generateMatrix(self, n: int) -> List[List[int]]:
-        return reduce(lambda a,b:[[a[0][h]if a[1]!=h else a[0][h][:a[2]]+[b+1]+a[0][h][a[2]+1:]for h in range(len(a[0]))]]+([a[1]+a[4],a[2]+-a[3],a[4],-a[3]]if a[0][(a[1]+a[3])%n][(a[2]+a[4])%n]else[a[1]+a[3],a[2]+a[4],a[3],a[4]]),range(n*n),[[[0]*n for _ in range(n)],0,0,0,1])[0]
-
-class Solution:
-    def generateMatrix(self, n: int) -> List[List[int]]:
-        m, l = [], n*n+1
-        while l > 1:
-            l, r = l - len(m), l
-            m = [list(range(l, r))] + list(map(list,zip(*m[::-1])))
-        return m
-
 # https://leetcode.com/problems/spiral-matrix-ii/discuss/22391/Python-Recursive-Solution.3-lines./1348411
 
 class Solution:
@@ -58,18 +15,6 @@ class Solution:
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         return [[4*(n-(a:=min(min(i,n-i-1),min(j,n-j-1))))*a + ((i+j-2*a+1) if i<=j else (4*(n-2*a-1)-(i+j-2*a)+1)) for j in range(n)] for i in range(n)]
-
-class Solution:
-    def generateMatrix(self, n: int) -> List[List[int]]:
-        return [[4*(n-(a:=min(min(i,n-i-1),min(j,n-j-1))))*a + (i>j and (4*(n-2*a-1)-(i+j-2*a)+1) or (i+j-2*a+1)) for j in range(n)] for i in range(n)]
-
-class Solution:
-    def generateMatrix(self, n: int) -> List[List[int]]:
-        return [[4*(n-(a:=min(min(i,n-i-1),min(j,n-j-1))))*a+(i>j and 4*n-6*a-i-j-3 or i+j-2*a+1) for j in range(n)] for i in range(n)]
-
-class Solution:
-    def generateMatrix(self, n: int) -> List[List[int]]:
-        return[[4*(n-(a:=min(i,j,n-i-1,n-j-1)))*a+(i+j-2*a+1,4*n-6*a-i-j-3)[i>j]for j in range(n)]for i in range(n)]
 
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:

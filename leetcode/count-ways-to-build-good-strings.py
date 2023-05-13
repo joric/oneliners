d1193f120e724fb85f186f2abe426ac81e536e29
joric <joric@users.noreply.github.com>
2023-05-13 20:08:51 +0500

    updated files

--- leetcode/count-ways-to-build-good-strings.py
+++ leetcode/count-ways-to-build-good-strings.py
@@ -19,7 +19,7 @@ class Solution:
 
 class Solution:
     def countGoodStrings(self, l: int, h: int, z: int, o: int) -> int:
-        return (f:=cache(lambda i:i<=h and((l<=i)+f(i+z)+f(i+o))%(10**9+7)))(0)
+        return (f:=cache(lambda i:i<=h and ((l<=i)+f(i+z)+f(i+o))%(10**9+7)))(0)
 
 test('''
 2466. Count Ways To Build Good Strings

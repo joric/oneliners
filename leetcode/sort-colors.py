661250d4feaf2ca49ac04710ec80b8c4e6315aaf
joric <joric@users.noreply.github.com>
2023-05-15 20:20:21 +0500

    updated files

--- leetcode/sort-colors.py
+++ leetcode/sort-colors.py
@@ -52,6 +52,10 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         (s:=lambda a,x,y:(t:=a[x],setitem(a,x,a[y]),setitem(a,y,t),a)[3],f:=lambda a,i,j,k:(f(s(a,i,j),i+1,j+1,k) if a[j]==0 else f(a,i,j+1,k) if a[j]==1 else f(s(a,j,k),i,j,k-1)) if i<=j<=k else None)[1](nums,0,0,len(nums)-1)
 
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        (s:=lambda a,x,y:exec('a[x],a[y]=a[y],a[x]')or a,(f:=lambda a,i,j,k:i<=j<=k and(f(s(a,i,j),i+1,j+1,k)if a[j]==0 else f(a,i,j+1,k)if a[j]==1 else f(s(a,j,k),i,j,k-1)))(nums,0,0,len(nums)-1))
+
 test('''
 75. Sort Colors
 Medium

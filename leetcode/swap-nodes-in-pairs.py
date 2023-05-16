e90f0d6cce86694516f1a5793569a6c8a0bd7b2b
joric <joric@users.noreply.github.com>
2023-05-16 13:00:56 +0500

    updated files

--- leetcode/swap-nodes-in-pairs.py
+++ leetcode/swap-nodes-in-pairs.py
@@ -33,8 +33,12 @@ class Solution:
         return h
 
 class Solution:
+    def swapPairs(self, h)
+        return (h and h.next and (t:=h.next.val,setattr(h.next,'val',h.val),setattr(h,'val',t)) and self.swapPairs(h.next.next),h)[1]
+
+class Solution:
     def swapPairs(s, h: Optional[ListNode]) -> Optional[ListNode]:
-        return(h and h.next and(t:=h.next.val,setattr(h.next,'val',h.val),setattr(h,'val',t),s.swapPairs(h.next.next)),h)[1]
+        h and h.next and(t:=h.next.val,setattr(h.next,'val',h.val),setattr(h,'val',t),s.swapPairs(h.next.next));return h
 
 test('''
 24. Swap Nodes in Pairs

dd6607b6f92a80bfa19fefcaffdd9afa46048dd8
joric <joric@users.noreply.github.com>
2023-05-21 16:44:22 +0500

    updated files

--- leetcode/shortest-bridge.py
+++ leetcode/shortest-bridge.py
@@ -55,15 +55,16 @@ class Solution:
 class Solution:
     def shortestBridge(self, g: List[List[int]]) -> int:
         n,a,b = len(g),[],[]
-        def f(i,j,p):
+        def f(i,j):
             if 0<=i<n and 0<=j<n and g[i][j]==1:
                 g[i][j] = 0
                 p.append((i,j))
                 for x,y in ((i-1,j),(i+1,j),(i,j-1),(i,j+1)):
-                    f(x,y,p)
+                    f(x,y)
         for i in range(n):
             for j in range(n):
-                f(i,j,b if a else a)
+                p = b if a else a
+                f(i,j)
         return min(abs(u[0]-v[0])+abs(u[1]-v[1])-1 for u in a for v in b)
 
 # same for complex numbers (TLE)

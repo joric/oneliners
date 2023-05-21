32dc7cc133696136f3acd3be7c95837cf6a66c97
joric <joric@users.noreply.github.com>
2023-05-21 16:17:21 +0500

    updated files

--- leetcode/shortest-bridge.py
+++ leetcode/shortest-bridge.py
@@ -51,30 +51,33 @@ class Solution:
 
 
 # https://leetcode.com/problems/shortest-bridge/discuss/189489/concise-C%2B%2B-dfs-solution.
-# groups approach, gives TLE
-
-# borderline TLE (9222 ms)
+# groups approach, borderline TLE
 class Solution:
     def shortestBridge(self, g: List[List[int]]) -> int:
-        n,a,b = len(g),set(),set()
-        def f(i,j):
+        n,a,b = len(g),[],[]
+        def f(i,j,p):
             if 0<=i<n and 0<=j<n and g[i][j]==1:
                 g[i][j] = 0
-                p.add((i,j))
-                [*map(f,(i-1,i+1,i,i),(j,j,j-1,j+1))]
+                p.append((i,j))
+                for x,y in ((i-1,j),(i+1,j),(i,j-1),(i,j+1)):
+                    f(x,y,p)
         for i in range(n):
             for j in range(n):
-                p = b if a else a
-                f(i,j)
+                f(i,j,b if a else a)
         return min(abs(u[0]-v[0])+abs(u[1]-v[1])-1 for u in a for v in b)
 
-# TLE
+# same for complex numbers (TLE)
 class Solution:
     def shortestBridge(self, g: List[List[int]]) -> int:
-        g,a,b={i+j*1j:x for i,r in enumerate(g) for j,x in enumerate(r)},set(),set()
-        f=lambda z:g.get(z,0)==1 and z not in p and (setitem(g,z,0),p.add(z),[f(z+1j**k) for k in range(4)])
-        [(p:=b if a else a,f(z)) for z in g]
+        g,a,b={i+j*1j:x for i,r in enumerate(g) for j,x in enumerate(r)},[],[]
+        f=lambda z:g.get(z,0) and (setitem(g,z,0),p.append(z),[f(z+1j**k) for k in range(4)])
+        [(p:=(a,b)[not b],f(z)) for z in g]
         return int(min(abs(u.real-v.real)+abs(u.imag-v.imag)-1 for u in a for v in b))
+
+# one-liner, borderline TLE (8037 ms)
+class Solution:
+    def shortestBridge(self, g: List[List[int]]) -> int:
+        n,a,b,f=len(g),[],[],lambda i,j:n>i>=0<=j<n and g[i][j]==1 and(setitem(g[i],j,0),p.append((i,j)),[*map(f,(i-1,i+1,i,i),(j,j,j-1,j+1))]);r=range(n);[(p:=(a,b)[not b],f(i,j))for i in r for j in r];return min(abs(u[0]-v[0])+abs(u[1]-v[1])-1 for u in a for v in b)
 
 test('''
 934. Shortest Bridge

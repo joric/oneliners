d5d330b3ca4fe9a1940886195c828ef8e49dc839
joric <joric@users.noreply.github.com>
2023-05-21 16:45:29 +0500

    updated files

--- leetcode/shortest-bridge.py
+++ leetcode/shortest-bridge.py
@@ -56,7 +56,7 @@ class Solution:
     def shortestBridge(self, g: List[List[int]]) -> int:
         n,a,b = len(g),[],[]
         def f(i,j,p):
-            if 0<=i<n and 0<=j<n and g[i][j]==1:
+            if 0<=i<n and 0<=j<n and g[i][j]:
                 g[i][j] = 0
                 p.append((i,j))
                 for x,y in ((i-1,j),(i+1,j),(i,j-1),(i,j+1)):

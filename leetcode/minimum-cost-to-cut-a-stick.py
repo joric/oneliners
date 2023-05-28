24047931fffde16b91fb261930c2b7a9332a7292
joric <joric@users.noreply.github.com>
2023-05-28 14:52:35 +0500

    updated files

--- leetcode/minimum-cost-to-cut-a-stick.py
+++ leetcode/minimum-cost-to-cut-a-stick.py
@@ -4,6 +4,10 @@ class Solution:
     def minCost(self, n: int, c: List[int]) -> int:
         return c.sort()or(f:=cache(lambda i,j:(x:=bisect_right(c,i))!=(y:=bisect_right(c,j-1))and min(f(i,c[k])+f(c[k],j)+(j-i)for k in range(x,y))))(0,n)
 
+class Solution:
+    def minCost(self, n: int, c: List[int]) -> int:
+        return c.sort()or(f:=cache(lambda i,j:min((f(i,c[k])+f(c[k],j)+(j-i)for k in range(bisect_right(c,i),bisect_right(c,j-1))),default=0)))(0,n)
+
 test('''
 1547. Minimum Cost to Cut a Stick
 Hard

dd5f9e4b9c02f2f6b42e439ba12f7bf2fa32b460
joric <joric@users.noreply.github.com>
2023-06-07 13:31:20 +0500

    updated files

--- leetcode/minimum-flips-to-make-a-or-b-equal-to-c.py
+++ leetcode/minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -15,6 +15,10 @@ class Solution:
         return (bin((a|b)^c)+bin(a&b&((a|b)^c))).count("1")
 
 class Solution:
+    def minFlips(self, a, b, c):
+        return (c:=(a|b)^c).bit_count()+(a&b&c).bit_count()
+
+class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
         return (c^(a|b)).bit_count()+(a&b&~c).bit_count()
 
@@ -32,7 +36,7 @@ class Solution:
 
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
-        return (c^(a|b)|(a&b&~c)<<32).bit_count()
+        return ((a|b)^c|(a&b&~c)<<32).bit_count()
 
 test('''
 1318. Minimum Flips to Make a OR b Equal to c

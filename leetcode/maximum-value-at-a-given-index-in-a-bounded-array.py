db808190659af46d505d02a4a2e3dd2c4119a35c
joric <joric@users.noreply.github.com>
2023-06-10 07:05:57 +0500

    updated files

--- leetcode/maximum-value-at-a-given-index-in-a-bounded-array.py
+++ leetcode/maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -67,7 +67,7 @@ class Solution:
 
 class Solution:
     def maxValue(self, n: int, i: int, m: int) -> int:
-        f=lambda l,b:(l-b+(1+b)*b,(1+b+b-l)*l)[b<l]//2;return bisect_right(range(1,m+1),0,key=lambda x:f(i,x-1)+f(n-i,x)>m)
+        f=lambda l,b:((b+b-l+1)*l//2,(1+b)*b//2+(l-b))[b<l];return bisect_right(range(1,m+1),0,key=lambda x:f(i,x-1)+f(n-i,x)>m)
 
 test('''
 1802. Maximum Value at a Given Index in a Bounded Array
@@ -103,6 +103,11 @@ Example 2:
 
 Input: n = 6, index = 1,  maxSum = 10
 Output: 3
+
+
+Input: n = 3, index = 2,  maxSum = 18
+Output: 7
+
  
 
 Constraints:

450da3ccec92c6bf620c971e44dfdae89378cedc
joric <joric@users.noreply.github.com>
2023-06-10 14:47:43 +0500

    updated files

--- leetcode/maximum-value-at-a-given-index-in-a-bounded-array.py
+++ leetcode/maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -1,5 +1,22 @@
 from lc import *
 
+# https://leetcode.com/problems/maximum-value-at-a-given-index-in-a-bounded-array/discuss/3620084/Math-Solution.-O(1)-time-complexity-O(1)-memory
+
+class Solution:
+    def maxValue(self, n: int, index: int, maxSum: int) -> int:
+        maxSum -= n
+        r = n - index - 1
+        (x1, x2) = (index, r) if index < r else (r, index)
+        res = sqrt(maxSum)
+        if res < x1:
+            return int(res) + 1
+        res = (sqrt((2 * x1 + 1) ** 2 + 4 * ((x1 + 1) * x1 + 2 * maxSum)) - (2 * x1 + 1)) / 2
+        if x1 <= res < x2:
+            return int(res) + 1
+        res = (2 * maxSum + x1 * (x1 + 1) + x2 * (x2 + 1)) / (x1 + x2 + 1) / 2
+        if res >= x2:
+            return int(res) + 1
+
 #https://leetcode.com/problems/maximum-value-at-a-given-index-in-a-bounded-array/discuss/1119801/JavaC%2B%2BPython-Binary-Search
 
 class Solution:

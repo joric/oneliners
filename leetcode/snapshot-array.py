c65aa2fe7973b0baee5a5860318f7c8b6a1bc461
joric <joric@users.noreply.github.com>
2023-06-11 06:17:53 +0500

    updated files

--- leetcode/snapshot-array.py
+++ leetcode/snapshot-array.py
@@ -2,20 +2,20 @@ from lc import *
 
 class SnapshotArray:
     def __init__(self, length: int):
-        self.a = [[[-1, 0]] for _ in range(length)]
+        self.a = [[[-1,0]] for _ in range(length)]
         self.i = 0
 
     def set(self, index: int, val: int) -> None:
         self.a[index].append([self.i, val])
 
     def snap(self) -> int:
-        self.i+=1
+        self.i += 1
         return self.i - 1
 
     def get(self, index: int, snap_id: int) -> int:
         return self.a[index][bisect_right(self.a[index],[snap_id+1])-1][1]
 
-SnapshotArray=type('',(),{'__init__':lambda s,l:setattr(s,'a',[[[-1, 0]]for _ in range(l)]) or setattr(s,'i',0),'set':lambda s,i,v:s.a[i].append([s.i,v]),'snap':lambda s:setattr(s,'i',s.i+1) or s.i-1,'get':lambda s,i,d:s.a[i][bisect_right(s.a[i],[d+1])-1][1]})
+SnapshotArray=type('',(),{'__init__':lambda s,l:setattr(s,'a',[[[-1,0]]for _ in range(l)])or setattr(s,'i',0),'set':lambda s,i,v:s.a[i].append([s.i,v]),'snap':lambda s:setattr(s,'i',s.i+1) or s.i-1,'get':lambda s,i,d:s.a[i][bisect_right(s.a[i],[d+1])-1][1]})
 
 
 test('''

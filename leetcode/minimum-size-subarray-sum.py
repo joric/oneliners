80ba176e0a8ab5dfbae772f54d08270a45d4f081
joric <joric@users.noreply.github.com>
2023-07-06 08:10:40 +0500

    updated files

--- leetcode/minimum-size-subarray-sum.py
+++ leetcode/minimum-size-subarray-sum.py
@@ -1,10 +1,28 @@
 from lc import *
 
+# https://leetcode.com/problems/minimum-size-subarray-sum/discuss/433123/JavaC%2B%2BPython-Sliding-Window
+
+class Solution:
+    def minSubArrayLen(self, s: int, a: List[int]) -> int:
+        n=len(a)
+        i,r=0,n+1
+        for j in range(n):
+            s -= a[j]
+            while s <= 0:
+                r = min(r, j-i+1)
+                s += a[i]
+                i += 1
+        return r % (n+1)
+
+class Solution:
+    def minSubArrayLen(self, s: int, a: List[int]) -> int:
+        n=len(a);i,r=0,n+1;[(s:=s-a[j],all(s<=0 and(r:=min(r,j-i+1),s:=s+a[i],i:=i+1)for _ in count()))for j in range(n)];return r%(n+1)
+
 # https://leetcode.com/problems/minimum-size-subarray-sum/discuss/466182/2-lines-immutable-bisect-python
 
 class Solution:
-    def minSubArrayLen(self, s: int, n: List[int]) -> int:
-        a=[*accumulate(n)];return min([i-bisect_right(a,x-s)+1for i,x in enumerate(a)if x>=s],default=0)
+    def minSubArrayLen(self, s: int, a: List[int]) -> int:
+        a=[*accumulate(a)];return min([i-bisect_right(a,x-s)+1for i,x in enumerate(a)if x>=s],default=0)
 
 test('''
 209. Minimum Size Subarray Sum

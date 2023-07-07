49f1ece51418f838844d078d3493e604311efab5
joric <joric@users.noreply.github.com>
2023-07-07 07:43:23 +0500

    updated files

--- leetcode/maximize-the-confusion-of-an-exam.py
+++ leetcode/maximize-the-confusion-of-an-exam.py
@@ -26,11 +26,6 @@ class Solution:
 # bonus Y-combinator: no semicolons, no walrus
 class Solution:
     def maxConsecutiveAnswers(self, s: str, k: int) -> int:
-        f = lambda i,r,c:i<len(s) and f(c.update(s[i]) or i+1,r+(not(r-k>=max(c.values())and[c.update({s[i-r]:-1})])),c) or r
-        return f(0,0,Counter())
-
-class Solution:
-    def maxConsecutiveAnswers(self, s: str, k: int) -> int:
         return (lambda a:lambda i,r,c:a(a,i,r,c))(lambda f,i,r,c:i<len(s) and f(f,c.update(s[i]) or i+1,r+(not(r-k>=max(c.values())and[c.update({s[i-r]:-1})])),c) or r)(0,0,Counter())
 
 test('''

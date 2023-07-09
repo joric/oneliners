c6318a192fbb7703befc545b35e61c4af7df6e03
joric <joric@users.noreply.github.com>
2023-07-09 08:18:13 +0500

    updated files

--- leetcode/substring-with-largest-variance.py
+++ leetcode/substring-with-largest-variance.py
@@ -1,6 +1,6 @@
 from lc import *
 
-# fast but very long
+# fast but long
 class Solution:
     def largestVariance(self, s: str) -> int:
         q = defaultdict(lambda:[])
@@ -29,7 +29,6 @@ class Solution:
     def largestVariance(self, s: str) -> int:
         r,q=0,defaultdict(lambda:[]);[q[c].append(i)for i,c in enumerate(s)];[(x:=--1,y:=-1,m:=Counter(),g:=Counter(),c:=0,[(s[j]==a and(x:=i,c:=c+1)or(s[j]==b and(y:=i,c:=c-1)),(k:=min(x,y))>=0 and(r:=max(r,c-m[k-1],g[k-1]-c)),setitem(m,i,min(m[i-1],c)),setitem(g,i,max(g[i-1],c)))for i,j in enumerate(sorted(q[a]+q[b]))])for a,b in itertools.combinations(q,2)];return r
 
-
 # https://leetcode.com/problems/substring-with-largest-variance/discuss/2551276/Python-super-clean-solution
 
 class Solution:
@@ -57,6 +56,30 @@ class Solution:
 class Solution:
     def largestVariance(self, s: str) -> int:
         r,q=0,Counter(s);[(p:=0,d:=-len(s),[(c==x and(p:=p+1,d:=d+1)or c==y and(p:=p-1,d:=p,p<0 and(p:=0)),r<d and(r:=d))for c in s])for x,y in permutations(set(s),2)if q[x]!=1];return r
+
+# https://leetcode.com/problems/substring-with-largest-variance/discuss/2039178/Weird-Kadane
+
+class Solution:
+    def largestVariance(self, s: str) -> int:
+        r = 0
+        for x,y in permutations(set(s), 2):
+            p = 0
+            b = 0
+            m = inf
+            for c in s:
+                if c==x:
+                    p += 1
+                elif c==y:
+                    m = min(m,b)
+                    p -= 1
+                    b = p
+                r = max(r,p-m)
+        return r
+
+# barely passes time limit
+class Solution:
+    def largestVariance(self, s: str) -> int:
+        r=0;all((p:=0,b:=0,m:=inf,all((c==x and(p:=p+1)or c==y and(m:=min(m,b),p:=p-1,b:=p),r:=max(r,p-m))for c in s))for x,y in permutations(set(s),2));return r
 
 test('''
 2272. Substring With Largest Variance

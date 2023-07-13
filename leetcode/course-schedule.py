1ac80c2553cbbc5626c8270dc98c8a04e0ad1bf5
joric <joric@users.noreply.github.com>
2023-07-13 14:55:09 +0500

    updated files

--- leetcode/course-schedule.py
+++ leetcode/course-schedule.py
@@ -20,15 +20,15 @@ class Solution:
 
 class Solution:
     def canFinish(self, n: int, p: List[List[int]]) -> bool:
-        g,c=defaultdict(list),Counter({k:0 for k in range(n)})
+        g,c=defaultdict(list),Counter()
         [(c.update({b:1}),g[a].append(b))for a,b in p]
-        t=[k for k,v in c.items()if v<=0]
+        t=[k for k in range(n)if c[k]<1]
         [c.update({k:-1})or c[k]==0 and t.append(k)for o in t for k in g[o]]
         return len(t)==n
 
 class Solution:
     def canFinish(self, n: int, p: List[List[int]]) -> bool:
-        g,c=defaultdict(list),Counter({k:0 for k in range(n)});[(c.update({b:1}),g[a].append(b))for a,b in p];t=[k for k,v in c.items()if v<=0];[c.update({k:-1})or c[k]==0 and t.append(k)for o in t for k in g[o]];return len(t)==n
+        g,c=defaultdict(list),Counter();[(c.update({b:1}),g[a].append(b))for a,b in p];t=[k for k in range(n)if c[k]<1];[c.update({k:-1})or c[k]==0 and t.append(k)for o in t for k in g[o]];return len(t)==n
 
 test('''
 207. Course Schedule

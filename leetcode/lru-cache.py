611a61d151d56b9a3e60767908ce0d0ffb380eb8
joric <joric@users.noreply.github.com>
2023-07-18 11:08:00 +0500

    updated files

--- leetcode/lru-cache.py
+++ leetcode/lru-cache.py
@@ -44,11 +44,11 @@ class LRUCache:
 LRUCache=type('',(),{
     '__init__':lambda s,c:setattr(s,'c',c)or setattr(s,'d',OrderedDict()),
     'get':lambda s,k:s.d.move_to_end(k)or s.d[k]if k in s.d else-1,
-    'put':lambda s,k,v:(setitem(s.d,k,v),s.d.move_to_end(k),len(s.d)>s.c and s.d.popitem(last=False))and None
+    'put':lambda s,k,v:(setitem(s.d,k,v),s.d.move_to_end(k),len(s.d)>s.c and s.d.popitem(last=0))and None
 })
 
 
-LRUCache=type('',(),{'__init__':lambda s,c:setattr(s,'c',c)or setattr(s,'d',OrderedDict()),'get':lambda s,k:s.d.move_to_end(k)or s.d[k]if k in s.d else-1,'put':lambda s,k,v:(setitem(s.d,k,v),s.d.move_to_end(k),len(s.d)>s.c and s.d.popitem(last=False))and None})
+LRUCache=type('',(),{'__init__':lambda s,c:setattr(s,'c',c)or setattr(s,'d',OrderedDict()),'get':lambda s,k:s.d.move_to_end(k)or s.d[k]if k in s.d else-1,'put':lambda s,k,v:(setitem(s.d,k,v),s.d.move_to_end(k),len(s.d)>s.c and s.d.popitem(last=0))and None})
 
 test('''
 146. LRU Cache

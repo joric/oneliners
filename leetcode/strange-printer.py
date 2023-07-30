1715db4e124b7ecb4613889d9a68e8a9929d19c7
joric <joric@users.noreply.github.com>
2023-07-30 11:29:50 +0500

    updated files

--- leetcode/strange-printer.py
+++ leetcode/strange-printer.py
@@ -24,6 +24,10 @@ class Solution:
     def strangePrinter(self, s: str) -> int:
         return(f:=cache(lambda s:s and(r:=f(s[:-1])+1,[r:=min(r,f(s[:i+1])+f(s[i+1:-1]))for i in range(len(s)-1)if s[i]==s[-1]],r)[2]or 0))(s)
 
+class Solution:
+    def strangePrinter(self, s: str) -> int:
+        return(f:=cache(lambda s:s and min([f(s[:-1])+1]+[f(s[:i+1])+f(s[i+1:-1])for i in range(len(s)-1)if s[i]==s[-1]])or 0))(s)
+
 test('''
 664. Strange Printer
 Hard

0495145e670e8535b69de1abc46a994ab6a171cc
joric <joric@users.noreply.github.com>
2023-07-31 13:22:21 +0500

    updated files

---
+++ leetcode/minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,54 @@
+from lc import *
+
+class Solution:
+    def minimumDeleteSum(self, a: str, b: str) -> int:
+        a,b = [*map(ord,a)],[*map(ord, b)]
+        m,n = len(a),len(b)
+        d = [[0 for _ in range(m)]for _ in range(n)]
+        for j,i in product(range(m),range(n)):
+            d[i][j] = max((j>0)*d[i][j-1],(i>0)*d[i-1][j],(a[j]==b[i])*((i>0 and j>0)*d[i-1][j-1]+a[j]))
+        return sum(a)+sum(b)-2*d[-1][-1]
+
+class Solution:
+    def minimumDeleteSum(self, a: str, b: str) -> int:
+        a,b=[*map(ord,a)],[*map(ord,b)];m,n=len(a),len(b);d=[[0 for _ in range(m)]for _ in range(n)];[setitem(d[i],j,max((j>0)*d[i][j-1],(i>0)*d[i-1][j],(a[j]==b[i])*((i>0<j)*d[i-1][j-1]+a[j])))for j,i in product(range(m),range(n))];return sum(a)+sum(b)-2*d[-1][-1]
+
+test('''
+712. Minimum ASCII Delete Sum for Two Strings
+Medium
+
+3102
+
+81
+
+Add to List
+
+Share
+Given two strings s1 and s2, return the lowest ASCII sum of deleted characters to make two strings equal.
+
+ 
+
+Example 1:
+
+Input: s1 = "sea", s2 = "eat"
+Output: 231
+Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the sum.
+Deleting "t" from "eat" adds 116 to the sum.
+At the end, both strings are equal, and 115 + 116 = 231 is the minimum sum possible to achieve this.
+Example 2:
+
+Input: s1 = "delete", s2 = "leet"
+Output: 403
+Explanation: Deleting "dee" from "delete" to turn the string into "let",
+adds 100[d] + 101[e] + 101[e] to the sum.
+Deleting "e" from "leet" adds 101[e] to the sum.
+At the end, both strings are equal to "let", and the answer is 100+101+101+101 = 403.
+If instead we turned both strings into "lee" or "eet", we would get answers of 433 or 417, which are higher.
+ 
+
+Constraints:
+
+1 <= s1.length, s2.length <= 1000
+s1 and s2 consist of lowercase English letters.
+''')
+

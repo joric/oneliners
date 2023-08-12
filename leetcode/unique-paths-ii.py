b2cfb2cd1b477d2821619750aa5903a102e8206c
joric <joric@users.noreply.github.com>
2023-08-12 12:49:32 +0500

    updated files

--- leetcode/unique-paths-ii.py
+++ leetcode/unique-paths-ii.py
@@ -24,7 +24,7 @@ class Solution:
 
 class Solution:
     def uniquePathsWithObstacles(self, g: List[List[int]]) -> int:
-        m,n=len(g),len(g[0]);return(f:=cache(lambda i,j:int(n>j>=0<=i<m and 0==g[i][j]and(i==m-1and j==n-1or sum(map(f,(i,i+1),(j+1,j)))))))(0,0)
+        m,n=len(g),len(g[0]);return(f:=cache(lambda i,j:int(n>j>=0<=i<m and 0==g[i][j]and((i,j)==(m-1,n-1)or sum(map(f,(i,i+1),(j+1,j)))))))(0,0)
 
 test('''
 63. Unique Paths II

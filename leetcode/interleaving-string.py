7cb34c15a6c1783140460e64e8f0778af617961f
joric <joric@users.noreply.github.com>
2023-08-25 14:52:20 +0500

    updated files

--- leetcode/interleaving-string.py
+++ leetcode/interleaving-string.py
@@ -9,11 +9,13 @@ class Solution:
             return (s1 or s2 or "") == s3    
         return any(s3[:1] == x[:1] and self.isInterleave(x[1:], y, s3[1:]) for x, y in ((s1, s2), (s2, s1)))
 
-# TLE without @cache
 class Solution:
     @cache
     def isInterleave(self, a: str, b: str, c: str) -> bool:
         return any(c[:1]==x[:1]and self.isInterleave(x[1:],y,c[1:])for x,y in((a,b),(b,a)))if(a and b)else(a or b or'')==c
+
+class Solution:
+    isInterleave=cache(lambda s,a,b,c:any(c[:1]==x[:1]and s.isInterleave(x[1:],y,c[1:])for x,y in((a,b),(b,a)))if(a and b)else(a or b or'')==c)
 
 class Solution:
     def isInterleave(self, a: str, b: str, c: str) -> bool:

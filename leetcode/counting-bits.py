5b6f0b8d7093591d2a4219d54d48891496230b26
joric <joric@users.noreply.github.com>
2023-09-01 05:12:01 +0500

    updated files

--- leetcode/counting-bits.py
+++ leetcode/counting-bits.py
@@ -2,6 +2,13 @@ from lc import *
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        r=[0]
+        for i in range(1,n+1):
+            r += r[i>>1]+int(i&1),
+        return r
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
         return[bin(x).count('1')for x in range(n+1)]
 
 test('''

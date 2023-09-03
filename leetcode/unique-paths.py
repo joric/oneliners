edce9a4dd1ffc7338802107306bdfbbcd7163659
joric <joric@users.noreply.github.com>
2023-09-03 05:36:53 +0500

    updated files

--- leetcode/unique-paths.py
+++ leetcode/unique-paths.py
@@ -1,5 +1,21 @@
 from lc import *
 
+class Solution(object):
+    def uniquePaths(self, m, n):
+        dp = [[0]*m for _ in range(n)]
+        for i in range(m):
+            dp[0][i] = 1
+        for i in range(n):
+            dp[i][0] = 1
+        for i in range(1,n):
+            for j in range(1,m):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[n-1][m-1]
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        return factorial(m + n - 2) // factorial(m - 1) // factorial(n - 1)
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         return comb(m+n-2,n-1)

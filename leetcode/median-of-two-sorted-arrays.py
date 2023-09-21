837df7090a5726b036584358f4eb2cd357050698
joric <joric@users.noreply.github.com>
2023-09-21 07:39:38 +0500

    updated files

--- leetcode/median-of-two-sorted-arrays.py
+++ leetcode/median-of-two-sorted-arrays.py
@@ -2,6 +2,21 @@ from lc import *
 
 # https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2755/6-lines-O(log(min(mn)))-Python
 
+def findMedianSortedArrays(self, nums1, nums2):
+    a, b = sorted((nums1, nums2), key=len)
+    m, n = len(a), len(b)
+    after = (m + n - 1) / 2
+    lo, hi = 0, m
+    while lo < hi:
+        i = (lo + hi) / 2
+        if after-i-1 < 0 or a[i] >= b[after-i-1]:
+            hi = i
+        else:
+            lo = i + 1
+    i = lo
+    nextfew = sorted(a[i:i+2] + b[after-i:after-i+2])
+    return (nextfew[0] + nextfew[1 - (m+n)%2]) / 2.0
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         a, b = sorted((nums1, nums2), key=len)

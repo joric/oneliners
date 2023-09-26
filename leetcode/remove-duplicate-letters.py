cb4efd856f2a11a644751f0595bc7d2db6287811
joric <joric@users.noreply.github.com>
2023-09-26 10:48:36 +0500

    updated files

---
+++ leetcode/remove-duplicate-letters.py
@@ -0,0 +1,65 @@
+from lc import *
+
+# https://leetcode.com/problems/remove-duplicate-letters/discuss/76787/Some-Python-solutions
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        for c in sorted(set(s)):
+            x = s[s.index(c):]
+            if set(x)==set(s):
+                return c+self.removeDuplicateLetters(x.replace(c,''))
+        return ''
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        def f(s):
+            for c in sorted(set(s)):
+                x = s[s.index(c):]
+                if set(x)==set(s):
+                    return c+f(x.replace(c,''))
+            return ''
+        return f(s)
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        return(f:=lambda s:next((c+f(x.replace(c,''))for c in sorted(set(s))if set(x:=s[s.index(c):])==set(s)),''))(s)
+
+test('''
+316. Remove Duplicate Letters
+Medium
+
+7510
+
+489
+
+Add to List
+
+Share
+Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+
+ 
+
+Example 1:
+
+Input: s = "bcabc"
+Output: "abc"
+Example 2:
+
+Input: s = "cbacdcbc"
+Output: "acdb"
+ 
+
+Constraints:
+
+1 <= s.length <= 10^4
+s consists of lowercase English letters.
+ 
+
+Note: This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+
+Accepted
+255,637
+Submissions
+549,748
+''')
+

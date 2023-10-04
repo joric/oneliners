294f31f63588763480d7446af8606079ac22025d
joric <joric@users.noreply.github.com>
2023-10-04 05:44:37 +0500

    updated files

--- leetcode/design-hashmap.py
+++ leetcode/design-hashmap.py
@@ -14,7 +14,9 @@ class MyHashMap(dict):
     def remove(s,k):
         s.pop(k,0)
 
-MyHashMap=type('',(dict,),{'put':dict.__setitem__,'get':lambda s,k:dict.get(s,k,-1),'remove':lambda s,k:s.pop(k,None)and None})
+MyHashMap=type('',(dict,),{'put':dict.__setitem__,'get':lambda s,k:dict.get(s,k,-1),'remove':lambda s,k:s.pop(k,1)and None})
+
+MyHashMap=type('',(dict,),{'put':dict.__setitem__,'get':lambda s,k:dict.get(s,k,-1),'remove':lambda s,k:s.put(k,-1)})
 
 test('''
 706. Design HashMap

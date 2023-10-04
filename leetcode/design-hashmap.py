9a728fa3262bbafff753fdb0f4a414c7528584de
joric <joric@users.noreply.github.com>
2023-10-04 07:34:03 +0500

    updated files

--- leetcode/design-hashmap.py
+++ leetcode/design-hashmap.py
@@ -1,10 +1,14 @@
 from lc import *
 
 class MyHashMap:
-    def __init__(self): self.map = [None]*10000000
-    def put(self, key: int, value: int) -> None: self.map[key] = value            
-    def get(self, key: int) -> int: return self.map[key] if self.map[key]!=None else -1
-    def remove(self, key: int) -> None: self.map[key] = None
+    def __init__(self):
+        self.map = [None]*10000000
+    def put(self, key: int, value: int) -> None:
+        self.map[key] = value
+    def get(self, key: int) -> int:
+        return self.map[key] if self.map[key]!=None else -1
+    def remove(self, key: int) -> None:
+        self.map[key] = None
 
 class MyHashMap(dict): 
     def put(s,k,v):

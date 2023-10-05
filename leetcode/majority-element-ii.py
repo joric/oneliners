1d5e286e77e4190cbb5334d27be143540d5e52f7
joric <joric@users.noreply.github.com>
2023-10-05 15:14:19 +0500

    updated files

--- leetcode/majority-element-ii.py
+++ leetcode/majority-element-ii.py
@@ -21,6 +21,10 @@ class Solution:
         return [n for n in (candidate1, candidate2)
                         if nums.count(n) > len(nums) // 3] 
 
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        c1,c2,v1,v2=0,0,0,1;all((c1:=c1+1)if n==v1 else(c2:=c2+1)if n==v2 else (c1:=1,v1:=n)if c1==0 else(c2:=1,v2:=n)if c2==0 else(c1:=c1-1,c2:=c2-1)for n in nums);return[n for n in(v1,v2)if nums.count(n)>len(nums)//3]
+
 # fast
 class Solution:
     def majorityElement(self, a: List[int]) -> List[int]:

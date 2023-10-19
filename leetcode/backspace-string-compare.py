877057fa6cc9902e72835a0ab14095e89735779b
joric <joric@users.noreply.github.com>
2023-10-19 09:23:16 +0500

    updated files

--- leetcode/backspace-string-compare.py
+++ leetcode/backspace-string-compare.py
@@ -33,17 +33,23 @@ Example 1:
 Input: s = "ab#c", t = "ad#c"
 Output: true
 Explanation: Both s and t become "ac".
+
 Example 2:
 
 Input: s = "ab##", t = "c#d#"
 Output: true
 Explanation: Both s and t become "".
+
 Example 3:
 
 Input: s = "a#c", t = "b"
 Output: false
 Explanation: s becomes "c" while t becomes "b".
- 
+
+Example 4:
+
+Input: s = "a##c", t = "#a#c"
+Output: true
 
 Constraints:
 

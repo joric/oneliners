b5c1840fd5c95eecdf70d119c8514ff4ae1da1ba
joric <joric@users.noreply.github.com>
2023-10-21 09:46:50 +0500

    updated files

--- leetcode/constrained-subsequence-sum.py
+++ leetcode/constrained-subsequence-sum.py
@@ -1,5 +1,7 @@
 from lc import *
 
+# https://leetcode.com/problems/constrained-subsequence-sum/discuss/597751/JavaC%2B%2BPython-O(N)-Decreasing-Deque
+
 class Solution:
     def constrainedSubsetSum(self, a: List[int], k: int) -> int:
         d = deque()

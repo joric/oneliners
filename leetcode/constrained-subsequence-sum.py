941fc1240e552741fad585fd0ef49ddc220d56c8
joric <joric@users.noreply.github.com>
2023-10-21 14:45:18 +0500

    updated files

--- leetcode/constrained-subsequence-sum.py
+++ leetcode/constrained-subsequence-sum.py
@@ -1,23 +1,10 @@
 from lc import *
 
-# https://leetcode.com/problems/constrained-subsequence-sum/discuss/597751/JavaC%2B%2BPython-O(N)-Decreasing-Deque
-
+# https://leetcode.com/problems/constrained-subsequence-sum/discuss/736798/Python-Thought-process%3A-Recursion-Memoization-Tabulation-Monoqueue
+# TLE
 class Solution:
     def constrainedSubsetSum(self, a: List[int], k: int) -> int:
-        d = deque()
-        for i in range(len(a)):
-            a[i] += d[0] if d else 0
-            while d and a[i]>d[-1]:
-                d.pop()
-            if a[i]>0:
-                d.append(a[i])
-            if i>=k and d and d[0]==a[i-k]:
-                d.popleft()
-        return max(a)
-
-class Solution:
-    def constrainedSubsetSum(self, a: List[int], k: int) -> int:
-        d=deque();[(setitem(a,i,a[i]+(d and d[0]or 0)),all(d and a[i]>d[-1]and d.pop()for _ in a),a[i]>0and d.append(a[i]),i>=k and d and d[0]==a[i-k]and d.popleft())for i in range(len(a))];return max(a)
+        return max(map(f:=cache(lambda j:j>=0 and a[j]+max(0,max(f(j-i)for i in range(1,k+1)))),range(len(a))))
 
 # another solution
 
@@ -42,6 +29,26 @@ class Solution:
 class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
         l,n,s=0,len(nums),[(-k-1,0)];return max(((s[l][0]+k>=i or(l:=l+1))and(d:=nums[i]+max(0,l<len(s)and s[l][1])),all(s[-1][1]<=d and s.pop()for _ in range(l,len(s))),s.append((i,d)))[0]for i in range(n))
+
+# https://leetcode.com/problems/constrained-subsequence-sum/discuss/597751/JavaC%2B%2BPython-O(N)-Decreasing-Deque
+
+class Solution:
+    def constrainedSubsetSum(self, a: List[int], k: int) -> int:
+        d = deque()
+        for i in range(len(a)):
+            a[i] += d[0] if d else 0
+            while d and a[i]>d[-1]:
+                d.pop()
+            if a[i]>0:
+                d.append(a[i])
+            if i>=k and d and d[0]==a[i-k]:
+                d.popleft()
+        return max(a)
+
+class Solution:
+    def constrainedSubsetSum(self, a: List[int], k: int) -> int:
+        d=deque();[(setitem(a,i,a[i]+(d and d[0]or 0)),all(d and a[i]>d[-1]and d.pop()for _ in a),a[i]>0and d.append(a[i]),i>=k and d and d[0]==a[i-k]and d.popleft())for i in range(len(a))];return max(a)
+
 
 test('''
 1425. Constrained Subsequence Sum

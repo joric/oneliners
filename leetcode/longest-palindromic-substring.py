0348eda04c7d12ec178a128394f966e01db4c644
joric <joric@users.noreply.github.com>
2023-10-27 17:03:04 +0500

    updated files

--- leetcode/longest-palindromic-substring.py
+++ leetcode/longest-palindromic-substring.py
@@ -24,9 +24,10 @@ class Solution:
         return ans
 
 # Manacher algorithm http://en.wikipedia.org/wiki/Longest_palindromic_substring
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        t = '#'.join('^{}$'.format(s))
+        t = '#'.join(f'^{s}$')
         n = len(t)
         p = [0]*n
         c = r = 0
@@ -42,9 +43,33 @@ class Solution:
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        return (t:='#'.join('^{}$'.format(s)),n:=len(t),p:=[0]*n,c:=0,r:=0,[(setitem(p,i,(r>i) and min(r-i,p[2*c-i])),next(0 for _ in count() if not(t[i+1+p[i]]==t[i-1-p[i]] and (setitem(p,i,p[i]+1),i+p[i]>r and (c:=i,r:=i+p[i]))))) for i in range (1,n-1)],v:=max((n,i) for i, n in enumerate(p)),s[(v[1]-v[0])//2:sum(v)//2])[-1]
+        return(t:='#'.join(f'^{s}$'),n:=len(t),p:=[0]*n,c:=0,r:=0,[(setitem(p,i,(r>i)and min(r-i,p[2*c-i])),next(0 for _ in count()if not(t[i+1+p[i]]==t[i-1-p[i]]and(setitem(p,i,p[i]+1),i+p[i]>r and(c:=i,r:=i+p[i])))))for i in range (1,n-1)],v:=max((n,i)for i,n in enumerate(p)),s[(v[1]-v[0])//2:sum(v)//2])[-1]
 
-# https://leetcode.com/problems/longest-palindromic-substring
+
+# https://leetcode.com/problems/longest-palindromic-substring/discuss/2928/Very-simple-clean-java-solution
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        r = [0,0]
+        if n<2:
+            return s
+        def f(j,k):
+            while j>=0 and k<n and s[j]==s[k]:
+                j-=1
+                k+=1
+            if r[1]<k-j-1:
+                r[:]=[j+1,k-j-1]
+        for i in range(n-1):
+            f(i,i)
+            f(i,i+1)
+        return s[r[0]:r[0]+r[1]]
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n,r=len(s),[0,1];f=lambda j,k:(all(j>=0 and k<n and s[j]==s[k]and(j:=j-1,k:=k+1)for _ in s),r[1]<k-j-1and setitem(r,slice(0,1),[j+1,k-j-1]));[(f(i,i),f(i,i+1))for i in range(n-1)];return s[r[0]:r[0]+r[1]]
+
+# https://leetcode.com/problems/longest-palindromic-substring/discuss/484124/Shortest-and-easy-understandable-but-Slow-Python
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:

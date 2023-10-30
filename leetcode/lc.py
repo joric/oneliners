abb5d5d182f9624291f1976a127562d6f83eb5eb
joric <joric@users.noreply.github.com>
2023-10-30 05:27:11 +0500

    updated files

--- leetcode/lc.py
+++ leetcode/lc.py
@@ -261,7 +261,7 @@ def test(text=None, classname=None, check=None, init=None, parser=None, custom=N
             elif s.startswith('Output:'):
                 tests[-1]['input'] = split_vars(p)
                 p,t = s[8:],2
-            elif t==2 and (s.startswith('Example') or s.startswith('Explanation') or s.startswith('//') or s==''):
+            elif t==2 and (s.startswith('Example') or s.startswith('Explan') or s.startswith('//') or s==''):
                 tests[-1]['output'] = split_vars(p)
                 p,t = '',0
             elif t != 0:

87e06b5bfbe3355cb1cb5070f1a8a945e882c92b
joric <joric@users.noreply.github.com>
2023-11-01 06:12:38 +0500

    updated files

--- leetcode/find-mode-in-binary-search-tree.py
+++ leetcode/find-mode-in-binary-search-tree.py
@@ -1,5 +1,49 @@
 from lc import *
 
+# editorial (boyer-moore + morris traversal) https://leetcode.com/problems/find-mode-in-binary-search-tree/solution
+
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        def add(num):
+            nonlocal max_streak, curr_streak, curr_num, ans
+            if num == curr_num:
+                curr_streak += 1
+            else:
+                curr_streak = 1
+                curr_num = num
+
+            if curr_streak > max_streak:
+                ans = []
+                max_streak = curr_streak
+
+            if curr_streak == max_streak:
+                ans.append(num)
+
+        max_streak = 0
+        curr_streak = 0
+        curr_num = 0
+        ans = []
+        
+        curr = root
+        while curr:
+            if curr.left:
+                friend = curr.left
+                while friend.right and friend.right != curr:
+                    friend = friend.right
+                
+                if not friend.right:
+                    friend.right = curr
+                    curr = curr.left
+                else:
+                    friend.right = None
+                    add(curr.val)
+                    curr = curr.right
+            else:
+                add(curr.val)
+                curr = curr.right
+        
+        return ans
+
 # https://leetcode.com/problems/find-mode-in-binary-search-tree/discuss/2235862/Python-or-One-Pass-or-O(n)
 
 class Solution:

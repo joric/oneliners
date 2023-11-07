55767343b079fd60867bec8651521cdeb18fdcfb
joric <joric@users.noreply.github.com>
2023-11-07 17:40:49 +0500

    updated files

--- leetcode/eliminate-maximum-number-of-monsters.py
+++ leetcode/eliminate-maximum-number-of-monsters.py
@@ -34,6 +34,12 @@ class Solution:
     def eliminateMaximum(self, d: List[int], s: List[int]) -> int:
         return next((i for i,t in enumerate(sorted(map(truediv,d,s)))if i>=t),len(d))
 
+# another solution
+
+class Solution:
+    def eliminateMaximum(self, d: List[int], s: List[int]) -> int:
+        return len([*takewhile(lambda t:lt(*t),enumerate(sorted(map(truediv,d,s))))])
+
 test('''
 1921. Eliminate Maximum Number of Monsters
 Medium

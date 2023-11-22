85c326f8607b4cfff980334b9cfab78ec581dee1
joric <joric@users.noreply.github.com>
2023-11-22 14:20:21 +0500

    updated files

---
+++ leetcode/diagonal-traverse-ii.py
@@ -0,0 +1,64 @@
+from lc import *
+
+# https://leetcode.com/problems/diagonal-traverse-ii/discuss/597794/Python-One-pass
+
+class Solution:
+    def findDiagonalOrder(self, n: List[List[int]]) -> List[int]:
+        res = []
+        for i, r in enumerate(n):
+            for j, a in enumerate(r):
+                if len(res) <= i + j:
+                    res.append([])
+                res[i + j].append(a)
+        return [a for r in res for a in reversed(r)]
+
+class Solution:
+    def findDiagonalOrder(self, n: List[List[int]]) -> List[int]:
+        d=[];[(i+j>=len(d)and d.append([]),d[i+j].append(a))for i,r in enumerate(n)for j,a in enumerate(r)];return[a for r in d for a in reversed(r)]
+
+class Solution:
+    def findDiagonalOrder(self, n: List[List[int]]) -> List[int]:
+        d=defaultdict(list);[d[i+j].append(a)for i,r in enumerate(n)for j,a in enumerate(r)];return[a for r in d for a in d[r][::-1]]
+
+class Solution:
+    def findDiagonalOrder(self, n: List[List[int]]) -> List[int]:
+        d=defaultdict(list);[d[i+j].append(a)for i,r in enumerate(n)for j,a in enumerate(r)];return chain(*map(reversed,d.values()))
+
+test('''
+1424. Diagonal Traverse II
+Medium
+
+1495
+
+120
+
+Add to List
+
+Share
+Given a 2D integer array nums, return all elements of nums in diagonal order as shown in the below images.
+
+ 
+
+Example 1:
+
+
+Input: nums = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,4,2,7,5,3,8,6,9]
+Example 2:
+
+
+Input: nums = [[1,2,3,4,5],[6,7],[8],[9,10,11],[12,13,14,15,16]]
+Output: [1,6,2,8,7,3,9,4,12,10,5,13,11,14,15,16]
+ 
+
+Constraints:
+
+1 <= nums.length <= 10^5
+1 <= nums[i].length <= 10^5
+1 <= sum(nums[i].length) <= 10^5
+1 <= nums[i][j] <= 10^5
+Accepted
+76,028
+Submissions
+141,667
+''')

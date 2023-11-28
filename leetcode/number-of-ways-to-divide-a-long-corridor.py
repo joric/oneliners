8cba261fa42b7142a2515783dd3feceb34f21e03
joric <joric@users.noreply.github.com>
2023-11-29 03:35:01 +0500

    updated files

--- leetcode/number-of-ways-to-divide-a-long-corridor.py
+++ leetcode/number-of-ways-to-divide-a-long-corridor.py
@@ -31,6 +31,10 @@ class Solution:
     def numberOfWays(self, s: str) -> int:
         return reduce(lambda p,i:(p[2]|p[1]*(i-p[3])%(10**9+7),p[0]&p[2]-1,0,i),(i for i,c in enumerate(s)if c>'P'),(1,0)*2)[1]
 
+class Solution:
+    def numberOfWays(self, s: str) -> int:
+        p=(1,0)*2;[p:=(p[2]|p[1]*(i-p[3])%(10**9+7),p[0]&p[2]-1,0,i)for i,c in enumerate(s)if c>'P'];return p[1]
+
 test('''
 2147. Number of Ways to Divide a Long Corridor
 Hard

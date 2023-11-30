b93e5397a38a1aef84406f6ca5d860681347b97c
joric <joric@users.noreply.github.com>
2023-11-30 05:11:46 +0500

    updated files

---
+++ leetcode/minimum-one-bit-operations-to-make-integers-zero.py
@@ -0,0 +1,67 @@
+from lc import *
+
+# https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/discuss/1582821/Python-28ms-crazy-formula-2-Lines
+
+class Solution:
+    def minimumOneBitOperations(self, n: int) -> int:
+        return sum(s*(1,-1)[i&1]for i,s in enumerate((1<<b+1)-1 for b in range(30,-1,-1)if(1<<b)&n))
+
+# https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/discuss/877738/5-Line-Python-or-Simple-or-O(logn)
+
+class Solution:
+    def minimumOneBitOperations(self, n: int) -> int:
+        r = 0
+        while n:
+            r ^= n
+            n //= 2
+        return r
+
+class Solution:
+    def minimumOneBitOperations(self, n: int) -> int:
+        return next((r for _ in count()if not(n and(r:=r^n,n:=n//2))),r:=0)
+
+test('''
+1611. Minimum One Bit Operations to Make Integers Zero
+Hard
+
+442
+
+364
+
+Add to List
+
+Share
+Given an integer n, you must transform it into 0 using the following operations any number of times:
+
+Change the rightmost (0th) bit in the binary representation of n.
+Change the ith bit in the binary representation of n if the (i-1)th bit is set to 1 and the (i-2)th through 0th bits are set to 0.
+Return the minimum number of operations to transform n into 0.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: 2
+Explanation: The binary representation of 3 is "11".
+"11" -> "01" with the 2nd operation since the 0th bit is 1.
+"01" -> "00" with the 1st operation.
+Example 2:
+
+Input: n = 6
+Output: 4
+Explanation: The binary representation of 6 is "110".
+"110" -> "010" with the 2nd operation since the 1st bit is 1 and 0th through 0th bits are 0.
+"010" -> "011" with the 1st operation.
+"011" -> "001" with the 2nd operation since the 0th bit is 1.
+"001" -> "000" with the 1st operation.
+ 
+
+Constraints:
+
+0 <= n <= 10^9
+Accepted
+14,523
+Submissions
+22,882
+''')

7bb7577c7aee0f23667e6491c5f67cd9606c3f03
joric <joric@users.noreply.github.com>
2023-11-30 05:24:20 +0500

    updated files

--- leetcode/minimum-one-bit-operations-to-make-integers-zero.py
+++ leetcode/minimum-one-bit-operations-to-make-integers-zero.py
@@ -18,6 +18,10 @@ class Solution:
     def minimumOneBitOperations(self, n: int) -> int:
         r=[0];exec('while n:\n r[0]^=n\n n//=2');return r[0]
 
+class Solution:
+    def minimumOneBitOperations(self, n: int) -> int:
+        return(f:=lambda r,n:n and f(r^n,n//2)or r)(0,n)
+
 test('''
 1611. Minimum One Bit Operations to Make Integers Zero
 Hard

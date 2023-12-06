bebb1ee3a5142a7568738340acc50849a406a783
joric <joric@users.noreply.github.com>
2023-12-06 09:44:15 +0500

    updated files

--- leetcode/calculate-money-in-leetcode-bank.py
+++ leetcode/calculate-money-in-leetcode-bank.py
@@ -31,7 +31,7 @@ class Solution:
 
 class Solution:
     def totalMoney(self, n: int) -> int:
-        return sum(1+i//7+i%7for i in range(n))
+        return sum(i//7+i%7+1for i in range(n))
 
 test('''
 1716. Calculate Money in Leetcode Bank

3e9b401636c31a80ebe6014675eed0992fe5550a
joric <joric@users.noreply.github.com>
2023-12-24 23:09:00 +0500

    updated files

--- leetcode/string-compression.py
+++ leetcode/string-compression.py
@@ -40,18 +40,21 @@ Example 1:
 
 Input: chars = ["a","a","b","b","c","c","c"]
 Output: ["a","2","b","2","c","3"]
+
 //Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
 Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
 Example 2:
 
 Input: chars = ["a"]
 Output: ["a"]
+
 //Output: Return 1, and the first character of the input array should be: ["a"]
 Explanation: The only group is "a", which remains uncompressed since it's a single character.
 Example 3:
 
 Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
 Output: ["a","b","1","2"]
+
 //Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
 Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
  

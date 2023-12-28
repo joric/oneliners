c7ca75202569263b15976b6960614ee8d08e874e
joric <joric@users.noreply.github.com>
2023-12-28 05:02:46 +0500

    updated files

--- leetcode/string-compression-ii.py
+++ leetcode/string-compression-ii.py
@@ -42,7 +42,7 @@ class Solution:
 
 class Solution:
     def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
-        return (f:=cache(lambda i,k,p,c:inf if k<0 else 0 if i==len(s) else min(f(i+1,k-1,p,c),f(i+1,k,s[i],c+1)+int(c in [1,9,99])) if p==s[i] else min(f(i+1,k-1,p,c), f(i+1,k,s[i],1)+1)))(0,k,'',0)
+        return(f:=cache(lambda i,k,p,c:inf if k<0 else 0 if i==len(s)else min(f(i+1,k-1,p,c),f(i+1,k,s[i],c+1)+int(c in[1,9,99]))if p==s[i]else min(f(i+1,k-1,p,c),f(i+1,k,s[i],1)+1)))(0,k,'',0)
 
 test('''
 Run-length encoding is a string compression method that works by replacing consecutive identical characters (repeated 2 or more times) with the concatenation of the character and the number marking the count of the characters (length of the run). For example, to compress the string "aabccc" we replace "aa" by "a2" and replace "ccc" by "c3". Thus the compressed string becomes "a2bc3".

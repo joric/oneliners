6f3a4b65aec2e3927be27379b8a533661d6dfd92
joric <joric@users.noreply.github.com>
2024-01-05 10:36:33 +0500

    updated files

--- leetcode/longest-increasing-subsequence.py
+++ leetcode/longest-increasing-subsequence.py
@@ -19,6 +19,10 @@ class Solution:
 
 class Solution:
     def lengthOfLIS(self, n: List[int]) -> int:
+        d=[];[setitem(d,i,x)if(i:=bisect_left(d,x))<len(d)else d.append(x)for x in n];return len(d)
+
+class Solution:
+    def lengthOfLIS(self, n: List[int]) -> int:
         d=[];[setitem(d,slice(i:=bisect_left(d,x),i+1),[x])for x in n];return len(d)
 
 class Solution:

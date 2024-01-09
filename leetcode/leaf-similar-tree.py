69264ac9ff96d8d85b6a5dfc31bdb6ea58c496b3
joric <joric@users.noreply.github.com>
2024-01-09 05:32:16 +0500

    updated files

--- leetcode/leaf-similar-tree.py
+++ leetcode/leaf-similar-tree.py
@@ -33,7 +33,11 @@ class Solution:
 
 class Solution:
     def leafSimilar(self, a: Optional[TreeNode], b: Optional[TreeNode]) -> bool:
-        return(f:=lambda r:(f(r.left)+f(r.right)if(r.left or r.right)else[r.val])if r else[])(a)==f(b)
+        return(f:=lambda r:r and(f(r.left)+f(r.right)if(r.left or r.right)else[r.val])or[])(a)==f(b)
+
+class Solution:
+    def leafSimilar(self, a: Optional[TreeNode], b: Optional[TreeNode]) -> bool:
+        return(f:=lambda r:r and(f(r.left)+f(r.right)if r.left or r.right else[r.val])or[])(a)==f(b)
 
 test('''
 

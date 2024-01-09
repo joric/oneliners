78f84b23feb365fb78a13e37c7a4c0dd29b786a4
joric <joric@users.noreply.github.com>
2024-01-09 05:25:33 +0500

    updated files

--- leetcode/leaf-similar-tree.py
+++ leetcode/leaf-similar-tree.py
@@ -2,7 +2,38 @@ from lc import *
 
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        return (f:=lambda l,r:l if not r else l+[r.val] if not r.left and not r.right else f(f(l,r.left),r.right))([],root1)==f([],root2)
+        return(f:=lambda l,r:l if not r else l+[r.val] if not r.left and not r.right else f(f(l,r.left),r.right))([],root1)==f([],root2)
+
+class Solution:
+    def leafSimilar(self, a: Optional[TreeNode], b: Optional[TreeNode]) -> bool:
+        return(f:=lambda l,r:l if not r else l+[r.val] if not (r.left or r.right) else f(f(l,r.left),r.right))([],a)==f([],b)
+
+
+class Solution:
+    def leafSimilar(self, a: Optional[TreeNode], b: Optional[TreeNode]) -> bool:
+        return(f:=lambda l,r:l if not r else l+[r.val] if not (r.left or r.right) else f(f(l,r.left),r.right))([],a)==f([],b)
+
+# https://leetcode.com/problems/leaf-similar-trees/discuss/152873/4-line-Python-Solution
+
+class Solution:
+    def leafSimilar(self, a: Optional[TreeNode], b: Optional[TreeNode]) -> bool:
+        def f(r):
+            if not r:
+                return []
+            if not (r.left or r.right):
+                return [r.val]
+            return f(r.left)+f(r.right)
+        return f(a)==f(b)
+
+class Solution:
+    def leafSimilar(self, a: Optional[TreeNode], b: Optional[TreeNode]) -> bool:
+        def f(r):
+            return []if not r else f(r.left)+f(r.right)if(r.left or r.right)else[r.val]
+        return f(a)==f(b)
+
+class Solution:
+    def leafSimilar(self, a: Optional[TreeNode], b: Optional[TreeNode]) -> bool:
+        return(f:=lambda r:(f(r.left)+f(r.right)if(r.left or r.right)else[r.val])if r else[])(a)==f(b)
 
 test('''
 
@@ -25,15 +56,18 @@ Return true if and only if the two given trees with head nodes root1 and root2 a
 
 Example 1:
 
-
 Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
 Output: true
-Example 2:
 
+Example 2:
 
 Input: root1 = [1,2,3], root2 = [1,3,2]
 Output: false
- 
+
+Example 3:
+
+Input: root1 = [1,2], root2 = [2,2]
+Output: true
 
 Constraints:
 

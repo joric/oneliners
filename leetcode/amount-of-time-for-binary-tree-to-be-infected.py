c47b64a358642b36d163c669e77e6b32a800888d
joric <joric@users.noreply.github.com>
2024-01-10 05:35:14 +0500

    updated files

--- leetcode/amount-of-time-for-binary-tree-to-be-infected.py
+++ leetcode/amount-of-time-for-binary-tree-to-be-infected.py
@@ -4,20 +4,20 @@ from lc import *
 
 # TODO
 class Solution:
-    def amountOfTime(self, r: Optional[TreeNode], s: int) -> int:
-        def f(n):
-            if n is None:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        def f(root):
+            if root is None:
                 return None, 0
-            a,l = f(n.left)
-            b,r = f(n.right)
-            if n.val == s:
+            node_l, l = f(root.left)
+            node_r, r = f(root.right)
+            if root.val == start:
                 return 0, max(l, r)
-            if a:
-                return a+1,max(a+1,l,a+1+r)
-            if b:
-                return b+1,max(b+1,r,b+1+l)
-            return None, max(l,r)+1
-        return f(r)[1]
+            if node_l is not None:
+                return node_l + 1, max(node_l + 1, l, node_l + 1 + r)
+            if node_r is not None:
+                return node_r + 1, max(node_r + 1, r, node_r + 1 + l)
+            return None, max(l, r) + 1
+        return f(root)[1]
 
 test('''
 2385. Amount of Time for Binary Tree to Be Infected

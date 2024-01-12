2559ffbe32d55cf8d533e936130ba0867ace9b4a
joric <joric@users.noreply.github.com>
2024-01-12 13:32:25 +0500

    updated files

--- leetcode/determine-if-string-halves-are-alike.py
+++ leetcode/determine-if-string-halves-are-alike.py
@@ -4,11 +4,46 @@ from lc import *
 
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
+        vowels = set('aeiouAEIOU')
+        a = b = 0
+        i, j = 0, len(s) - 1
+        while i < j:
+            a += s[i] in vowels
+            b += s[j] in vowels
+            i += 1
+            j -= 1
+        return a == b 
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
         return(v:=set('aeiouAEIOU'))and sum((s[i] in v)-(s[~i] in v) for i in range(len(s)//2))==0
 
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
         v=set('aeiouAEIOU');return sum((s[i]in v)-(s[~i]in v)for i in range(len(s)//2))==0
+
+# https://leetcode.com/problems/determine-if-string-halves-are-alike/discuss/1173214/Python-Simple-Zip-Loop
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        left = right = 0
+        vowels = {'a','e','i','o','u','A', 'E','I','O','U'}
+        for (x,y) in zip(s[:len(s)//2], s[len(s)//2:]):
+            if x in vowels:
+                left+=1
+            if y in vowels:
+                right+=1
+        return left == right
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        n,v=len(s)//2,set('aeiouAEIOU');return sum(1 for c in s[:n]if c in v)==sum(1 for c in s[n:]if c in v)
+
+# https://leetcode.com/problems/determine-if-string-halves-are-alike/discuss/2864237/one-line-in-Python
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        return sum((i<len(s)//2)*2-1for i,c in enumerate(s.lower())if c in'aeiou')==0
 
 test('''
 

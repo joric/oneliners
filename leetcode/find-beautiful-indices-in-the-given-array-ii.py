edcc5ec8973ae324d52a4d6168d22533b0761c71
joric <joric@users.noreply.github.com>
2024-01-14 18:42:02 +0500

    updated files

--- leetcode/find-beautiful-indices-in-the-given-array-ii.py
+++ leetcode/find-beautiful-indices-in-the-given-array-ii.py
@@ -67,12 +67,12 @@ class Solution:
         f=lambda s:((p:=[0]*len(s),[setitem(p,i,next((c+(s[i]==s[c])for _ in count() if not(c and s[i]!=s[c]and(c:=p[c-1]))),c:=p[i-1]))for i in range(1,len(s))])and p);v,w=[[i-len(w)*2 for i,x in enumerate(f(w+'#'+s))if x>=len(w)]for w in (a,b)];p={x:bisect_left(w,x)for x in v};return sorted({x for x in v for j in(p[x],p[x]-1)if 0<=j<len(w)and abs(w[j]-x)<=k})
 
 test('''
-3006. Find Beautiful Indices in the Given Array I
-Medium
+3008. Find Beautiful Indices in the Given Array II
+Hard
 
-29
+43
 
-10
+8
 
 Add to List
 
@@ -90,6 +90,33 @@ s[j..(j + b.length - 1)] == b
 Return the array that contains beautiful indices in sorted order from smallest to largest.
 
  
+
+Example 1:
+
+Input: s = "isawsquirrelnearmysquirrelhouseohmy", a = "my", b = "squirrel", k = 15
+Output: [16,33]
+Explanation: There are 2 beautiful indices: [16,33].
+- The index 16 is beautiful as s[16..17] == "my" and there exists an index 4 with s[4..11] == "squirrel" and |16 - 4| <= 15.
+- The index 33 is beautiful as s[33..34] == "my" and there exists an index 18 with s[18..25] == "squirrel" and |33 - 18| <= 15.
+Thus we return [16,33] as the result.
+Example 2:
+
+Input: s = "abcd", a = "a", b = "a", k = 4
+Output: [0]
+Explanation: There is 1 beautiful index: [0].
+- The index 0 is beautiful as s[0..0] == "a" and there exists an index 0 with s[0..0] == "a" and |0 - 0| <= 4.
+Thus we return [0] as the result.
+ 
+
+Constraints:
+
+1 <= k <= s.length <= 5 * 105
+1 <= a.length, b.length <= 5 * 105
+s, a, and b contain only lowercase English letters.
+Accepted
+3,868
+Submissions
+24,301
 
 Example 1:
 
@@ -120,11 +147,11 @@ Output: [6,11,13]
 
 Constraints:
 
-1 <= k <= s.length <= 10^5
-1 <= a.length, b.length <= 10
+1 <= k <= s.length <= 5 * 10^5
+1 <= a.length, b.length <= 5 * 10^5
 s, a, and b contain only lowercase English letters.
 Accepted
-13,326
+3,868
 Submissions
-41,633
+24,301
 ''')

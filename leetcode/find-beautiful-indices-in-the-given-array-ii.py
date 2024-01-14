2368445595682deb1d9033ce5ef240e64006e28a
joric <joric@users.noreply.github.com>
2024-01-14 19:42:29 +0500

    updated files

--- leetcode/find-beautiful-indices-in-the-given-array-ii.py
+++ leetcode/find-beautiful-indices-in-the-given-array-ii.py
@@ -66,13 +66,36 @@ class Solution:
     def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
         f=lambda s:((p:=[0]*len(s),[setitem(p,i,next((c+(s[i]==s[c])for _ in count() if not(c and s[i]!=s[c]and(c:=p[c-1]))),c:=p[i-1]))for i in range(1,len(s))])and p);v,w=[[i-len(w)*2 for i,x in enumerate(f(w+'#'+s))if x>=len(w)]for w in (a,b)];p={x:bisect_left(w,x)for x in v};return sorted({x for x in v for j in(p[x],p[x]-1)if 0<=j<len(w)and abs(w[j]-x)<=k})
 
+class Solution:
+    def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
+        def kmp(s):
+            p = [0] * len(s)
+            for i in range(1, len(s)):
+                c = p[i - 1]
+                while c and s[i] != s[c]:
+                    c = p[c - 1]
+                p[i] = c + (s[i] == s[c])
+            return p
+        v,w = [[i-len(w)*2 for i,x in enumerate(kmp(w+'#'+s))if x>=len(w)]for w in(a,b)]
+        return[x for x in v if(lambda i,x:any(0<=i<len(w)and abs(w[i]-x)<=k for i in(i,i-1)))(bisect_left(w,x),x)]
+
+class Solution:
+    def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
+        f=lambda s:((p:=[0]*len(s),[setitem(p,i,next((c+(s[i]==s[c])for _ in count()if not(c and s[i]!=s[c]and(c:=p[c-1]))),c:=p[i-1]))for i in range(1,len(s))])and p)
+        v,w=[[i-len(w)*2for i,x in enumerate(f(w+'#'+s))if x>=len(w)]for w in(a,b)]
+        return[x for x in v if(lambda i,x:any(0<=i<len(w)and abs(w[i]-x)<=k for i in(i,i-1)))(bisect_left(w,x),x)]
+
+class Solution:
+    def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
+        f=lambda s:((p:=[0]*len(s),[setitem(p,i,next((c+(s[i]==s[c])for _ in count()if not(c and s[i]!=s[c]and(c:=p[c-1]))),c:=p[i-1]))for i in range(1,len(s))])and p);v,w=[[i-len(w)*2for i,x in enumerate(f(w+'#'+s))if x>=len(w)]for w in(a,b)];return[x for x in v if(lambda i,x:any(0<=i<len(w)and abs(w[i]-x)<=k for i in(i,i-1)))(bisect_left(w,x),x)]
+
 test('''
-3008. Find Beautiful Indices in the Given Array II
-Hard
+3006. Find Beautiful Indices in the Given Array I
+Medium
 
-43
+29
 
-8
+10
 
 Add to List
 
@@ -90,33 +113,6 @@ s[j..(j + b.length - 1)] == b
 Return the array that contains beautiful indices in sorted order from smallest to largest.
 
  
-
-Example 1:
-
-Input: s = "isawsquirrelnearmysquirrelhouseohmy", a = "my", b = "squirrel", k = 15
-Output: [16,33]
-Explanation: There are 2 beautiful indices: [16,33].
-- The index 16 is beautiful as s[16..17] == "my" and there exists an index 4 with s[4..11] == "squirrel" and |16 - 4| <= 15.
-- The index 33 is beautiful as s[33..34] == "my" and there exists an index 18 with s[18..25] == "squirrel" and |33 - 18| <= 15.
-Thus we return [16,33] as the result.
-Example 2:
-
-Input: s = "abcd", a = "a", b = "a", k = 4
-Output: [0]
-Explanation: There is 1 beautiful index: [0].
-- The index 0 is beautiful as s[0..0] == "a" and there exists an index 0 with s[0..0] == "a" and |0 - 0| <= 4.
-Thus we return [0] as the result.
- 
-
-Constraints:
-
-1 <= k <= s.length <= 5 * 105
-1 <= a.length, b.length <= 5 * 105
-s, a, and b contain only lowercase English letters.
-Accepted
-3,868
-Submissions
-24,301
 
 Example 1:
 
@@ -147,11 +143,11 @@ Output: [6,11,13]
 
 Constraints:
 
-1 <= k <= s.length <= 5 * 10^5
-1 <= a.length, b.length <= 5 * 10^5
+1 <= k <= s.length <= 10^5
+1 <= a.length, b.length <= 10
 s, a, and b contain only lowercase English letters.
 Accepted
-3,868
+13,326
 Submissions
-24,301
+41,633
 ''')

3862116d81ba1f3ae0babcb5e1a5f30d5d4229ca
joric <joric@users.noreply.github.com>
2024-01-15 05:12:11 +0500

    updated files

--- leetcode/find-players-with-zero-or-one-losses.py
+++ leetcode/find-players-with-zero-or-one-losses.py
@@ -8,7 +8,7 @@ class Solution:
 
 class Solution:
     def findWinners(self, m: List[List[int]]) -> List[List[int]]:
-        a,b=zip(*m);return sorted({*a}-{*b}),sorted(k for k,v in Counter(b).items()if v<2)
+        a,b=zip(*m);return map(sorted,({*a}-{*b},(k for k,v in Counter(b).items()if v<2)))
 
 test('''
 2225. Find Players With Zero or One Losses

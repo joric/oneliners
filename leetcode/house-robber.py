dd772d4e10da07f3eeba44864eb1b4abad9a13df
joric <joric@users.noreply.github.com>
2024-01-21 05:40:40 +0500

    updated files

--- leetcode/house-robber.py
+++ leetcode/house-robber.py
@@ -12,9 +12,7 @@ class Solution:
     def rob(self, n: List[int]) -> int:
         a,b = 0,0
         for i in n:
-            t = max(i+a,b)
-            a = b
-            b = t
+            a,b = b,max(i+a,b)
         return b
 
 class Solution:

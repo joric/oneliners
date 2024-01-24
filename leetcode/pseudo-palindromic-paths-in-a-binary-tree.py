4d87e4ec1e53b894ee262473ff59f0510e0c85ac
joric <joric@users.noreply.github.com>
2024-01-24 20:57:29 +0500

    updated files

--- leetcode/pseudo-palindromic-paths-in-a-binary-tree.py
+++ leetcode/pseudo-palindromic-paths-in-a-binary-tree.py
@@ -4,7 +4,7 @@ from lc import *
 
 class Solution:
     def pseudoPalindromicPaths (self, t: Optional[TreeNode]) -> int:
-        return(f:=lambda t,c:t and f(l:=t.left,c:=c^1<<(t.val-1))+f(r:=t.right,c)+(l==r and c&(c-1)<1)or 0)(t,0)
+        return(f:=lambda t,c:t and f(l:=t.left,c:=c^1<<(t.val-1))+f(r:=t.right,c)+(l==r and c&(c-1)==0)or 0)(t,0)
 
 class Solution:
     def pseudoPalindromicPaths (self, t: Optional[TreeNode]) -> int:

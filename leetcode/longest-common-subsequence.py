fe10fbc00c4aae4b9f13958033f6985c82ceaaeb
joric <joric@users.noreply.github.com>
2024-01-25 06:30:00 +0500

    updated files

--- leetcode/longest-common-subsequence.py
+++ leetcode/longest-common-subsequence.py
@@ -42,7 +42,7 @@ class Solution:
 
 class Solution:
     def longestCommonSubsequence(self, a: str, b: str) -> int:
-        return+reduce(lambda D,B:(p:=0,d:=0,[p:=max(A==B and d+1,d:=u,p)for A,u in zip(a,D)])[2],b,[0]*len(a))[-1]
+        return+reduce(lambda c,i:(p:=0,d:=0,[p:=max(u==i and d+1,d:=v,p)for u,v in zip(a,c)])[2],b,[0]*len(a))[-1]
 
 test('''
 

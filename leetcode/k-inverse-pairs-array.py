4b1cf97d0718b40b617e22dbfad95fc9c3e7e1d2
joric <joric@users.noreply.github.com>
2024-01-27 06:43:57 +0500

    updated files

---
+++ leetcode/k-inverse-pairs-array.py
@@ -0,0 +1,78 @@
+from lc import *
+
+# 2d dp
+
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        dp = [[1] * (k+1) for _ in range(n+1)]
+        sp = [[1] * (k+1) for _ in range(n+1)]
+        N = 10**9 + 7
+        for i in range(1, n+1):
+            for j in range(1, k+1):
+                dp[i][j] = sp[i-1][j] if j < i else (sp[i-1][j] - sp[i-1][j-i]) % N
+                sp[i][j] = (sp[i][j-1] + dp[i][j]) % N
+        return dp[-1][-1]
+
+# 1d dp
+
+class Solution():
+    def kInversePairs(self, n: int, k: int) -> int:
+        d = [0] + [1] * (k + 1)
+        for n in range(2, n+1):
+            t = [0]
+            for k in range(k+1):
+                v = d[k+1]
+                v -= d[k-n+1] if k >= n else 0
+                t.append( (t[-1] + v) )
+            d = t
+        return (d[k+1] - d[k]) % (10**9 + 7)  
+
+# https://leetcode.com/problems/k-inverse-pairs-array/discuss/3832571/python.-1-line!!
+
+class Solution:
+    @cache
+    def kInversePairs(self, n: int, k: int) -> int:
+        return int(not k or n != 1 and (self.kInversePairs(n, k-1) + self.kInversePairs(n-1, k) - (k >= n and self.kInversePairs(n-1, k-n))) % (10**9+7))
+
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        return(f:=cache(lambda n,k:k<1or n!=1and(f(n,k-1)+f(n-1,k)-(k>=n and f(n-1,k-n)))))(n,k)%(10**9+7)
+
+test('''
+629. K Inverse Pairs Array
+Hard
+
+1996
+
+226
+
+Add to List
+
+Share
+For an integer array nums, an inverse pair is a pair of integers [i, j] where 0 <= i < j < nums.length and nums[i] > nums[j].
+
+Given two integers n and k, return the number of different arrays consist of numbers from 1 to n such that there are exactly k inverse pairs. Since the answer can be huge, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: n = 3, k = 0
+Output: 1
+Explanation: Only the array [1,2,3] which consists of numbers from 1 to 3 has exactly 0 inverse pairs.
+Example 2:
+
+Input: n = 3, k = 1
+Output: 2
+Explanation: The array [1,3,2] and [2,1,3] have exactly 1 inverse pair.
+ 
+
+Constraints:
+
+1 <= n <= 1000
+0 <= k <= 1000
+Accepted
+61,409
+Submissions
+143,250
+''')
--- leetcode/stickers-to-spell-word.py
+++ leetcode/stickers-to-spell-word.py
@@ -1,5 +1,67 @@
 from lc import *
 
+class Solution(object):
+    def minStickers(self, stickers, target):
+        t_count = Counter(target)
+        A = [Counter(sticker) & t_count
+             for sticker in stickers]
+
+        for i in range(len(A) - 1, -1, -1):
+            if any(A[i] == A[i] & A[j] for j in range(len(A)) if i != j):
+                A.pop(i)
+
+        self.best = len(target) + 1
+        def search(ans = 0):
+            if ans >= self.best: return
+            if not A:
+                if all(t_count[letter] <= 0 for letter in t_count):
+                    self.best = ans
+                return
+
+            sticker = A.pop()
+            used = max((t_count[letter] - 1) // sticker[letter] + 1
+                        for letter in sticker)
+            used = max(used, 0)
+
+            for c in sticker:
+                t_count[c] -= used * sticker[c]
+
+            search(ans + used)
+            for i in range(used - 1, -1, -1):
+                for letter in sticker:
+                    t_count[letter] += sticker[letter]
+                search(ans + i)
+
+            A.append(sticker)
+
+        search()
+        return self.best if self.best <= len(target) else -1
+
+class Solution(object):
+    def minStickers(self, stickers, target):
+        t_count = Counter(target)
+        A = [Counter(sticker) & t_count
+             for sticker in stickers]
+        for i in range(len(A) - 1, -1, -1):
+            if any(A[i] == A[i] & A[j] for j in range(len(A)) if i != j):
+                A.pop(i)
+        stickers = ["".join(s_count.elements()) for s_count in A]
+        dp = [-1] * (1 << len(target))
+        dp[0] = 0
+        for state in range(1 << len(target)):
+            if dp[state] == -1: continue
+            for sticker in stickers:
+                now = state
+                for letter in sticker:
+                    for i, c in enumerate(target):
+                        if (now >> i) & 1: continue
+                        if c == letter:
+                            now |= 1 << i
+                            break
+                if dp[now] == -1 or dp[now] > dp[state] + 1:
+                    dp[now] = dp[state] + 1
+        return dp[-1]
+
 # https://leetcode.com/problems/stickers-to-spell-word/discuss/2155647/Python.-LRU-cache....
 
 class Solution:

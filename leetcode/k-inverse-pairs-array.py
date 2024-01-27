20859823f1d1c4f9ff1d6601e231df5bfd163b91
joric <joric@users.noreply.github.com>
2024-01-27 06:44:57 +0500

    updated files

--- leetcode/k-inverse-pairs-array.py
+++ leetcode/k-inverse-pairs-array.py
@@ -32,7 +32,7 @@ class Solution():
 class Solution:
     @cache
     def kInversePairs(self, n: int, k: int) -> int:
-        return int(not k or n != 1 and (self.kInversePairs(n, k-1) + self.kInversePairs(n-1, k) - (k >= n and self.kInversePairs(n-1, k-n))) % (10**9+7))
+        return+(not k or n!=1 and(self.kInversePairs(n,k-1)+self.kInversePairs(n-1,k)-(k>=n and self.kInversePairs(n-1,k-n)))%(10**9+7))
 
 class Solution:
     def kInversePairs(self, n: int, k: int) -> int:

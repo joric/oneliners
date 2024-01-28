9a203390e46c047a2e670a9f9076210d62aa6444
joric <joric@users.noreply.github.com>
2024-01-29 00:56:46 +0500

    updated files

--- leetcode/word-search.py
+++ leetcode/word-search.py
@@ -11,14 +11,32 @@ class Solution:
             if  m>i>=0<=j<n and w[0]==b[i][j]:
                 t = b[i][j]
                 b[i][j]='$'
-                r = any(map(f,(i+1,i-1,i,i),(j,j,j+1,j-1),[w[1:]]*4))
+                r = any(map(f,(i+1,i-1,i,i),(j,j,j+1,j-1),[w[k]]*4))
                 b[i][j]=t
                 return r
         return any(f(i,j,w)for i in range(m)for j in range(n))
 
 class Solution:
     def exist(self, b: List[List[str]], w: str) -> bool:
-        m,n=len(b),len(b[0]);f=lambda i,j,w:not w or m>i>=0<=j<n and w[0]==b[i][j]and(t:=b[i][j],setitem(b[i],j,'$'),r:=any(map(f,(i+1,i-1,i,i),(j,j,j+1,j-1),[w[1:]]*4)),setitem(b[i],j,t))and r;return any(f(i,j,w)for i in range(m)for j in range(n))
+        m,n=len(b),len(b[0]);f=lambda i,j,w:not w or m>i>=0<=j<n and w[0]==b[i][j] and(t:=b[i][j],setitem(b[i],j,'$'),r:=any(map(f,(i+1,i-1,i,i),(j,j,j+1,j-1),[w[1:]]*4)),setitem(b[i],j,t))and r;return any(f(i,j,w)for i in range(m)for j in range(n))
+
+class Solution:
+    def exist(self, b: List[List[str]], w: str) -> bool:
+        m,n = len(b),len(b[0])
+        def f(i,j,k):
+            if k==len(w):
+                return True
+            if  m>i>=0<=j<n and w[k]==b[i][j]:
+                t = b[i][j]
+                b[i][j]='$'
+                r = any(f(i+q,j,k+1)or f(i,j+q,k+1)for q in(-1,1))
+                b[i][j]=w[k]
+                return r
+        return any(f(i,j,0)for i in range(m)for j in range(n))
+
+class Solution:
+    def exist(self, b: List[List[str]], w: str) -> bool:
+        m,n=len(b),len(b[0]);f=lambda i,j,k:k==len(w)or m>i>=0<=j<n and w[k]==b[i][j]and(t:=b[i][j],setitem(b[i],j,'$'),r:=any(f(i+q,j,k+1)or f(i,j+q,k+1)for q in(-1,1)),setitem(b[i],j,t))and r;return any(f(i,j,0)for i in range(m)for j in range(n))
 
 test('''
 79. Word Search

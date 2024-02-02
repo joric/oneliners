03ba214f31116505db640b03ee4a1299a4480382
joric <joric@users.noreply.github.com>
2024-02-02 08:48:04 +0500

    updated files

--- leetcode/sequential-digits.py
+++ leetcode/sequential-digits.py
@@ -6,6 +6,18 @@ class Solution:
     def sequentialDigits(self, l: int, h: int) -> List[int]:
         return[i for i in(12,23,34,45,56,67,78,89,123,234,345,456,567,678,789,1234,2345,3456,4567,5678,6789,12345,23456,34567,45678,56789,123456,234567,345678,456789,1234567,2345678,3456789,12345678,23456789,123456789)if l<=i<=h]
 
+# https://leetcode.com/problems/sequential-digits/discuss/627692/Python3-One-line-golf-solution
+
+class Solution:
+    def sequentialDigits(self, l: int, h: int) -> List[int]:
+        return[s for i in range(len(str(l)),len(str(h))+1)for s in[int(''.join(str(x)for x in range(j,j+i)))for j in range(1,11-i)]if l<=s<= h]
+
+# https://leetcode.com/problems/sequential-digits/discuss/1277345/Python-3-one-line-smart-binary-search
+
+class Solution:
+  def sequentialDigits(self, l: int, h: int) -> List[int]:
+    r=range;return(x:=[int(''.join(map(str,r(j,i+j))))for i in r(2,10)for j in r(1,9-i+2)])[(i:=bisect_left(x,l)):bisect_left(x,h+1,i)]
+
 # https://leetcode.com/problems/sequential-digits/discuss/451829/Python-3-(one-line)-(beats-100)
 
 class Solution:

e4a36e5f4aaac5ef87a1eec4479dd16264316b65
joric <joric@users.noreply.github.com>
2024-02-02 08:40:11 +0500

    updated files

---
+++ leetcode/sequential-digits.py
@@ -0,0 +1,53 @@
+from lc import *
+
+# https://leetcode.com/problems/sequential-digits/discuss/751269/Python-3-One-Liner-for-fun
+
+class Solution:
+    def sequentialDigits(self, l: int, h: int) -> List[int]:
+        return[i for i in(12,23,34,45,56,67,78,89,123,234,345,456,567,678,789,1234,2345,3456,4567,5678,6789,12345,23456,34567,45678,56789,123456,234567,345678,456789,1234567,2345678,3456789,12345678,23456789,123456789)if l<=i<=h]
+
+# https://leetcode.com/problems/sequential-digits/discuss/451829/Python-3-(one-line)-(beats-100)
+
+class Solution:
+    def sequentialDigits(self,l: int, h: int) -> List[int]:
+        return[int('123456789'[i:i+d])for d in range(len(str(l)),len(str(h))+1)for i in range(10-d)if l<=int('123456789'[i:i+d])<=h]
+
+class Solution:
+    def sequentialDigits(self, l: int, h: int) -> List[int]:
+        t='123456789';return[x for d in range(len(str(l)),len(str(h))+1)for i in range(10-d)if l<=(x:=int(t[i:i+d]))<=h]
+
+test('''
+1291. Sequential Digits
+Medium
+
+2050
+
+121
+
+Add to List
+
+Share
+An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+
+Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+
+ 
+
+Example 1:
+
+Input: low = 100, high = 300
+Output: [123,234]
+Example 2:
+
+Input: low = 1000, high = 13000
+Output: [1234,2345,3456,4567,5678,6789,12345]
+ 
+
+Constraints:
+
+10 <= low <= high <= 10^9
+Accepted
+104,752
+Submissions
+168,367
+''')

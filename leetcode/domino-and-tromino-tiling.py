628ccb5fb9592fd8599a9244ee15b49e05f75a0d
joric <joric@users.noreply.github.com>
2024-02-02 12:31:46 +0500

    updated files

--- leetcode/domino-and-tromino-tiling.py
+++ leetcode/domino-and-tromino-tiling.py
@@ -15,7 +15,7 @@ class Solution:
 
 class Solution:
     def numTilings(self, n: int) -> int:
-        return(f:=cache(lambda n:n>2and 2*f(n-1)+f(n-3)or(n,1)[n==0]))(n)%(10**9+7)
+        return(f:=cache(lambda n:n>2 and f(n-1)*2+f(n-3)or(n,1)[n<1]))(n)%(10**9+7)
 
 test('''
 790. Domino and Tromino Tiling

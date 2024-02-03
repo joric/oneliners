6afdda82f7564e2dd5f7c4916d0c6e23d3d20a58
joric <joric@users.noreply.github.com>
2024-02-03 21:49:23 +0500

    updated files

---
+++ leetcode/type-of-triangle-ii.py
@@ -0,0 +1,72 @@
+from lc import *
+
+# biweekly-contest-123 Q1
+# https://leetcode.com/problems/type-of-triangle-ii
+
+class Solution:
+    def triangleType(self, n: List[int]) -> str:
+        a,b,c=sorted(n);s=len({*n});return('none',(('scalene','isosceles')[s<3],'equilateral')[s<2])[c<a+b]
+
+class Solution:
+    def triangleType(self, n: List[int]) -> str:
+        a,b,c=sorted(n);return('none',('equilateral','isosceles','scalene')[len({*n})-1])[c<a+b]
+
+class Solution:
+    def triangleType(self, n: List[int]) -> str:
+        a,b,c=sorted(n);return'none equilateral isosceles scalene'.split()[c<a+b and len({*n})]
+
+class Solution:
+    def triangleType(self, n: List[int]) -> str:
+        return'none equilateral isosceles scalene'.split()[2*max(n)<sum(n) and len({*n})]
+
+test('''
+3024. Type of Triangle II
+Easy
+
+5
+
+0
+
+Add to List
+
+Share
+You are given a 0-indexed integer array nums of size 3 which can form the sides of a triangle.
+
+A triangle is called equilateral if it has all sides of equal length.
+A triangle is called isosceles if it has exactly two sides of equal length.
+A triangle is called scalene if all its sides are of different lengths.
+Return a string representing the type of triangle that can be formed or "none" if it cannot form a triangle.
+
+ 
+
+Example 1:
+
+Input: nums = [3,3,3]
+Output: "equilateral"
+Explanation: Since all the sides are of equal length, therefore, it will form an equilateral triangle.
+Example 2:
+
+Input: nums = [3,4,5]
+Output: "scalene"
+Explanation: 
+nums[0] + nums[1] = 3 + 4 = 7, which is greater than nums[2] = 5.
+nums[0] + nums[2] = 3 + 5 = 8, which is greater than nums[1] = 4.
+nums[1] + nums[2] = 4 + 5 = 9, which is greater than nums[0] = 3. 
+Since the sum of the two sides is greater than the third side for all three cases, therefore, it can form a triangle.
+As all the sides are of different lengths, it will form a scalene triangle.
+ 
+
+Example 3:
+
+Input: nums = [9,4,9]
+Output: "isosceles"
+
+Constraints:
+
+nums.length == 3
+1 <= nums[i] <= 100
+Accepted
+20,070
+Submissions
+53,037
+''')

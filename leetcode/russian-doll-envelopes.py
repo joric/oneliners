bd2bdd64c6f78f3c3f929299c300e358215126d5
joric <joric@users.noreply.github.com>
2024-02-03 13:24:22 +0500

    updated files

--- leetcode/russian-doll-envelopes.py
+++ leetcode/russian-doll-envelopes.py
@@ -6,6 +6,31 @@ class Solution:
     def maxEnvelopes(self, e: List[List[int]]) -> int:
         return reduce(lambda d,a:(setitem(d,bisect_left(d,a[1]),a[1]),d)[1],sorted(e,key=lambda x:[x[0],-x[1]]),[inf]*(len(e)+1)).index(inf)
 
+# https://leetcode.com/problems/russian-doll-envelopes/discuss/2599960/Python-6-line-solution
+
+class Solution:
+    def maxEnvelopes(self, e: List[List[int]]) -> int:
+        d = []
+        for i,_ in sorted(e,key=lambda x:(x[1],-x[0])):
+            j = bisect_left(d,i)
+            if not d or j==len(d):
+                d.append(i)
+            else:
+                d[j] = i 
+        return len(d)
+
+class Solution:
+    def maxEnvelopes(self, e: List[List[int]]) -> int:
+        d = []
+        for i,_ in sorted(e,key=lambda x:(x[1],-x[0])):
+            j=bisect_left(d,i)
+            d[j:j+1] = i,
+        return len(d)
+
+class Solution:
+    def maxEnvelopes(self, e: List[List[int]]) -> int:
+        d=[];[setitem(d,slice(j:=bisect_left(d,i),j+1),[i])for i,_ in sorted(e,key=lambda x:(x[1],-x[0]))];return len(d)
+
 test('''
 354. Russian Doll Envelopes
 Hard

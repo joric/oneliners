4d0c70f20205314a98d1d63f1f2812382de30b5e
joric <joric@users.noreply.github.com>
2024-02-07 14:48:40 +0500

    updated files

--- leetcode/sort-characters-by-frequency.py
+++ leetcode/sort-characters-by-frequency.py
@@ -82,5 +82,5 @@ s consists of uppercase and lowercase English letters and digits.
 
 ''',
     #check=lambda res,exp,s:(g:=set(),c:=Counter(res),f:=inf) and next((0 for a,_ in groupby(res) if a in g or c[a]>f or not (f:=c[a],g.add(a))),c==Counter(s))
-    check = lambda r,e,s: Counter(r)==Counter(s) and [*Counter(r).values()]==sorted(Counter(s).values())[::-1]
+    check = lambda r,e,s:(a:=Counter(r))==(b:=Counter(s))and[*a.values()]==sorted(b.values())[::-1]
 )

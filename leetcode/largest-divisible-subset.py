00bca5d04101dc3f638543431c2957b1e729bede
joric <joric@users.noreply.github.com>
2024-02-09 12:04:00 +0500

    updated files

--- leetcode/largest-divisible-subset.py
+++ leetcode/largest-divisible-subset.py
@@ -1,5 +1,19 @@
 from lc import *
 
+# https://leetcode.com/problems/largest-divisible-subset/discuss/684738/Python-Short-DP-with-O(n2)-explained-(update)
+
+class Solution:
+    def largestDivisibleSubset(self, nums):
+        if len(nums) == 0:
+            return []
+        nums.sort()
+        sol = [[num] for num in nums]
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and len(sol[i]) < len(sol[j]) + 1:
+                    sol[i] = sol[j] + [nums[i]]
+        return max(sol, key=len)
+
 # https://leetcode.com/problems/largest-divisible-subset/discuss/84002/4-lines-in-Python
 
 class Solution:

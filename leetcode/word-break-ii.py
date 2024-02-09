5488499e405e849ca5b805381e0e1317e8e6900f
joric <joric@users.noreply.github.com>
2024-02-10 00:07:46 +0500

    updated files

---
+++ leetcode/word-break-ii.py
@@ -0,0 +1,63 @@
+from lc import *
+
+class Solution:
+    def wordBreak(self, s: str, d: List[str]) -> List[str]:
+        m = {len(s): ['']}
+        def f(i):
+            if i not in m:
+                m[i] = [s[i:j] + (t and ' ' + t) for j in range(i+1,len(s)+1) if s[i:j] in d for t in f(j)]
+            return m[i]
+        return f(0)
+
+# https://leetcode.com/problems/word-break-ii/discuss/764188/Python-4-lines-using-lru_cache
+
+class Solution:
+    def wordBreak(self, s: str, d: List[str]) -> List[str]:
+        return(f:=cache(lambda s:[s[:j]+(e and' '+e)for j in range(len(s)+1)if s[:j]in d for e in f(s[j:])]if s else['']))(s)
+
+test('''
+140. Word Break II
+Hard
+
+6646
+
+525
+
+Add to List
+
+Share
+Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+Example 1:
+
+Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+Output: ["cats and dog","cat sand dog"]
+Example 2:
+
+Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+Explanation: Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: []
+ 
+
+Constraints:
+
+1 <= s.length <= 20
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 10
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
+Input is generated in a way that the length of the answer doesn't exceed 105.
+Accepted
+561,170
+Submissions
+1,185,897
+''', check=lambda r,e,*args:sorted(r)==sorted(e))
+

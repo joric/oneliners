db84eb450abc3a8520381a1e76ffa645bfec87a8
joric <joric@users.noreply.github.com>
2024-02-12 05:11:23 +0500

    updated files

--- leetcode/majority-element.py
+++ leetcode/majority-element.py
@@ -1,5 +1,28 @@
 from lc import *
 
+# partition
+class Solution:
+    def majorityElement(self, a: List[int]) -> int:
+        n = len(a)
+        def partition(lo, hi):
+            piv, k = a[hi], lo
+            for i in range(lo, hi):
+                if a[i] < piv:
+                    a[i], a[k] = a[k], a[i]
+                    k += 1
+            a[k], a[hi] = a[hi], a[k]
+            return k
+        lo, hi = 0, n - 1
+        mid = n // 2
+        while True:
+            k = partition(lo, hi)
+            if k == mid:
+                return a[mid]
+            elif k < mid:
+                lo = k + 1
+            else:
+                hi = k - 1
+
 # boyer-moore majority vote
 
 class Solution:

d4eb6b4c6f1437f6b6d268391bd5ab4b328bdc05
joric <joric@users.noreply.github.com>
2024-03-01 11:28:44 +0500

    updated files

--- leetcode/sudoku-solver.py
+++ leetcode/sudoku-solver.py
@@ -4,8 +4,15 @@ from lc import *
 
 class Solution:
     def solveSudoku(self, b: List[List[str]]) -> None:
-        s,e=setitem,enumerate;return next((any(s(b[i],j,c)or self.solveSudoku(b)for c in{*digits[1:]}-{*b[i],*[*zip(*b)][j]}
-            -{b[i//3*3+p//3][j//3*3+p%3]for p in range(9)})or s(b[i],j,'.')for i,r in e(b)for j,v in e(r)if'.'==v),1)
+        def f(i,j):
+            if any(setitem(b[i],j,c)or self.solveSudoku(b)for c in{*digits[1:]}-{*b[i],*[*zip(*b)][j]}-{b[i//3*3+p//3][j//3*3+p%3]for p in range(9)}):
+                return True
+            return setitem(b[i],j,'.')
+        return next((f(i,j)for i,r in enumerate(b)for j,v in enumerate(r)if'.'==v),1)
+
+class Solution:
+    def solveSudoku(self, b: List[List[str]]) -> None:
+        s,e=setitem,enumerate;return next((any(s(b[i],j,c)or self.solveSudoku(b)for c in{*digits[1:]}-{*b[i],*[*zip(*b)][j]}-{b[i//3*3+p//3][j//3*3+p%3]for p in range(9)})or s(b[i],j,'.')for i,r in e(b)for j,v in e(r)if'.'==v),1)
 
 test('''
 37. Sudoku Solver

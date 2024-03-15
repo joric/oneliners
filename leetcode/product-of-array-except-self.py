045d667ad860e61d7476f3a2b69e8b7543dbb11e
joric <joric@users.noreply.github.com>
2024-03-15 07:13:28 +0500

    updated files

--- leetcode/product-of-array-except-self.py
+++ leetcode/product-of-array-except-self.py
@@ -33,10 +33,6 @@ class Solution:
 
 class Solution:
     def productExceptSelf(self, n: List[int]) -> List[int]:
-        a=accumulate;return map(mul,[1]+[*a(n[:-1],mul)],[*a(n[:0:-1],mul)][::-1]+[1])
-
-class Solution:
-    def productExceptSelf(self, n: List[int]) -> List[int]:
         a=accumulate;return map(mul,a([1]+n[:-1],mul),[*a(n[:0:-1],mul)][::-1]+[1])
 
 test('''

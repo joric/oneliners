0edec4bf9725d0519c0d52a7abfcb801f606a759
joric <joric@users.noreply.github.com>
2024-03-22 09:02:05 +0500

    updated files

--- leetcode/palindrome-linked-list.py
+++ leetcode/palindrome-linked-list.py
@@ -20,7 +20,11 @@ class Solution:
 
 class Solution:
     def isPalindrome(self, h: ListNode) -> bool:
-        s=eval(type(h).serialize(h));return s==s[::-1]
+        s=eval(type(h).serialize(h)));return s==s[::-1]
+
+class Solution:
+    def isPalindrome(self, h: ListNode) -> bool:
+        return(s:=eval(type(h).serialize(h)))==s[::-1]
 
 test('''
 234. Palindrome Linked List

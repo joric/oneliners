48cf3cbf1107c74e7321c496aa4ea76e876eeab6
joric <joric@users.noreply.github.com>
2024-03-26 06:17:51 +0500

    updated files

--- leetcode/first-missing-positive.py
+++ leetcode/first-missing-positive.py
@@ -3,6 +3,31 @@ from lc import *
 # https://leetcode.com/problems/first-missing-positive
 
 class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        # Use cycle sort to place positive elements smaller than n
+        # at the correct index
+        i = 0
+        while i < n:
+            correct_idx = nums[i] - 1
+            if 0 < nums[i] <= n and nums[i] != nums[correct_idx]:
+                # swap
+                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
+            else:
+                i += 1
+
+        # Iterate through nums
+        # return smallest missing positive integer
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+
+        # If all elements are at the correct index
+        # the smallest missing positive number is n + 1
+        return n + 1
+
+class Solution:
     def firstMissingPositive(self, n: List[int]) -> int:
         return min({*range(1,len(n)+2)}-{*n})
 

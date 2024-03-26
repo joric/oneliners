917a8afb5efd831bb0fb0632b7c7d7d87240ea26
joric <joric@users.noreply.github.com>
2024-03-26 06:15:38 +0500

    updated files

--- leetcode/first-missing-positive.py
+++ leetcode/first-missing-positive.py
@@ -46,8 +46,8 @@ Explanation: The smallest positive integer 1 is missing.
 
 Constraints:
 
-1 <= nums.length <= 105
--231 <= nums[i] <= 231 - 1
+1 <= nums.length <= 10^5
+-2^31 <= nums[i] <= 2^31 - 1
 Accepted
 1,057,631
 Submissions

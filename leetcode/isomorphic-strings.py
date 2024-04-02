86f8d0ce905f9bd6d5f2c853af5524aa62ffc104
joric <joric@users.noreply.github.com>
2024-04-02 14:51:28 +0500

    updated files

--- leetcode/isomorphic-strings.py
+++ leetcode/isomorphic-strings.py
@@ -1,5 +1,7 @@
 from lc import *
 
+# https://leetcode.com/problems/isomorphic-strings
+
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         return len(set(zip(s,t)))==len(set(s))==len(set(t))

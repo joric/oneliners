3a710e5fcd1f43021895529bac8d8b0f6eb17d37
joric <joric@users.noreply.github.com>
2024-04-07 09:18:07 +0500

    updated files

---
+++ leetcode/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -0,0 +1,76 @@
+from lc import *
+
+# https://leetcode.com/problems/longest-strictly-increasing-or-strictly-decreasing-subarray/
+
+class Solution:
+    def longestMonotonicSubarray(self, a: List[int]) -> int:
+        i=d=1;return max(max(i:=i*(a[j]<a[j+1])+1,d:=d*(a[j]>a[j+1])+1) for j in range(len(a)-1))
+
+test('''
+3105. Longest Strictly Increasing or Strictly Decreasing Subarray
+Easy
+
+2
+
+0
+
+Add to List
+
+Share
+You are given an array of integers nums. Return the length of the longest subarray of nums which is either strictly increasing or strictly decreasing.
+
+ 
+
+Example 1:
+
+Input: nums = [1,4,3,3,2]
+
+Output: 2
+
+Explanation:
+
+The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
+
+The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
+
+Hence, we return 2.
+
+Example 2:
+
+Input: nums = [3,3,3,3]
+
+Output: 1
+
+Explanation:
+
+The strictly increasing subarrays of nums are [3], [3], [3], and [3].
+
+The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
+
+Hence, we return 1.
+
+Example 3:
+
+Input: nums = [3,2,1]
+
+Output: 3
+
+Explanation:
+
+The strictly increasing subarrays of nums are [3], [2], and [1].
+
+The strictly decreasing subarrays of nums are [3], [2], [1], [3,2], [2,1], and [3,2,1].
+
+Hence, we return 3.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+1 <= nums[i] <= 50
+Accepted
+20,249
+Submissions
+37,317
+''')

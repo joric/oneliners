793cd43a605ce1ab58f3b98ff4d2bd68bbe4afa8
joric <joric@users.noreply.github.com>
2024-04-12 08:22:01 +0500

    updated files

--- leetcode/trapping-rain-water.py
+++ leetcode/trapping-rain-water.py
@@ -30,6 +30,13 @@ class Solution:
     def trap(self, h: List[int]) -> int:
         v=w=l=0;r=len(h)-1;return next(w for _ in h if not(l<r and(m:=h[(l:=l+1)-1 if h[l]<h[r]else(r:=r-1)+1],v:=max(v,m),w:=w+v-m)))
 
+# borderline TLE, 9678 ms
+# https://leetcode.com/problems/trapping-rain-water/discuss/3848508/One-Line-Simple-Pyhton-Code
+
+class Solution:
+    def trap(self, h: List[int]) -> int:
+        return sum(min(max(h[:i+1]),max(h[i:]))-x for i,x in enumerate(h))
+
 # updated 2024-04-12
 # https://leetcode.com/problems/trapping-rain-water/discuss/3848508/One-Line-Simple-Pyhton-Code
 
@@ -49,13 +56,6 @@ class Solution:
 class Solution:
     def trap(self, h: List[int]) -> int:
         a=accumulate;return sum(map(min,a(h,max),[*a(h[::-1],max)][::-1]))-sum(h)
-
-# borderline TLE, 9678 ms
-# https://leetcode.com/problems/trapping-rain-water/discuss/3848508/One-Line-Simple-Pyhton-Code
-
-class Solution:
-    def trap(self, h: List[int]) -> int:
-        return sum(min(max(h[:i+1]),max(h[i:]))-x for i,x in enumerate(h))
 
 test('''
 42. Trapping Rain Water

3f94031b5a71c97fd40531a1b72d7471666f0c53
joric <joric@users.noreply.github.com>
2024-04-14 14:27:53 +0500

    updated files

--- README.md
+++ README.md
@@ -86,7 +86,7 @@ class Solution(object):
 ZjFEGESgMMJVDtphDKGK7SGKFy2FKwAAFyTRGilkCgowAkLIAcMx0IaQmSN9IxAqsz0zE0B9IGOsDABQCpFVmV'))),exit(0)
 ```
 
-The solution above is not very reliable, because tests may change in the future, but it may be the fastest:
+The solution above is not very reliable, because tests change, but it's certainly the fastest:
 
 * https://leetcode.com/discuss/feedback/4643730/a-python-solution-that-contain-malicious-payload-in-your-website
 
--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -33,7 +33,7 @@ class Solution(object):
     def twoSum(self, nums, target):
         from zlib import decompress
         from base64 import b64decode
-        open('user.out', 'wb').write(decompress(b64decode("eJyLNtRRMIjlijbSUTAEUggekqAxiqCJjoIRhgZjFEGESgMMJVDtphDKGK7SGKFy2FKwAAFyTRGilkCgowAkLIAcMx0IaQmSN9IxAqsz0zE0B9IGOsDABQCpFVmV"))),exit(0)
+        open('user.out', 'wb').write(decompress(b64decode('eJyLNtRRMIjlijbSUTAEUggekqAxiqCJjoIRhgZjFEGESgMMJVDtphDKGK7SGKFy2FKwAAFyTRGilkCgowAkLIAcMx0IaQmSN9IxAqsz0zE0B9IGOsDABQCpFVmV'))),exit(0)
 
 # shortest
 

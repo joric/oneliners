5be20fdcc12f98aea219f3a17887c6612a050044
joric <joric@users.noreply.github.com>
2024-04-16 07:38:09 +0500

    updated files

--- leetcode/add-one-row-to-tree.py
+++ leetcode/add-one-row-to-tree.py
@@ -40,6 +40,10 @@ class Solution:
     def addOneRow(self, r: TreeNode, v: int, d: int) -> TreeNode:
         c=type(r);return(f:=lambda r,d,s=1:r and c(r.val,f(r.left,d-1),f(r.right,d-1,-1))if d>1 else c(v,*(r,None)[::s]))(r,d)
 
+class Solution:
+    def addOneRow(self, r: TreeNode, v: int, d: int) -> TreeNode:
+        c=type(r);return(f:=lambda r,d,s=1:[r and c(r.val,f(r.left,d-1),f(r.right,d-1,-1)),c(v,*(r,None)[::s])][d<2])(r,d)
+
 test('''
 
 623. Add One Row to Tree

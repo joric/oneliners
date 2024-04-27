2e9fb89c8c266397fad6f4a7d7fde9befcf53790
joric <joric@users.noreply.github.com>
2024-04-28 03:01:21 +0500

    updated files

--- leetcode/make-a-square-with-the-same-color.py
+++ leetcode/make-a-square-with-the-same-color.py
@@ -1,17 +1,37 @@
 from lc import *
 
 # Q1. https://leetcode.com/contest/biweekly-contest-129/
-# https://leetcode.com/problems/make-a-square-with-the-same-color/
-
-class Solution:
-    def canMakeSquare(self, g: List[List[str]]) -> bool:
-        r=(0,1);return any(2<(t:=sum('W'>g[i+x][j+y]for x in r for y in r))or(t<2)for i in r for j in r)
+# https://leetcode.com/problems/make-a-square-with-the-same-color
 
 # https://leetcode.com/problems/make-a-square-with-the-same-color/discuss/5080163/Python-3-oror-4-lines-Counter-oror-TS%3A-23-ms-16.7-MB
 
 class Solution:
     def canMakeSquare(self, g: List[List[str]]) -> bool:
         return any(2<max(Counter((g[i][j]+g[i][j+1]+g[i+1][j]+g[i+1][j+1])).values())for i,j in product((0,1),(0,1)))
+
+# https://leetcode.com/problems/make-a-square-with-the-same-color/discuss/5081338/Simple-Python-solution
+
+class Solution:
+    def canMakeSquare(self, g: List[List[str]]) -> bool:
+        m=__import__('numpy').array(g)
+        for i in(0,1):
+            for j in(0,1):
+                n = (m[i:i+2,j:j+2]<'W').sum()
+                if n<2 or n>2:
+                    return True
+        return False
+
+class Solution:
+    def canMakeSquare(self, g: List[List[str]]) -> bool:
+        return any(2-(__import__('numpy').array(g)[i:i+2,j:j+2]<'W').sum()for i in(0,1)for j in(0,1))
+
+class Solution:
+    def canMakeSquare(self, g: List[List[str]]) -> bool:
+        r=(0,1);return any(2-sum('W'>g[i+x][j+y]for x in r for y in r)for i in r for j in r)
+
+class Solution:
+    def canMakeSquare(self, g: List[List[str]]) -> bool:
+        p=[*product(*[[0,1]]*2)];return any(2-sum('W'>g[i+x][j+y]for x,y in p)for i,j in p)
 
 test('''
 3127. Make a Square with the Same Color
@@ -29,19 +49,8 @@ You are given a 2D matrix grid of size 3 x 3 consisting only of characters 'B' a
 Your task is to change the color of at most one cell so that the matrix has a 2 x 2 square where all cells are of the same color.
 Return true if it is possible to create a 2 x 2 square of the same color, otherwise, return false.
 
- 
-
 Example 1:
 
- 
- 
- 
- 
- 
- 
- 
- 
- 
 Input: grid = [["B","W","B"],["B","W","W"],["B","W","B"]]
 
 Output: true
@@ -52,15 +61,6 @@ It can be done by changing the color of the grid[0][2].
 
 Example 2:
 
- 
- 
- 
- 
- 
- 
- 
- 
- 
 Input: grid = [["B","W","B"],["W","B","W"],["B","W","B"]]
 
 Output: false
@@ -71,15 +71,6 @@ It cannot be done by changing at most one cell.
 
 Example 3:
 
- 
- 
- 
- 
- 
- 
- 
- 
- 
 Input: grid = [["B","W","B"],["B","W","W"],["B","W","W"]]
 
 Output: true
@@ -92,6 +83,12 @@ The grid already contains a 2 x 2 square of the same color.
 More examples:
 
 Input: grid = [["B","B","B"],["B","B","B"],["B","B","B"]]
+Output: true
+
+Input: grid = [["B","B","B"],["B","W","B"],["B","B","B"]]
+Output: true
+
+Input: grid = [["W","W","W"],["W","W","W"],["B","B","B"]]
 Output: true
 
 Constraints:

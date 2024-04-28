61458295550fa775e5eab8e0b00d3b81cc71fce4
joric <joric@users.noreply.github.com>
2024-04-28 13:47:31 +0500

    updated files

--- leetcode/sum-of-distances-in-tree.py
+++ leetcode/sum-of-distances-in-tree.py
@@ -3,6 +3,30 @@ from lc import *
 # https://leetcode.com/problems/sum-of-distances-in-tree
 
 class Solution:
+    def sumOfDistancesInTree(self, N, edges):
+        G = defaultdict(set)
+        for i, j in edges:
+            G[i].add(j)
+            G[j].add(i)
+        def dfs(node, parent, depth):
+            ans = 1
+            for neib in G[node]:
+                if neib != parent:
+                    ans += dfs(neib, node, depth + 1)
+            weights[node] = ans
+            depths[node] = depth
+            return ans
+        def dfs2(node, parent, w):
+            ans[node] = w
+            for neib in G[node]:
+                if neib != parent:
+                    dfs2(neib, node, w + N - 2*weights[neib])
+        weights, depths, ans = [0]*N, [0]*N, [0]*N
+        dfs(0, -1, 0)
+        dfs2(0, -1, sum(depths))
+        return ans
+
+class Solution:
     def sumOfDistancesInTree(self, n, edges):
         g = defaultdict(set)
         for i, j in edges:

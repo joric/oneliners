798e1b90bbd163987360b7b9d5420d1b6dd893e1
joric <joric@users.noreply.github.com>
2024-05-05 14:18:37 +0500

    updated files

--- leetcode/valid-word.py
+++ leetcode/valid-word.py
@@ -28,7 +28,7 @@ class Solution:
 
 class Solution:
     def isValid(self, w: str) -> bool:
-        return re.match('^(?=.*[aeiou])(?=.*[^0-9aeiou])[a-z0-9]{3,}$',w,re.I)
+        return match('^(?=.*[aeiou])(?=.*[^0-9aeiou])[a-z0-9]{3,}$',w,I)
 
 test('''
 3136. Valid Word

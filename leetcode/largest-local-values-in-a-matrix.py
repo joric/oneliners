a5071576ed2f1f05488c2c1733be8d671915d357
joric <joric@users.noreply.github.com>
2024-05-12 05:21:15 +0500

    updated files

--- leetcode/largest-local-values-in-a-matrix.py
+++ leetcode/largest-local-values-in-a-matrix.py
@@ -6,11 +6,25 @@ class Solution:
     def largestLocal(self, g: List[List[int]]) -> List[List[int]]:
         r=range(len(g)-2);return[[max(g[x][y]for x,y in product(range(i,i+3),range(j,j+3)))for j in r]for i in r]
 
+# https://leetcode.com/problems/largest-local-values-in-a-matrix/discuss/2907214/SciPy-solution
+
+import scipy.ndimage as nd
+
+class Solution:
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        filtered = nd.maximum_filter(grid, size=(3,3))
+        return filtered[1: len(grid) - 1, 1: len(grid[0]) - 1].tolist()
+
+class Solution:
+    def largestLocal(self, g: List[List[int]]) -> List[List[int]]:
+        return __import__('scipy').ndimage.maximum_filter(g,size=(3,3))[1:len(g)-1,1:len(g[0])-1]
+
 # https://leetcode.com/problems/largest-local-values-in-a-matrix/discuss/2475476/One-liner
 
 class Solution:
     def largestLocal(self, g: List[List[int]]) -> List[List[int]]:
         t=range(len(g)-2);return[[max(max(r[c:c+3])for r in g[r:r+3])for c in t]for r in t]
+
 
 test('''
 2373. Largest Local Values in a Matrix

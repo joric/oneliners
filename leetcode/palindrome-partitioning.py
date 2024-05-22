53ee7ad974250b1a60d850459a6dbb6b825694db
joric <joric@users.noreply.github.com>
2024-05-22 05:09:31 +0500

    updated files

--- leetcode/palindrome-partitioning.py
+++ leetcode/palindrome-partitioning.py
@@ -4,7 +4,14 @@ from lc import *
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        return [[s[:i]]+p for i in range(1,len(s)+1) if s[:i]==s[i-1::-1] for p in self.partition(s[i:])] or [[]]
+        return[[s[:i]]+p for i in range(1,len(s)+1) if s[:i]==s[i-1::-1] for p in self.partition(s[i:])]or[[]]
+
+# updated 2024-05-22
+
+class Solution:
+    def partition(t, s: str) -> List[List[str]]:
+        return[[s[:i],*p]for i in range(1,len(s)+1)if s[:i]==s[i-1::-1]for p in t.partition(s[i:])]or[[]]
+
 
 test('''
 

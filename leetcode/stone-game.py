89afd47b932a3df19a6302bc4cf4256601c6b8d4
joric <joric@users.noreply.github.com>
2024-06-22 19:02:49 +0500

    updated files

--- leetcode/stone-game.py
+++ leetcode/stone-game.py
@@ -1,7 +1,36 @@
 from lc import *
 
+# https://leetcode.com/problems/stone-game/discuss/154610/DP-or-Just-return-true
+
+class Solution:
+    def stoneGame(self, p: List[int]) -> bool:
+        n = len(p)
+        dp = [[0] * n for i in range(n)]
+        for i in range(n): dp[i][i] = p[i]
+        for d in range(1, n):
+            for i in range(n - d):
+                dp[i][i + d] = max(p[i] - dp[i + 1][i + d], p[i + d] - dp[i][i + d - 1])
+        return dp[0][-1] > 0
+
+# simplify to 1D dp
+
+class Solution:
+    def stoneGame(self, p: List[int]) -> bool:
+        n = len(p)
+        dp = p[:]
+        for d in range(1, n):
+            for i in range(n - d):
+                dp[i] = max(p[i] - dp[i + 1], p[i + d] - dp[i])
+        return dp[0] > 0
+
+# recursive
+
+class Solution:
+    def stoneGame(self, p: List[int]) -> bool:
+        return(k:=sum(p))<2*(f:=cache(lambda i,j,k:i<j and max(k-f(i+1,j,k-p[i]),k-f(i,j-1,k-p[j]))or p[i]))(0,len(p)-1,k)
+
 # https://leetcode.com/problems/stone-game
-# shortest solution
+# shortest solution, except strictly-palindromic-number
 
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:

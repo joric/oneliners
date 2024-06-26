a77a372bd3a0033290d09c67daaa2f6227c8790f
joric <joric@users.noreply.github.com>
2024-06-26 14:19:36 +0500

    updated files

--- leetcode/balance-a-binary-search-tree.py
+++ leetcode/balance-a-binary-search-tree.py
@@ -7,14 +7,13 @@ class Solution:
         def dfs(node):
             if not node: return []
             return dfs(node.left) + [node.val] + dfs(node.right)
-        ns = dfs(root)
-        def build(l, r):
-            if l > r: return None
-            m = (l + r) // 2
-            root = TreeNode(ns[m])
-            root.left, root.right = build(l, m-1), build(m + 1, r)
-            return root
-        return build(0, len(ns) - 1)
+        a = dfs(root)
+        def build(l,r):
+            if l>r:
+                return None
+            m = (l+r) // 2
+            return TreeNode(a[m], build(l,m-1), build(m+1,r))
+        return build(0, len(a)-1)
 
 class Solution:
     def balanceBST(self, t: TreeNode) -> TreeNode:

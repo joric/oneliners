4a73bf94d3e2d24cd736cc4ca392d507829015e9
joric <joric@users.noreply.github.com>
2024-07-14 19:05:21 +0500

    updated files

--- leetcode/number-of-atoms.py
+++ leetcode/number-of-atoms.py
@@ -112,7 +112,7 @@ class Solution:
 
 class Solution:
     def countOfAtoms(self, s: str) -> str:
-        [s:=re.sub('\((\w+)\)(\d*)',lambda m:re.sub('([A-Z][a-z]*)(\d*)',lambda mm:mm[1]+str(int(mm[2] or '1')*int(m[2]or'1')),m[1]),s)for _ in s];return''.join(e+str(c)*(c>1)for e,c in sorted(sum((Counter({m[1]:int(m[2] or 1)})for m in finditer('([A-Z][a-z]*)(\d*)',s)),Counter()).items()))
+        p='([A-Z][a-z]*)(\d*)';[s:=re.sub('\((\w+)\)(\d*)',lambda m:re.sub(p,lambda g:g[1]+str(int(g[2]or'1')*int(m[2]or'1')),m[1]),s)for _ in s];return''.join(e+str(c)*(c>1)for e,c in sorted(sum((Counter({m[1]:int(m[2]or 1)})for m in finditer(p,s)),Counter()).items()))
 
 test('''
 726. Number of Atoms

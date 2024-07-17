2682812b978a052af462364dd5be4a1372fc2654
joric <joric@users.noreply.github.com>
2024-07-17 13:58:43 +0500

    updated files

--- leetcode/delete-nodes-and-return-forest.py
+++ leetcode/delete-nodes-and-return-forest.py
@@ -45,8 +45,7 @@ class Solution:
             if t:
                 e = t.val in d
                 not e and i and a.append(t)
-                t.left = f(t.left, e)
-                t.right = f(t.right, e)
+                t.left, t.right = f(t.left,e), f(t.right,e)
                 if not e:
                     return t
         f(r,1)
@@ -55,6 +54,14 @@ class Solution:
 class Solution:
     def delNodes(self, r: Optional[TreeNode], d: List[int]) -> List[TreeNode]:
         a,d=[],{*d};(f:=lambda t,i:t and(not(e:=t.val in d)and i and a.append(t)or[setattr(t,k,f(getattr(t,k),e))for k in('left','right')]and(t,None)[e]))(r,1);return a
+
+class Solution:
+    def delNodes(self, r: Optional[TreeNode], d: List[int]) -> List[TreeNode]:
+        a,d=[],{*d};(f:=lambda t,i:t and(not(e:=t.val in d)and i and a.append(t)or f and exec('t.left,t.right=f(t.left,e),f(t.right,e)')or(t,None)[e]))(r,1);return a
+
+class Solution:
+    def delNodes(self, r: Optional[TreeNode], d: List[int]) -> List[TreeNode]:
+        a,d=[],{*d};(f:=lambda t,i=1:t and(not(e:=t.val in d)and i and a.append(t)or f(exec('t.left,t.right=f(t.left,e),f(t.right,e)'))or(t,None)[e]))(r);return a
 
 test('''
 1110. Delete Nodes And Return Forest

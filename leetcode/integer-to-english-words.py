b794cff76900044531d59467c5472a5e6961b7f8
joric <joric@users.noreply.github.com>
2024-08-07 05:36:12 +0500

    updated files

--- leetcode/integer-to-english-words.py
+++ leetcode/integer-to-english-words.py
@@ -75,6 +75,11 @@ Input: num = 1234567
 Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
  
 
+Other examples:
+
+Input: num = 0
+Output: "Zero"
+
 Constraints:
 
 0 <= num <= 2^31 - 1

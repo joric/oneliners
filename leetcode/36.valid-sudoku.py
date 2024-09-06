49813fab79e865b211e61c559a5ccba1a567aabc
joric <joric@users.noreply.github.com>
2024-09-06 13:33:41 +0500

    updated files

--- leetcode/36.valid-sudoku.py
+++ leetcode/36.valid-sudoku.py
@@ -35,8 +35,7 @@ Only the filled cells need to be validated according to the mentioned rules.
 
 Example 1:
 
-Input: board =
-[["5","3",".",".","7",".",".",".","."]
+Input: board = [["5","3",".",".","7",".",".",".","."]
 ,["6",".",".","1","9","5",".",".","."]
 ,[".","9","8",".",".",".",".","6","."]
 ,["8",".",".",".","6",".",".",".","3"]
@@ -49,8 +48,7 @@ Output: true
 
 Example 2:
 
-Input: board =
-[["8","3",".",".","7",".",".",".","."]
+Input: board = [["8","3",".",".","7",".",".",".","."]
 ,["6",".",".","1","9","5",".",".","."]
 ,[".","9","8",".",".",".",".","6","."]
 ,["8",".",".",".","6",".",".",".","3"]
@@ -63,8 +61,7 @@ Output: false
 Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 
 Example 3:
-Input: board =
-[[".",".",".",".",".",".",".",".","."]
+Input: board = [[".",".",".",".",".",".",".",".","."]
 ,[".",".",".",".",".",".",".",".","."]
 ,[".",".",".",".",".",".",".",".","."]
 ,[".",".",".",".",".",".",".",".","."]

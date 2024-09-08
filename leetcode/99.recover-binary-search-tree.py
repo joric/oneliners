ffdeaba9b6237b2ac61f164a5461fd21c3732a96
joric <joric@users.noreply.github.com>
2024-09-08 10:16:50 +0500

    updated files

--- leetcode/99.recover-binary-search-tree.py
+++ leetcode/99.recover-binary-search-tree.py
@@ -16,6 +16,10 @@ class Solution:
         inorder(root)
         self.first.val, self.second.val = self.second.val, self.first.val
 
+class Solution:
+    def recoverTree(s, r: Optional[TreeNode]) -> None:
+        s.a,s.b,s.c=0,0,TreeNode(-inf);(f:=lambda t:t and (f(t.left),s.c.val>=t.val and exec('s.a=s.a or s.c;s.b=t'),exec('s.c=t'),f(t.right)))(r);exec('s.a.val,s.b.val=s.b.val,s.a.val')
+
 test('''
 99. Recover Binary Search Tree
 Medium

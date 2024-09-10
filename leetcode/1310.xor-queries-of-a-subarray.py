2503b1aaf367f81c605c3c4c8e1d44fbd69c0b80
joric <joric@users.noreply.github.com>
2024-09-10 05:12:00 +0500

    updated files

---
+++ leetcode/1310.xor-queries-of-a-subarray.py
@@ -0,0 +1,67 @@
+from lc import *
+
+# https://leetcode.com/problems/xor-queries-of-a-subarray/discuss/470787/JavaC%2B%2BPython-Straight-Forward-Solution
+
+class Solution:
+    def xorQueries(self, a: List[int], q: List[List[int]]) -> List[int]:
+        for i in range(len(a)-1):
+            a[i+1] ^= a[i]
+        return [a[j]^a[i-1]if i else a[j]for i,j in q]
+
+# https://leetcode.com/problems/xor-queries-of-a-subarray/discuss/471231/2-clean-lines-Python
+
+class Solution:
+    def xorQueries(self, a: List[int], q: List[List[int]]) -> List[int]:
+        x=[0,*accumulate(a,xor)];return [x[i]^x[j+1]for i,j in q]
+
+test('''
+1310. XOR Queries of a Subarray
+Medium
+
+1502
+
+37
+
+Add to List
+
+Share
+You are given an array arr of positive integers. You are also given the array queries where queries[i] = [lefti, righti].
+
+For each query i compute the XOR of elements from lefti to righti (that is, arr[lefti] XOR arr[lefti + 1] XOR ... XOR arr[righti] ).
+
+Return an array answer where answer[i] is the answer to the ith query.
+
+ 
+
+Example 1:
+
+Input: arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+Output: [2,7,14,8] 
+Explanation: 
+The binary representation of the elements in the array are:
+1 = 0001 
+3 = 0011 
+4 = 0100 
+8 = 1000 
+The XOR values for queries are:
+[0,1] = 1 xor 3 = 2 
+[1,2] = 3 xor 4 = 7 
+[0,3] = 1 xor 3 xor 4 xor 8 = 14 
+[3,3] = 8
+Example 2:
+
+Input: arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+Output: [8,0,4,4]
+ 
+
+Constraints:
+
+1 <= arr.length, queries.length <= 3 * 104
+1 <= arr[i] <= 109
+queries[i].length == 2
+0 <= lefti <= righti < arr.length
+Accepted
+61,244
+Submissions
+83,566
+''')

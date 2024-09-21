13d26423a7d0b3bf7d2325bf591fbc007d54bc0a
joric <joric@users.noreply.github.com>
2024-09-21 05:32:21 +0500

    updated files

--- leetcode/440.k-th-smallest-in-lexicographical-order.py
+++ leetcode/440.k-th-smallest-in-lexicographical-order.py
@@ -7,28 +7,6 @@ class Solution:
     def findKthNumber(self, n, k):
         return sorted(range(1,n+1), key=str)[k-1]
 
-# https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/discuss/1608540/Python3-traverse-denary-trie
-
-class Solution:
-    def findKthNumber(self, n: int, k: int) -> int:
-        def fn(x):
-            r,d = 0,1
-            while x <= n:
-                r += min(n-x+1, d)
-                x *= 10
-                d *= 10
-            return r
-        x = 1
-        while k > 1:
-            c = fn(x)
-            if k > c:
-                k -= c
-                x += 1
-            else:
-                k -= 1
-                x *= 10
-        return x
-
 # https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/discuss/222264/short-python
 
 class Solution:
@@ -51,11 +29,6 @@ class Solution:
 
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
-        c=1;return next(c for _ in count()if not(k>1 and((s:=0,a:=(b:=c)),all(a<=n for _ in count()if(s:=s+min(b,n)-a+1,a:=a*10,b:=b*10+9)),k>s and(c:=c+1,k:=k-s)or(c:=c*10,k:=k-1))))
-
-
-class Solution:
-    def findKthNumber(self, n: int, k: int) -> int:
         c = 1 
         while k>1:
             s = (f:=lambda s,a,b,c:a<=n and f(s+min(b,n)-a+1,a*10,b*10+9,c)or s)(0,c,c,c)
@@ -69,7 +42,26 @@ class Solution:
 
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
-        return(g:=lambda c,k:k>1 and(g(c+1,k-s)if k>(s:=(f:=lambda s,a,b,c:a<=n and f(s+min(b,n)-a+1,a*10,b*10+9,c)or s)(0,c,c,c))else g(c*10,k-1))or c)(1,k)
+        return(g:=lambda c,k:k>1 and(k>(s:=(f:=lambda s,a,b,c:a<=n and f(s+min(b,n)-a+1,a*10,b*10+9,c)or s)(0,c,c,c))and g(c+1,k-s)or g(c*10,k-1))or c)(1,k)
+
+# https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/discuss/1608540/Python3-traverse-denary-trie
+
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        x = 1
+        while k > 1:
+            c = (f:=lambda x,r,d:x<=n and f(x*10,r+min(n-x+1,d),d*10)or r)(x,0,1)
+            if k > c:
+                k -= c
+                x += 1
+            else:
+                k -= 1
+                x *= 10
+        return x
+
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        return(g:=lambda k,x:k>1 and(k>(c:=(f:=lambda x,r,d:x<=n and f(x*10,r+min(n-x+1,d),d*10)or r)(x,0,1))and g(k-c,x+1)or g(k-1,x*10))or x)(k,1)
 
 test('''
 440. K-th Smallest in Lexicographical Order

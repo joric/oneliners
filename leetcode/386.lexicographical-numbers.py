16b6f80a755b2ef77538d0dd862af026c4bfbaf9
joric <joric@users.noreply.github.com>
2024-09-21 13:08:19 +0500

    updated files

--- leetcode/386.lexicographical-numbers.py
+++ leetcode/386.lexicographical-numbers.py
@@ -1,5 +1,26 @@
 from lc import *
 
+# tutorial
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        lexicographical_numbers = []
+        current_number = 1
+
+        # Generate numbers from 1 to n
+        for _ in range(n):
+            lexicographical_numbers.append(current_number)
+
+            # If multiplying the current number by 10 is within the limit, do it
+            if current_number * 10 <= n:
+                current_number *= 10
+            else:
+                # Adjust the current number by moving up one digit
+                while current_number % 10 == 9 or current_number >= n:
+                    current_number //= 10  # Remove the last digit
+                current_number += 1  # Increment the number
+        return lexicographical_numbers
+
 # https://leetcode.com/problems/lexicographical-numbers/discuss/86228/The-most-elegant-python-solution-so-far.-10-liner.-iterative.-O(n)-time-O(1)-space.
 
 class Solution(object):

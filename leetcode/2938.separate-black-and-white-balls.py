ee6b3fa2c7401d1ee194deef83753e2bef355d6d
joric <joric@users.noreply.github.com>
2024-10-08 04:43:31 +0500

    updated files

---
+++ leetcode/2938.separate-black-and-white-balls.py
@@ -0,0 +1,60 @@
+from lc import *
+
+# https://leetcode.com/problems/separate-black-and-white-balls/discuss/4305143/Python-3-oror-3-lines-with-explanation-oror-TS%3A-92-15
+
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        return sum(d:=[i for i,c in enumerate(s)if c=='0'])-comb(len(d),2)
+
+test('''
+2938. Separate Black and White Balls
+Medium
+
+195
+
+6
+
+Add to List
+
+Share
+There are n balls on a table, each ball has a color black or white.
+
+You are given a 0-indexed binary string s of length n, where 1 and 0 represent black and white balls, respectively.
+
+In each step, you can choose two adjacent balls and swap them.
+
+Return the minimum number of steps to group all the black balls to the right and all the white balls to the left.
+
+ 
+
+Example 1:
+
+Input: s = "101"
+Output: 1
+Explanation: We can group all the black balls to the right in the following way:
+- Swap s[0] and s[1], s = "011".
+Initially, 1s are not grouped together, requiring at least 1 step to group them to the right.
+Example 2:
+
+Input: s = "100"
+Output: 2
+Explanation: We can group all the black balls to the right in the following way:
+- Swap s[0] and s[1], s = "010".
+- Swap s[1] and s[2], s = "001".
+It can be proven that the minimum number of steps needed is 2.
+Example 3:
+
+Input: s = "0111"
+Output: 0
+Explanation: All the black balls are already grouped to the right.
+ 
+
+Constraints:
+
+1 <= n == s.length <= 105
+s[i] is either '0' or '1'.
+Accepted
+25,321
+Submissions
+47,211
+''')

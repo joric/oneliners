107dc4f9c613db459aa756581cbc4f80aa3c3b4a
joric <joric@users.noreply.github.com>
2024-10-12 07:12:20 +0500

    updated files

--- leetcode/2406.divide-intervals-into-minimum-number-of-groups.py
+++ leetcode/2406.divide-intervals-into-minimum-number-of-groups.py
@@ -62,6 +62,18 @@ class Solution:
     def minGroups(self, v: List[List[int]]) -> int:
         v.sort();h=[];[heappush(h,b)or[a]>h[:1]and heappop(h)for a,b in v];return len(h)
 
+class Solution:
+    def minGroups(self, v: List[List[int]]) -> int:
+        h=[];[heappush(h,b)or[a]>h[:1]and heappop(h)for a,b in sorted(v)];return len(h)
+
+class Solution:
+    def minGroups(self, v: List[List[int]]) -> int:
+        h=[];[heappush(h,b)or a>h[0]and heappop(h)for a,b in sorted(v)];return len(h)
+
+class Solution:
+    def minGroups(self, v: List[List[int]]) -> int:
+        h=[];[(heappush(h,b),a>h[0]and heappop(h))for a,b in sorted(v)];return len(h)
+
 test('''
 2406. Divide Intervals Into Minimum Number of Groups
 Medium

d714bd355d408e4a1618351dcbb254ef8f0abbff
joric <joric@users.noreply.github.com>
2024-10-17 12:50:00 +0500

    updated files

--- leetcode/670.maximum-swap.py
+++ leetcode/670.maximum-swap.py
@@ -1,6 +1,24 @@
 from lc import *
 
+# https://leetcode.com/problems/maximum-swap/discuss/2114239/Intuitive-or-recursive-or-python-3
+
+class Solution:
+    def maximumSwap(self, a: int) -> int:
+        def f(a):
+            if not a:
+                return a
+            m = max(a)
+            if m == a[0]:
+                return [a[0]] + f(a[1:])
+            else:
+                i = a[::-1].index(m)
+                a[0],a[~i]=a[~i],a[0]
+                return a
+        return int(''.join(map(str,f([*map(int,str(a))]))))
+
 # https://leetcode.com/problems/maximum-swap/discuss/107137/3-5-lines-PythonC%2B%2BJavaRuby
+
+# updated 2024-10-17 (POTD)
 
 class Solution:
     def maximumSwap(self, n: int) -> int:

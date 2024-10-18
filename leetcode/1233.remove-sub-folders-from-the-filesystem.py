2bbd45a149a3ce4421135ca5cea739d62ef13366
joric <joric@users.noreply.github.com>
2024-10-18 14:10:04 +0500

    updated files

---
+++ leetcode/1233.remove-sub-folders-from-the-filesystem.py
@@ -0,0 +1,56 @@
+from lc import *
+
+# https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/discuss/409028/JavaPython-3-3-methods-from-O(n-*-(logn-%2B-m-2))-to-O(n-*-m)-w-brief-explanation-and-analysis.
+
+class Solution:
+    def removeSubfolders(self, d: List[str]) -> List[str]:
+        r=[];[r.append(f)for f in sorted(d)if not r or not f.startswith(r[-1]+'/')];return r
+
+test('''
+1233. Remove Sub-Folders from the Filesystem
+Medium
+
+823
+
+116
+
+Add to List
+
+Share
+Given a list of folders folder, return the folders after removing all sub-folders in those folders. You may return the answer in any order.
+
+If a folder[i] is located within another folder[j], it is called a sub-folder of it. A sub-folder of folder[j] must start with folder[j], followed by a "/". For example, "/a/b" is a sub-folder of "/a", but "/b" is not a sub-folder of "/a/b/c".
+
+The format of a path is one or more concatenated strings of the form: '/' followed by one or more lowercase English letters.
+
+For example, "/leetcode" and "/leetcode/problems" are valid paths while an empty string and "/" are not.
+ 
+
+Example 1:
+
+Input: folder = ["/a","/a/b","/c/d","/c/d/e","/c/f"]
+Output: ["/a","/c/d","/c/f"]
+Explanation: Folders "/a/b" is a subfolder of "/a" and "/c/d/e" is inside of folder "/c/d" in our filesystem.
+Example 2:
+
+Input: folder = ["/a","/a/b/c","/a/b/d"]
+Output: ["/a"]
+Explanation: Folders "/a/b/c" and "/a/b/d" will be removed because they are subfolders of "/a".
+Example 3:
+
+Input: folder = ["/a/b/c","/a/b/ca","/a/b/d"]
+Output: ["/a/b/c","/a/b/ca","/a/b/d"]
+ 
+
+Constraints:
+
+1 <= folder.length <= 4 * 104
+2 <= folder[i].length <= 100
+folder[i] contains only lowercase letters and '/'.
+folder[i] always starts with the character '/'.
+Each folder name is unique.
+Accepted
+55,535
+Submissions
+83,983
+''')

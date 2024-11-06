65f25c9deabc0632431b2cc19c6224f61c706a92
joric <joric@users.noreply.github.com>
2024-11-06 07:15:35 +0500

    updated files

--- leetcode/3011.find-if-array-can-be-sorted.py
+++ leetcode/3011.find-if-array-can-be-sorted.py
@@ -7,7 +7,7 @@ from lc import *
 
 class Solution:
     def canSortArray(self, a: List[int]) -> bool:
-        return all(max(a)<min(b)for a,b in pairwise([[*v]for _,v in groupby(a,key=int.bit_count)]))
+        return all(max(a)<min(b)for a,b in pairwise([[*v]for _,v in groupby(a,int.bit_count)]))
 
 # https://leetcode.com/problems/find-if-array-can-be-sorted/discuss/4859284/one-line-solution
 

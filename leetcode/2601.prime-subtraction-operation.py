1771da61198fbf256cfdcbbbd23817b36c94f816
joric <joric@users.noreply.github.com>
2024-11-11 05:55:14 +0500

    updated files

--- leetcode/2601.prime-subtraction-operation.py
+++ leetcode/2601.prime-subtraction-operation.py
@@ -1,39 +1,6 @@
 from lc import *
 
-# https://leetcode.com/problems/prime-subtraction-operation/discuss/3721120/Python-almost-linear-solution-with-explanation
-
-class Solution:
-    def primeSubOperation(self, nums: List[int]) -> bool:
-        primes = [0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
-        prev_small = 0
-        for v in nums:
-            pos, is_found = bisect_left(primes, v), False
-            for i in range(pos - 1, -1, -1):
-                if v - primes[i] > prev_small:
-                    prev_small = v - primes[i]
-                    is_found = True
-                    break
-            if not is_found:
-                return False
-        return True
-
-class Solution:
-    def primeSubOperation(self, nums: List[int]) -> bool:
-        g=range(2,n:=999);primes=sorted([0,*reduce(lambda r,x:r-set(range(x**2,n,x))if x in r else r,g,set(g))])
-        prev_small = 0
-        for v in nums:
-            pos, is_found = bisect_left(primes, v), False
-            for i in range(pos - 1, -1, -1):
-                if v - primes[i] > prev_small:
-                    prev_small = v - primes[i]
-                    is_found = True
-                    break
-            if not is_found:
-                return False
-        return True
-
 # https://leetcode.com/problems/prime-subtraction-operation/discuss/5386264/Small-Python-solution
-
 # Pregenerated primes + 0 as a prefix for convenience
 class Solution:
     def primeSubOperation(self, nums: List[int]) -> bool:

24905387f5ec3ee1fa3728acd972f757ae6a5785
joric <joric@users.noreply.github.com>
2024-11-14 10:44:48 +0500

    updated files

--- leetcode/2064.minimized-maximum-of-products-distributed-to-any-store.py
+++ leetcode/2064.minimized-maximum-of-products-distributed-to-any-store.py
@@ -20,6 +20,10 @@ class Solution:
     def minimizedMaximum(self, n: int, q: List[int]) -> int:
         return-~bisect_left(range(1,max(q)),0,key=lambda x:n-sum(-(-i//x)for i in q))
 
+class Solution:
+    def minimizedMaximum(self, n: int, q: List[int]) -> int:
+        return-~bisect_left(range(1,max(q)),0,key=lambda x:n+sum(-i//x for i in q))
+
 test('''
 2064. Minimized Maximum of Products Distributed to Any Store
 Medium

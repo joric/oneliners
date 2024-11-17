b36bbe81b0632da189c1843ac3c9c334c1ed4753
joric <joric@users.noreply.github.com>
2024-11-17 10:47:44 +0500

    updated files

--- leetcode/862.shortest-subarray-with-sum-at-least-k.py
+++ leetcode/862.shortest-subarray-with-sum-at-least-k.py
@@ -1,5 +1,21 @@
 from lc import *
 
+# https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/discuss/349161/Python-7-line-heap
+
+class Solution:
+    def shortestSubarray(self, a: List[int], k: int) -> int:
+        h,t,s=[(0,-1)],inf,0
+        for i,x in enumerate(a):
+            s += x
+            while h and s-k>=h[0][0]:
+                t = min(t,i-heappop(h)[1])
+            heappush(h,(s,i))
+        return(-1,t)[t<inf]
+
+class Solution:
+    def shortestSubarray(self, a: List[int], k: int) -> int:
+        h,t,s=[(0,-1)],inf,0;[(s:=s+x,all(h and s-k>=h[0][0]and(t:=min(t,i-heappop(h)[1]))for _ in a),heappush(h,(s,i)))for i,x in enumerate(a)];return(-1,t)[t<inf]
+
 # https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/discuss/6053014/Python-3-6-lines-an-extremely-simple-solution
 
 class Solution:

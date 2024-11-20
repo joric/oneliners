97d78aeb3f3ef3323af7b2e6c7693aad3bd52170
joric <joric@users.noreply.github.com>
2024-11-20 05:17:44 +0500

    updated files

--- leetcode/2516.take-k-of-each-character-from-left-and-right.py
+++ leetcode/2516.take-k-of-each-character-from-left-and-right.py
@@ -1,22 +1,20 @@
 from lc import *
 
-# https://leetcode.com/problems/take-k-of-each-character-from-left-and-right/discuss/2948183/Python-clean-12-line-sliding-window-solution-with-explanation
+# https://leetcode.com/problems/take-k-of-each-character-from-left-and-right/discuss/3100203/Python-short-and-clean-sliding-window-solution
 # TODO
 
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
-        t=j=0
-        d = {c:s.count(c)-k for c in 'abc'}
-        p = {c:0 for c in 'abc'}
-        if any(x<0 for x in d.values()):
+        j=t=0;d,p=Counter(s),Counter()
+        if any(k>d[c]for c in'abc'):
             return -1
         for i,c in enumerate(s):
-            p[c] += 1
-            while p[c]>d[c]:
-                p[s[j]] -= 1
+            p[c]+=1
+            while d[c]-p[c]<k:
+                p[s[j]]-=1
                 j += 1
-            t = max(t, i-j+1)
-        return len(s) - t
+            t=max(t,i-j+1)
+        return len(s)-t
 
 test('''
 2516. Take K of Each Character From Left and Right

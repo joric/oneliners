23805781e58211b630722a98af66edbd252161d4
joric <joric@users.noreply.github.com>
2024-11-23 05:03:19 +0500

    updated files

---
+++ leetcode/1861.rotating-the-box.py
@@ -0,0 +1,76 @@
+from lc import *
+
+# https://leetcode.com/problems/rotating-the-box/discuss/1246483/Python-3-one-line
+
+class Solution:
+    def rotateTheBox(self, b: List[List[str]]) -> List[List[str]]:
+        return zip(*['*'.join(''.join(sorted(p)[::-1])for p in''.join(r).split('*'))for r in b][::-1])
+
+test('''
+1861. Rotating the Box
+Medium
+
+991
+
+57
+
+Add to List
+
+Share
+You are given an m x n matrix of characters box representing a side-view of a box. Each cell of the box is one of the following:
+
+A stone '#'
+A stationary obstacle '*'
+Empty '.'
+The box is rotated 90 degrees clockwise, causing some of the stones to fall due to gravity. Each stone falls down until it lands on an obstacle, another stone, or the bottom of the box. Gravity does not affect the obstacles' positions, and the inertia from the box's rotation does not affect the stones' horizontal positions.
+
+It is guaranteed that each stone in box rests on an obstacle, another stone, or the bottom of the box.
+
+Return an n x m matrix representing the box after the rotation described above.
+
+ 
+
+Example 1:
+
+
+
+Input: box = [["#",".","#"]]
+Output: [["."],
+         ["#"],
+         ["#"]]
+Example 2:
+
+
+
+Input: box = [["#",".","*","."],
+              ["#","#","*","."]]
+Output: [["#","."],
+         ["#","#"],
+         ["*","*"],
+         [".","."]]
+Example 3:
+
+
+
+Input: box = [["#","#","*",".","*","."],
+              ["#","#","#","*",".","."],
+              ["#","#","#",".","#","."]]
+Output: [[".","#","#"],
+         [".","#","#"],
+         ["#","#","*"],
+         ["#","*","."],
+         ["#",".","*"],
+         ["#",".","."]]
+ 
+
+Constraints:
+
+m == box.length
+n == box[i].length
+1 <= m, n <= 500
+box[i][j] is either '#', '*', or '.'.
+Accepted
+57,132
+Submissions
+83,906
+''')

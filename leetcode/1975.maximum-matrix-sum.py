7fc254cab036e370eaadb6fa18e7c11243fdd4bd
joric <joric@users.noreply.github.com>
2024-11-24 05:06:18 +0500

    updated files

---
+++ leetcode/1975.maximum-matrix-sum.py
@@ -0,0 +1,55 @@
+from lc import *
+
+# https://leetcode.com/problems/maximum-matrix-sum/discuss/3088994/Python-3-line-solution-or-negative-frequency-is-evenodd
+
+class Solution:
+    def maxMatrixSum(self, m: List[List[int]]) -> int:
+        return sum(abs(x) for r in m for x in r)-2*(sum(x<1 for r in m for x in r)%2 and min(abs(x) for r in m for x in r)or 0)
+
+test('''
+1975. Maximum Matrix Sum
+Medium
+
+570
+
+25
+
+Add to List
+
+Share
+You are given an n x n integer matrix. You can do the following operation any number of times:
+
+Choose any two adjacent elements of matrix and multiply each of them by -1.
+Two elements are considered adjacent if and only if they share a border.
+
+Your goal is to maximize the summation of the matrix's elements. Return the maximum sum of the matrix's elements using the operation mentioned above.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,-1],[-1,1]]
+Output: 4
+Explanation: We can follow the following steps to reach sum equals 4:
+- Multiply the 2 elements in the first row by -1.
+- Multiply the 2 elements in the first column by -1.
+Example 2:
+
+
+Input: matrix = [[1,2,3],[-1,-2,-3],[1,2,3]]
+Output: 16
+Explanation: We can follow the following step to reach sum equals 16:
+- Multiply the 2 last elements in the second row by -1.
+ 
+
+Constraints:
+
+n == matrix.length == matrix[i].length
+2 <= n <= 250
+-105 <= matrix[i][j] <= 105
+Accepted
+20,967
+Submissions
+41,241
+''')

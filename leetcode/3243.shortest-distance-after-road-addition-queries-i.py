154122f8abee98f6f5231d07be4637fd4b74a20d
joric <joric@users.noreply.github.com>
2024-11-27 10:53:07 +0500

    updated files

--- leetcode/3243.shortest-distance-after-road-addition-queries-i.py
+++ leetcode/3243.shortest-distance-after-road-addition-queries-i.py
@@ -1,5 +1,31 @@
 from lc import *
 
+# https://leetcode.com/problems/shortest-distance-after-road-addition-queries-i/discuss/5583277/Python3-BFS
+
+class Solution:
+    def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(n)]
+        for i in range(n-1): 
+            graph[i].append(i+1)
+        def bfs(graph): 
+            queue = deque([0])
+            seen = {0}
+            ans = 0 
+            while queue:
+                for _ in range(len(queue)):
+                    u = queue.popleft()
+                    if u == n-1: return ans 
+                    for v in graph[u]: 
+                        if v not in seen: 
+                            seen.add(v)
+                            queue.append(v)
+                ans += 1
+        ans = []
+        for u,v in queries: 
+            graph[u].append(v)
+            ans.append(bfs(graph))
+        return ans 
+
 # https://leetcode.com/problems/shortest-distance-after-road-addition-queries-i/discuss/6087332/10-lines-easy-Dynamic-Programming-solution-or-Python
 
 class Solution:

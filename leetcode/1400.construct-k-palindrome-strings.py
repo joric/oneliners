c4f8da41968f7998637d2954491373616066a250
joric <joric@users.noreply.github.com>
2025-01-11 16:41:40 +0500

    updated files

--- leetcode/1400.construct-k-palindrome-strings.py
+++ leetcode/1400.construct-k-palindrome-strings.py
@@ -6,6 +6,10 @@ class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
         return sum(i&1 for i in Counter(s).values())<=k<=len(s)
 
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        c=Counter(s);return sum(c[k]&1 for k in c)<=k<=len(s)
+
 test('''
 1400. Construct K Palindrome Strings
 Solved

851fa041c222c5709b159915f3c01569b99e1f6b
joric <joric@users.noreply.github.com>
2025-01-21 05:20:41 +0500

    updated files

---
+++ leetcode/2017.grid-game.py
@@ -0,0 +1,105 @@
+from lc import *
+
+# https://leetcode.com/problems/grid-game
+
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        n = len(grid[0])
+        ans = math.inf
+        topSum = sum(grid[0])
+        bottomSum = 0
+        for i in range(n):
+            topSum -= grid[0][i]
+            ans = min(ans, max(topSum, bottomSum))
+            bottomSum += grid[1][i]
+        return ans
+
+class Solution:
+    def gridGame(self, g: List[List[int]]) -> int:
+        r,a,b=inf,sum(g[0]),0
+        for i in range(len(g[0])):
+            a -= g[0][i]
+            r = min(r,max(a,b))
+            b += g[1][i]
+        return r
+
+class Solution:
+    def gridGame(self, g: List[List[int]]) -> int:
+        a,b=sum(g[0]),0;return min(max(a:=a-g[0][i],(b:=b+g[1][i])-g[1][i])for i in range(len(g[0])))
+
+class Solution:
+    def gridGame(self, g: List[List[int]]) -> int:
+        a,b=sum(g[0]),0;return min(max(a:=a-x,(b:=b+y)-y)for x,y in zip(*g))
+
+test('''
+2017. Grid Game
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given a 0-indexed 2D array grid of size 2 x n, where grid[r][c] represents the number of points at position (r, c) on the matrix. Two robots are playing a game on this matrix.
+
+Both robots initially start at (0, 0) and want to reach (1, n-1). Each robot may only move to the right ((r, c) to (r, c + 1)) or down ((r, c) to (r + 1, c)).
+
+At the start of the game, the first robot moves from (0, 0) to (1, n-1), collecting all the points from the cells on its path. For all cells (r, c) traversed on the path, grid[r][c] is set to 0. Then, the second robot moves from (0, 0) to (1, n-1), collecting the points on its path. Note that their paths may intersect with one another.
+
+The first robot wants to minimize the number of points collected by the second robot. In contrast, the second robot wants to maximize the number of points it collects. If both robots play optimally, return the number of points collected by the second robot.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[2,5,4],[1,5,1]]
+Output: 4
+Explanation: The optimal path taken by the first robot is shown in red, and the optimal path taken by the second robot is shown in blue.
+The cells visited by the first robot are set to 0.
+The second robot will collect 0 + 0 + 4 + 0 = 4 points.
+Example 2:
+
+
+Input: grid = [[3,3,1],[8,5,2]]
+Output: 4
+Explanation: The optimal path taken by the first robot is shown in red, and the optimal path taken by the second robot is shown in blue.
+The cells visited by the first robot are set to 0.
+The second robot will collect 0 + 3 + 1 + 0 = 4 points.
+Example 3:
+
+
+Input: grid = [[1,3,1,15],[1,3,3,1]]
+Output: 7
+Explanation: The optimal path taken by the first robot is shown in red, and the optimal path taken by the second robot is shown in blue.
+The cells visited by the first robot are set to 0.
+The second robot will collect 0 + 1 + 3 + 3 + 0 = 7 points.
+ 
+
+Constraints:
+
+grid.length == 2
+n == grid[r].length
+1 <= n <= 5 * 104
+1 <= grid[r][c] <= 105
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+28K
+Submissions
+60K
+Acceptance Rate
+46.6%
+Topics
+Array
+Matrix
+Prefix Sum
+Companies
+Hint 1
+There are n choices for when the first robot moves to the second row.
+Hint 2
+Can we use prefix sums to help solve this problem?
+Similar Questions
+Minimum Penalty for a Shop
+Medium
+''')

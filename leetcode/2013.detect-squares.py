ec12a51c56df959dc80e7bf53b0a39288954c740
joric <joric@users.noreply.github.com>
2025-02-11 17:34:47 +0500

    updated files

--- leetcode/2013.detect-squares.py
+++ leetcode/2013.detect-squares.py
@@ -9,8 +9,10 @@ class DetectSquares:
         self.p.append(point)
     def count(self, point: List[int]) -> int:
         u,v = point;return sum(self.p.count([x,v])*self.p.count([u,y])for x,y in self.p if(u-x)**2==(v-y)**2>0)
-
+    
 DetectSquares=type('',(),{'__init__':lambda s:setattr(s,'p',[]),'add':lambda s,p:s.p.append(p),'count':lambda s,p:sum(s.p.count([p[0],y])*s.p.count([x,p[1]])for x,y in s.p if(p[0]-x)**2==(p[1]-y)**2>0)})
+
+DetectSquares=type('',(list,),{'add':list.append,'count':lambda s,p:sum((c:=list.count)(s,[p[0],y])*c(s,[x,p[1]])for x,y in s if(p[0]-x)**2==(p[1]-y)**2>0)})
 
 test('''
 2013. Detect Squares

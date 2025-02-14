551697580491de5523dd4379973ff6f69d9a6c99
joric <joric@users.noreply.github.com>
2025-02-14 19:19:05 +0500

    updated files

--- leetcode/530.minimum-absolute-difference-in-bst.py
+++ leetcode/530.minimum-absolute-difference-in-bst.py
@@ -1,5 +1,11 @@
 from lc import *
 
+# https://leetcode.com/problems/minimum-absolute-difference-in-bst/solutions/338515/python-recursive/
+
+class Solution:
+    def getMinimumDifference(self, r: Optional[TreeNode]) -> int:
+        return(f:=lambda t,a,b:t and min(f(t.left,a,t.val),f(t.right,t.val,b))or b-a)(r,-inf,inf)
+
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
         f=lambda n:n and f(n.left)+[n.val]+f(n.right)or[];return min(b-a for a,b in pairwise(f(root)))

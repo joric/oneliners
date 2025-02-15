ead386b38796325c33828e0f040558d7e4b63ad5
joric <joric@users.noreply.github.com>
2025-02-15 15:35:40 +0500

    updated files

--- leetcode/2698.find-the-punishment-number-of-an-integer.py
+++ leetcode/2698.find-the-punishment-number-of-an-integer.py
@@ -6,8 +6,6 @@ class Solution:
     def punishmentNumber(self, n: int) -> int:
         return sum(i*i for i in[0,1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000]if i<=n)
 
-# https://leetcode.com/problems/find-the-punishment-number-of-an-integer/description/?envType=daily-question&envId=2025-02-15
-
 class Solution:
     def punishmentNumber(self, n: int) -> int:
         def f(s, t):

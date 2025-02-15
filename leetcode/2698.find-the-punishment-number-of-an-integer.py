f4c346d1a23526b7ba0b2a75511e7d52075ef83d
joric <joric@users.noreply.github.com>
2025-02-15 15:34:33 +0500

    updated files

--- leetcode/2698.find-the-punishment-number-of-an-integer.py
+++ leetcode/2698.find-the-punishment-number-of-an-integer.py
@@ -28,7 +28,11 @@ class Solution:
 
 class Solution:
     def punishmentNumber(self, n: int) -> int:
-        f=lambda x,t:any(x==t or f(x//10**p,t-x%10**p)for p in(1,2,3)if x>0);return sum(x*x*f(x*x,x)for x in range(1,n+1))
+        return sum(x*x*(f:=lambda x,t:any(x==t or f(x//10**p,t-x%10**p)for p in(1,2,3)if x>0))(x*x,x)for x in range(1,n+1))
+
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        f=lambda x,t:x*any(x==t or f(x//10**p,t-x%10**p)for p in(1,2,3)if x>0);return sum(f(x*x,x)for x in range(1,n+1))
 
 test('''
 2698. Find the Punishment Number of an Integer

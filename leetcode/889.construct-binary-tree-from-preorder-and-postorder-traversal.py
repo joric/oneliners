54b7213d85774c08d81aab619e4559ef4cd494d7
joric <joric@users.noreply.github.com>
2025-02-23 18:21:37 +0500

    updated files

--- leetcode/889.construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ leetcode/889.construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -19,6 +19,10 @@ class Solution:
     def constructFromPrePost(self, a: List[int], b: List[int]) -> Optional[TreeNode]:
         return(f:=lambda a,b:{*a}&{*b}and TreeNode(x:=a.pop(0),f(a,b:=b[:b.index(x)]),f(a,b))or None)(a,b)
 
+class Solution:
+    def constructFromPrePost(self, a: List[int], b: List[int]) -> Optional[TreeNode]:
+        return(f:=lambda b:{*a}&{*b}and TreeNode(x:=a.pop(0),f(b:=b[:b.index(x)]),f(b))or None)(b)
+
 test('''
 889. Construct Binary Tree from Preorder and Postorder Traversal
 Medium

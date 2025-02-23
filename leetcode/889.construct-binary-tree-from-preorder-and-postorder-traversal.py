792dbb5ea84651279024e17216d4db0002669f56
joric <joric@users.noreply.github.com>
2025-02-23 05:15:10 +0500

    updated files

---
+++ leetcode/889.construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -0,0 +1,69 @@
+from lc import *
+
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/solutions/278586/easy-6-line-python-recursive/?envType=daily-question&envId=2025-02-23
+
+class Solution:
+    def constructFromPrePost(self, pre: List[int], post: List[int]) -> Optional[TreeNode]:
+        if pre and post and pre[0] in post:
+            rootval = pre.pop(0)
+            root = TreeNode(rootval)
+            root.left = self.constructFromPrePost(pre, post[:post.index(rootval)])
+            root.right = self.constructFromPrePost(pre, post[:post.index(rootval)])
+            return root
+
+class Solution:
+    def constructFromPrePost(self, e: List[int], o: List[int]) -> Optional[TreeNode]:
+        return(f:=lambda e,o:e and e[0]in o and TreeNode(x:=e.pop(0),f(e,o[:o.index(x)]),f(e,o[:o.index(x)]))or None)(e,o)
+
+class Solution:
+    def constructFromPrePost(self, e: List[int], o: List[int]) -> Optional[TreeNode]:
+        return(f:=lambda e,o:e and e[0]in o and TreeNode(x:=e.pop(0),f(e,o:=o[:o.index(x)]),f(e,o))or None)(e,o)
+
+test('''
+889. Construct Binary Tree from Preorder and Postorder Traversal
+Medium
+Topics
+Companies
+Given two integer arrays, preorder and postorder where preorder is the preorder traversal of a binary tree of distinct values and postorder is the postorder traversal of the same tree, reconstruct and return the binary tree.
+
+If there exist multiple answers, you can return any of them.
+
+ 
+
+Example 1:
+
+
+Input: preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1]
+Output: [1,2,3,4,5,6,7]
+Example 2:
+
+Input: preorder = [1], postorder = [1]
+Output: [1]
+ 
+
+Constraints:
+
+1 <= preorder.length <= 30
+1 <= preorder[i] <= preorder.length
+All the values of preorder are unique.
+postorder.length == preorder.length
+1 <= postorder[i] <= postorder.length
+All the values of postorder are unique.
+It is guaranteed that preorder and postorder are the preorder traversal and postorder traversal of the same binary tree.
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+111.4K
+Submissions
+154.6K
+Acceptance Rate
+72.1%
+Topics
+Array
+Hash Table
+Divide and Conquer
+Tree
+Binary Tree
+''')

5145b670c9d0e221278baeda3a97d21649c927d3
joric <joric@users.noreply.github.com>
2025-03-16 09:44:37 +0500

    updated files

--- leetcode/2594.minimum-time-to-repair-cars.py
+++ leetcode/2594.minimum-time-to-repair-cars.py
@@ -6,6 +6,16 @@ class Solution:
     def repairCars(self, r: List[int], c: int) -> int:
         return bisect_left(range(c*c*min(r)),c,key=lambda m:sum(isqrt(m//x)for x in r))
 
+# https://leetcode.com/problems/minimum-time-to-repair-cars/solutions/3311975/c-java-python3-binary-search-1-liner/?envType=daily-question&envId=2025-03-16
+
+class Solution:
+    def repairCars(self, r: List[int], c: int) -> int:
+        return bisect_left(range(10**14),c,key=lambda m:sum(isqrt(m//x)for x in r))
+
+class Solution:
+    def repairCars(self, r: List[int], c: int) -> int:
+        return bisect_left(range(9**15),c,key=lambda m:sum(isqrt(m//x)for x in r))
+
 test('''
 2594. Minimum Time to Repair Cars
 Medium

eebda197d5ebcb1ec8fc4019f8481736a2bc6715
joric <joric@users.noreply.github.com>
2025-04-23 14:31:53 +0500

    updated files

--- leetcode/1399.count-largest-group.py
+++ leetcode/1399.count-largest-group.py
@@ -10,6 +10,14 @@ class Solution:
 
 class Solution:
     def countLargestGroup(self, n: int) -> int:
+        return len(multimode(map(sum,map(map,int,map(str,range(1,n+1))))))
+
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        return len(multimode(sum(map(int,str(i)))for i in range(1,n+1)))
+
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
         return len(multimode(sum(map(int,str(i+1)))for i in range(n)))
 
 test('''

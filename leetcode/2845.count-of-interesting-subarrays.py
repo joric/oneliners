cb28f6793903dff02717983594fd638a9841ca6f
joric <joric@users.noreply.github.com>
2025-04-25 10:51:39 +0500

    updated files

--- leetcode/2845.count-of-interesting-subarrays.py
+++ leetcode/2845.count-of-interesting-subarrays.py
@@ -18,6 +18,10 @@ class Solution:
     def countInterestingSubarrays(self, a: List[int], m: int, k: int) -> int:
         p=Counter([0]);return sum((p[(q-k)%m],p.update([q%m]))[0]for q in accumulate(v%m==k for v in a))
 
+class Solution:
+    def countInterestingSubarrays(self, a: List[int], m: int, k: int) -> int:
+        c,p=0,Counter([0]);return sum((p[((c:=c+(x%m==k))-k)%m],p.update([c%m]))[0]for x in a)
+
 test('''
 2845. Count of Interesting Subarrays
 Solved

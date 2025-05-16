215dd43280b28ec8e20032d908a11311a644827f
joric <joric@users.noreply.github.com>
2025-05-16 05:10:25 +0500

    updated files

--- leetcode/2901.longest-unequal-adjacent-groups-subsequence-ii.py
+++ leetcode/2901.longest-unequal-adjacent-groups-subsequence-ii.py
@@ -6,9 +6,7 @@ class Solution:
     def getWordsInLongestSubsequence(self, w: List[str], g: List[int]) -> List[str]:
         dp = []
         for s,g1 in zip(w,g):
-            dp.append(max((q for t,g2,q in zip(w,g,dp) 
-                    if g1!=g2 and len(s)==len(t) and sum(map(ne,s,t))<2),
-                key=len,default=[]) + [s])
+            dp.append(max((q for t,g2,q in zip(w,g,dp) if g1!=g2 and len(s)==len(t) and sum(map(ne,s,t))<2),key=len,default=[])+[s])
         return max(dp,key=len)
 
 # TODO failing test suite

1170cadd7bc38fbd2edeb26d0d1180793c3702dd
joric <joric@users.noreply.github.com>
2025-06-10 16:27:00 +0500

    updated files

--- leetcode/3442.maximum-difference-between-even-and-odd-frequency-i.py
+++ leetcode/3442.maximum-difference-between-even-and-odd-frequency-i.py
@@ -33,6 +33,10 @@ class Solution:
     def maxDifference(self, s: str) -> int:
         v=Counter(s).values();return max(x%2*x for x in v)-min(x for x in v if~x%2)
 
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        v=Counter(s).values();return max(x%2*x for x in v)-min(x+x%2*99for x in v)
+
 test('''
 3442. Maximum Difference Between Even and Odd Frequency I
 Solved

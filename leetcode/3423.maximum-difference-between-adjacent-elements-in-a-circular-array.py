301bae81c188d20b4a3ba18e0f187c85504d72ae
joric <joric@users.noreply.github.com>
2025-06-12 05:15:07 +0500

    updated files

---
+++ leetcode/3423.maximum-difference-between-adjacent-elements-in-a-circular-array.py
@@ -0,0 +1,72 @@
+from lc import *
+
+# https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/solutions/6299571/python-one-line/?envType=daily-question&envId=2025-06-12
+
+class Solution:
+    def maxAdjacentDistance(self, a: List[int]) -> int:
+        return max(map(abs,starmap(sub,pairwise(a+[a[0]]))))
+
+class Solution:
+    def maxAdjacentDistance(self, a: List[int]) -> int:
+        return max(abs(a[i]-a[i - 1])for i in range(len(a)))
+
+class Solution:
+    def maxAdjacentDistance(self, a: List[int]) -> int:
+        return max(abs(x-y)for x,y in pairwise(a+[a[0]]))
+
+test('''
+3423. Maximum Difference Between Adjacent Elements in a Circular Array
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given a circular array nums, find the maximum absolute difference between adjacent elements.
+
+Note: In a circular array, the first and last elements are adjacent.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,4]
+
+Output: 3
+
+Explanation:
+
+Because nums is circular, nums[0] and nums[2] are adjacent. They have the maximum absolute difference of |4 - 1| = 3.
+
+Example 2:
+
+Input: nums = [-5,-10,-5]
+
+Output: 5
+
+Explanation:
+
+The adjacent elements nums[0] and nums[1] have the maximum absolute difference of |-5 - (-10)| = 5.
+
+ 
+
+Constraints:
+
+2 <= nums.length <= 100
+-100 <= nums[i] <= 100
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+38,584/57.8K
+Acceptance Rate
+66.8%
+Topics
+Array
+icon
+Companies
+Hint 1
+Traverse from the second element to the last element and check the difference of every adjacent pair.
+Hint 2
+The edge case is to check the difference between the first and last elements.
+''')

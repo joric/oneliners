03f2f5e3d20d372b3e16159e3116bf0765c75332
joric <joric@users.noreply.github.com>
2025-06-18 07:00:22 +0500

    updated files

--- leetcode/2966.divide-array-into-arrays-with-max-difference.py
+++ leetcode/2966.divide-array-into-arrays-with-max-difference.py
@@ -57,6 +57,10 @@ class Solution:
     def divideArray(self, a: List[int], k: int) -> List[List[int]]:
         return(r:=[*zip(*[iter(sorted(a))]*3)])*all(q-p<=k for p,_,q in r)
 
+class Solution:
+    def divideArray(self, a: List[int], k: int) -> List[List[int]]:
+        r=*zip(*[iter(sorted(a))]*3),;return r*all(q-p<=k for p,_,q in r)
+
 test(''''
 2966. Divide Array Into Arrays With Max Difference
 Medium

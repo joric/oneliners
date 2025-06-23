a1b5544cb0cab6a07fabd31181f8963ecb964b6d
joric <joric@users.noreply.github.com>
2025-06-23 05:21:30 +0500

    updated files

--- leetcode/2081.sum-of-k-mirror-numbers.py
+++ leetcode/2081.sum-of-k-mirror-numbers.py
@@ -4,23 +4,24 @@ from lc import *
 
 class Solution:
     def kMirror(self, k: int, n: int) -> int:
-        def f(x): # Return next k-symmetric number
-            n = len(x)//2
-            for i in range(n, len(x)): 
-                if int(x[i])+1 < k: 
-                    x[i] = x[~i] = str(int(x[i])+1)
-                    for ii in range(n, i): x[ii] = x[~ii] = '0'
+        def f(x):
+            m = len(x) // 2
+            for i in range(m, len(x)):
+                if int(x[i]) + 1 < k:
+                    x[i] = x[~i] = str(int(x[i]) + 1)
+                    for j in range(m, i):
+                        x[j] = x[~j] = '0'
                     return x
-            return ["1"] + ["0"]*(len(x)-1) + ["1"]
-        x = ["0"]
-        ans = 0
-        for _ in range(n): 
-            while True: 
+            return ['1'] + ['0'] * (len(x) - 1) + ['1']
+        x, r = ['0'], 0
+        for _ in range(n):
+            while True:
                 x = f(x)
-                val = int("".join(x), k)
-                if str(val)[::-1] == str(val): break
-            ans += val
-        return ans 
+                v = int(''.join(x), k)
+                if str(v) == str(v)[::-1]:
+                    break
+            r += v
+        return r
 
 # TODO
 class Solution:

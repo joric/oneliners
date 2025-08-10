b3e55fafcb00f35ca3636206aa9afcdb6e038dc4
joric <joric@users.noreply.github.com>
2025-08-10 08:52:51 +0500

    updated files

--- leetcode/869.reordered-power-of-2.py
+++ leetcode/869.reordered-power-of-2.py
@@ -4,15 +4,15 @@ class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         return ''.join(sorted(str(n))) in set(["1", "2", "4", "8", "16", "23", "46", "128", "256", "125", "0124", "0248", "0469", "1289", "13468", "23678", "35566", "011237", "122446", "224588", "0145678", "0122579", "0134449", "0368888", "11266777", "23334455", "01466788", "112234778", "234455668", "012356789", "0112344778"])
 
-class Solution:
-    def reorderedPowerOf2(self, n: int) -> bool:
-        return any(sorted(str(n))==sorted(str(1<<i))for i in range(30))
-
 # POTD 2025-08-10
 
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         return any(eq(*map(sorted,map(str,(n,1<<i))))for i in range(30))
+
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        return any(sorted(str(n))==sorted(str(1<<i))for i in range(30))
 
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:

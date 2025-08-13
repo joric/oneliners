8a8e7c633da19a43f775b48df834acf072f2b1b8
joric <joric@users.noreply.github.com>
2025-08-13 13:28:02 +0500

    updated files

--- leetcode/326.power-of-three.py
+++ leetcode/326.power-of-three.py
@@ -4,6 +4,10 @@ from lc import *
 
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
+        return n>0==pow(3,19,n)
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
         return n>0==3**19%n
 
 test('''

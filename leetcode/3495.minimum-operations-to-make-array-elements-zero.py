11cfa48bd537473b12bb760cf2d4e55a86303b01
joric <joric@users.noreply.github.com>
2025-09-06 05:23:38 +0500

    updated files

--- leetcode/3495.minimum-operations-to-make-array-elements-zero.py
+++ leetcode/3495.minimum-operations-to-make-array-elements-zero.py
@@ -26,10 +26,6 @@ class Solution:
 
 class Solution:
     def minOperations(self, q: List[List[int]]) -> int:
-        return sum((sum((-~i*(min(r,4**-~i-1)+1-max(l,4**i)))for i in range(r.bit_length()//2+2)if max(l,4**i)<=min(r,4**-~i-1))+1)//2 for l,r in q)
-
-class Solution:
-    def minOperations(self, q: List[List[int]]) -> int:
         return sum((sum((-~i*(min(r,4**-~i-1)+1-max(l,4**i)))for i in range(16)if max(l,4**i)<=min(r,4**-~i-1))+1)//2 for l,r in q)
 
 test('''

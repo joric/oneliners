25c6f84c4e1afd93e0bae0577e685def71ca97cb
joric <joric@users.noreply.github.com>
2025-09-06 05:30:54 +0500

    updated files

--- leetcode/3495.minimum-operations-to-make-array-elements-zero.py
+++ leetcode/3495.minimum-operations-to-make-array-elements-zero.py
@@ -28,6 +28,14 @@ class Solution:
     def minOperations(self, q: List[List[int]]) -> int:
         return sum((sum((-~i*(min(r,4**-~i-1)+1-max(l,4**i)))for i in range(16)if max(l,4**i)<=min(r,4**-~i-1))+1)//2 for l,r in q)
 
+class Solution:
+    def minOperations(self, q: List[List[int]]) -> int:
+        return sum(-~sum(-~i*(b+1-a)for i in range(16)if(a:=max(l,4**i))<=(b:=min(r,4**-~i-1)))//2 for l,r in q)
+
+class Solution:
+    def minOperations(self, q: List[List[int]]) -> int:
+        return sum(-~sum(-~i*-~t for i in range(16)if(t:=min(r,4**-~i-1)-max(l,4**i))>=0)//2 for l,r in q)
+
 test('''
 3495. Minimum Operations to Make Array Elements Zero
 Solved

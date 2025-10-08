a443c3d4d2cba686029285fe55928a0c3c35cc02
joric <joric@users.noreply.github.com>
2025-10-08 05:06:35 +0500

    updated files

--- leetcode/2300.successful-pairs-of-spells-and-potions.py
+++ leetcode/2300.successful-pairs-of-spells-and-potions.py
@@ -1,8 +1,14 @@
 from lc import *
 
+# https://leetcode.com/problems/successful-pairs-of-spells-and-potions/?envType=daily-question&envId=2025-10-08
+
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         return (lambda p:[len(p)-bisect_left(p,(success+a-1)//a) for a in spells])(sorted(potions))
+
+class Solution:
+    def successfulPairs(self, s: List[int], p: List[int], c: int) -> List[int]:
+        p.sort();return[len(p)-bisect_left(p,(c+a-1)//a)for a in s]
 
 test('''
 2300. Successful Pairs of Spells and Potions

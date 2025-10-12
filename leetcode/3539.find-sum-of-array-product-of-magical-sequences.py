71c038a2f16679269d4534974906fb75753e746b
joric <joric@users.noreply.github.com>
2025-10-12 09:15:56 +0500

    updated files

--- leetcode/3539.find-sum-of-array-product-of-magical-sequences.py
+++ leetcode/3539.find-sum-of-array-product-of-magical-sequences.py
@@ -33,11 +33,7 @@ class Solution:
 
 class Solution:
     def magicalSum(self, m: int, k: int, a: List[int]) -> int:
-        return(f:=cache(lambda x,y,i,b:0 if x<0 or y<0 or x+b.bit_count()<y else int(y==b.bit_count())if x==0 else i<len(a) and sum(comb(x,c)*pow(a[i],c)*f(x-c,y-((d:=b+c)%2),i+1,d//2)for c in range(x+1))%(10**9+7)))(m,k,0,0)
-
-class Solution:
-    def magicalSum(self, m: int, k: int, a: List[int]) -> int:
-        return(f:=cache(lambda x,y,i,b:0 if y>x+(t:=b.bit_count())or x<0 or y<0 else y==t if x==0 else i<len(a)and sum(comb(x,c)*a[i]**c*f(x-c,y-((d:=b+c)%2),i+1,d//2)for c in range(x+1))%(10**9+7)))(m,k,0,0)
+        return(f:=cache(lambda x,y,i,b:y>-1<y<=x+(t:=b.bit_count())and(i<len(a)and sum(comb(x,c)*a[i]**c*f(x-c,y-((d:=b+c)%2),i+1,d//2)for c in range(x+1))%(10**9+7)if x else y==t)))(m,k,0,0)
 
 test('''
 3539. Find Sum of Array Product of Magical Sequences

356c282bfcd76471edaf448694f7580837371cc5
joric <joric@users.noreply.github.com>
2025-10-15 16:07:11 +0500

    updated files

--- leetcode/3350.adjacent-increasing-subarrays-detection-ii.py
+++ leetcode/3350.adjacent-increasing-subarrays-detection-ii.py
@@ -64,7 +64,6 @@ class Solution:
     def maxIncreasingSubarrays(self, a: list[int]) -> int:
         u=p=0;return max((a[i]>a[i-1]and(u:=u+1)or(p:=u,u:=1))and max(u//2,min(p,u))for i in range(len(a)))
 
-
 test('''
 3350. Adjacent Increasing Subarrays Detection II
 Medium

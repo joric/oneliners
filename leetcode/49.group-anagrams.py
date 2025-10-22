621211e09101cd8442f2246681ab6515bbb33ba5
joric <joric@users.noreply.github.com>
2025-10-22 14:01:17 +0500

    updated files

--- leetcode/49.group-anagrams.py
+++ leetcode/49.group-anagrams.py
@@ -1,10 +1,21 @@
 from lc import *
 
+# https://leetcode.com/problems/group-anagrams/
+
+# return[*zip(*groupby(f(s,key=f),f))][1] does not work
+'''
+class Solution:
+    def groupAnagrams(self, s: List[str]) -> List[List[str]]:
+        f=sorted;return[*zip(*groupby(f(s,key=f),f))] - nope
+'''
+
+class Solution:
+    def groupAnagrams(self, s: List[str]) -> List[List[str]]:
+        f=sorted;return map(itemgetter(1),groupby(f(s,key=f),f))
+
 class Solution:
     def groupAnagrams(self, s: List[str]) -> List[List[str]]:
         f=sorted;return(v for _,v in groupby(f(s,key=f),f))
-
-# return[*zip(*groupby(f(s,key=f),f))][1] does not work
 
 test('''
 49. Group Anagrams

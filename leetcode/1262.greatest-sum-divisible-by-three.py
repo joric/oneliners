4a90581e20cffd68ad7028dd24fd53cc220fec47
joric <joric@users.noreply.github.com>
2025-11-23 16:43:34 +0500

    updated files

--- leetcode/1262.greatest-sum-divisible-by-three.py
+++ leetcode/1262.greatest-sum-divisible-by-three.py
@@ -25,6 +25,10 @@ class Solution:
     def maxSumDivThree(self, a: List[int]) -> int:
         return(f:=cache(lambda i,r:i<len(a)and max(f(i+1,r),a[i]+f(i+1,(r+a[i])%3))or r and-inf))(0,0)
 
+class Solution:
+    def maxSumDivThree(self, a: List[int]) -> int:
+        return(f:=cache(lambda i,r:i<len(a)and max(f(i+1,r),a[i]+f(i+1,(r+a[i])%3))or r and-99))(0,0)
+
 test('''
 1262. Greatest Sum Divisible by Three
 Solved

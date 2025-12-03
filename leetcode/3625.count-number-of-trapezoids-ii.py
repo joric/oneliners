5e03ad5f34e7754f53f2559ef0f2afa91e8959e6
joric <joric@users.noreply.github.com>
2025-12-03 06:22:52 +0500

    updated files

--- leetcode/3625.count-number-of-trapezoids-ii.py
+++ leetcode/3625.count-number-of-trapezoids-ii.py
@@ -8,20 +8,17 @@ class Solution:
         lines = Counter()
         mids = Counter()
         midlines = Counter()
-
         for (x1, y1), (x2, y2) in combinations(points, 2):
             dx, dy = x2 - x1, y2 - y1
             g = gcd(dx, dy)
             dx, dy = dx // g, dy // g
             if dx < 0 or (dx == 0 and dy < 0):
                 dx, dy = -dx, -dy
-
             inter = dx * y1 - dy * x1
             slopes[dx, dy] += 1
             lines[dx, dy, inter] += 1
             mids[x1 + x2, y1 + y2] += 1
             midlines[x1 + x2, y1 + y2, dx, dy, inter] += 1
-
         ans = sum(comb(v, 2) for v in slopes.values())
         ans -= sum(comb(v, 2) for v in lines.values())
         ans -= sum(comb(v, 2) for v in mids.values())

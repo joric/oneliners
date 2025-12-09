68eaa62e91e4c3a8d718e985b45924d7a9de508f
joric <joric@users.noreply.github.com>
2025-12-09 05:28:04 +0500

    updated files

--- leetcode/3583.count-special-triplets.py
+++ leetcode/3583.count-special-triplets.py
@@ -14,11 +14,7 @@ class Solution:
 
 class Solution:
     def specialTriplets(self, a: List[int]) -> int:
-        z1,z2=Counter(),Counter(); return sum((z2[v/2],z2.update({v:z1[v*2]}),z1.update([v]))[0]for v in a)%(10**9+7)
-
-class Solution:
-    def specialTriplets(self, a: List[int]) -> int:
-        f=lambda a,z:[(z[v*2],z.update([v]))[0]for v in a];return sum(map(mul,f(a,Counter()),f(a[::-1],Counter())[::-1]))%(10**9+7)
+        z1,z2=Counter(),Counter();return sum((z2[v/2],z2.update({v:z1[v*2]}),z1.update([v]))[0]for v in a)%(10**9+7)
 
 class Solution:
     def specialTriplets(self, a: List[int]) -> int:

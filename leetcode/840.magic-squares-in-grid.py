0a0365b23c629c9c01e5e128c842ee4b79f0f308
joric <joric@users.noreply.github.com>
2025-12-30 10:37:23 +0500

    updated files

--- leetcode/840.magic-squares-in-grid.py
+++ leetcode/840.magic-squares-in-grid.py
@@ -1,5 +1,12 @@
 from lc import *
 
+
+# https://leetcode.com/problems/magic-squares-in-grid/solutions/7449444/one-line-solution-by-xxxxkav-v058/
+
+class Solution:
+    def numMagicSquaresInside(self, g: List[List[int]]) -> int:
+        return sum(''.join(map(str,g[i][j:j+3]+g[i+1][j:j+3]+g[i+2][j:j+3]))in{'276951438','294753618','438951276','492357816','618753294','672159834','816357492','834159672'}for i,j in product(range(len(g)-2), range(len(g[0])-2)))
+
 # https://leetcode.com/problems/magic-squares-in-grid/discuss/569678/Python-3-one-line
 
 class Solution:

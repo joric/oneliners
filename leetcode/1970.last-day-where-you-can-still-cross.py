6af419cb5054fe16985645b830ccfcd594d86dd1
joric <joric@users.noreply.github.com>
2025-12-31 05:22:23 +0500

    updated files

--- leetcode/1970.last-day-where-you-can-still-cross.py
+++ leetcode/1970.last-day-where-you-can-still-cross.py
@@ -108,6 +108,18 @@ class Solution:
     def latestDayToCross(self, r: int, c: int, s: List[List[int]]) -> int:
         v,j=range,"".join;u=j(f'A{j(chr(i*c+k+67)for k in v(c-2))}B'for i in v(r));w=set();return next(i for i,(p,q)in enumerate(s)if(x:=p-1,y:=q-1,w.add((x,y)),[(b:=d+x,a:=e+y,r>b>=0<=a<c and(b,a)in w and(u:=u.replace(u[x*c+y],u[b*c+a])))for d,e in product(*[[-1,0,1]]*2)])and u[0]==u[-1])
 
+class Solution:
+    def latestDayToCross(self, r: int, c: int, s: List[List[int]]) -> int:
+        v,j=range,''.join;u=j(f'A{j(chr(i*c+k+67)for k in v(c-2))}B'for i in v(r));w=set();return next(i for i,(p,q)in enumerate(s)if(x:=p-1,y:=q-1,w.add((x,y)),[((b:=d+x,a:=e+y)in w and(r>b>=0<=a<c)and(u:=u.replace(u[x*c+y],u[b*c+a])))for d,e in product(*[[-1,0,1]]*2)])and u[0]==u[-1])
+
+class Solution:
+    def latestDayToCross(self, r: int, c: int, s: List[List[int]]) -> int:
+        w,v,j=set(),range,''.join;u=j(f'A{j(chr(i*c+k+67)for k in v(c-2))}B'for i in v(r));return next(i for i,(p,q)in enumerate(s)if(x:=p-1,y:=q-1,w.add((x,y)),[((b:=d+x,a:=e+y)in w and r>b>=0<=a<c!=(u:=u.replace(u[x*c+y],u[b*c+a])))for d,e in product(*[[-1,0,1]]*2)])!=u[0]==u[-1])
+
+class Solution:
+    def latestDayToCross(self, r: int, c: int, s: List[List[int]]) -> int:
+        w,v,j=set(),range,''.join;u=j(f'A{j(chr(i*c+k+67)for k in v(c-2))}B'for i in v(r));return next(i for i,(p,q)in enumerate(s)if(w.add((x:=p-1,y:=q-1)),[r>b>-1<a<c!=(b,a)in w and(u:=u.replace(u[x*c+y],u[b*c+a]))for b in v(x-1,x+2)for a in v(y-1,y+2)])!=u[0]==u[-1])
+
 test('''
 1970. Last Day Where You Can Still Cross
 Hard

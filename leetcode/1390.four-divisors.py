c3c666d1437b3dabdfb0d0836288eebe4bf6d560
joric <joric@users.noreply.github.com>
2026-01-04 06:49:31 +0500

    updated files

--- leetcode/1390.four-divisors.py
+++ leetcode/1390.four-divisors.py
@@ -1,6 +1,26 @@
 from lc import *
 
+# https://leetcode.com/problems/four-divisors/solutions/860814/clean-and-short-python-by-daciuk-bhs0/
+
+class Solution:
+    def sumFourDivisors(self, a: List[int]) -> int:
+        @cache
+        def f(x):
+            s=set()
+            for i in range(1,ceil(sqrt(x))+1):
+                x%i<1!=s.update({i,x//i})
+                if len(s)>4:
+                    return 0
+            return sum(s)*(len(s)==4)
+        return sum(map(f,a))
+
 # https://leetcode.com/problems/four-divisors/solutions/1434020/python3-dynamic-programming-7-lines-by-r-7fwh/?envType=daily-question&envId=2026-01-04
+
+class Solution:
+    def sumFourDivisors(self, a: List[int]) -> int:
+        n=max(a)+1;p,q=[1]*n,[1]*n;
+        [setitem(p,j,p[j]+i)or setitem(q,j,q[j]+1)for i in range(2,n)for j in range(i,n,i)];
+        return sum(p[x]*(q[x]==4)for x in a)
 
 class Solution:
     def sumFourDivisors(self, a: List[int]) -> int:
@@ -22,6 +42,7 @@ Example 1:
 
 Input: nums = [21,4,7]
 Output: 32
+
 Explanation: 
 21 has 4 divisors: 1, 3, 7, 21
 4 has 3 divisors: 1, 2, 4
@@ -35,7 +56,6 @@ Example 3:
 
 Input: nums = [1,2,3,4,5]
 Output: 0
- 
 
 Constraints:
 

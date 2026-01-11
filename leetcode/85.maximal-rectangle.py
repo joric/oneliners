a84468b0e3613a3ec82b0815778b135809b83c68
joric <joric@users.noreply.github.com>
2026-01-11 09:00:01 +0500

    updated files

--- leetcode/85.maximal-rectangle.py
+++ leetcode/85.maximal-rectangle.py
@@ -20,6 +20,58 @@ class Solution:
     def maximalRectangle(self, a: list[list[str]]) -> int:
         m,n,r=len(a),len(a[0]),0;d=[0]*(n+1);[(q:=deque([-1]),[(setitem(d,j,1+d[j]if j<n and a[i][j]=='1'else 0),all(d[q[0]]>d[j]and(r:=max(r,d[q.popleft()]*(j-q[0]-1)))for _ in d),q.appendleft(j))for j in range(n+1)])for i in range(m)];return r
 
+# https://leetcode.com/problems/maximal-rectangle/solutions/2892403/short-simple-by-macraiu-qhys/?envType=daily-question&envId=2026-01-11
+
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        maxarea = 0
+        Y = len(matrix)
+        X = len(matrix[0])
+        for i in range(Y):
+            for j in range(X):
+                if(maxarea > (Y - i) * (X - j)):
+                    break
+                row =  0
+                col =  X - j 
+                for k in range(i, Y):
+                    for l in range(j, j + col):
+                        if matrix[k][l] == "0":
+                            col  = min(col, l - j)
+                            break
+                    row += 1
+                    maxarea = max(maxarea, col * row)
+        return maxarea
+
+class Solution:
+    def maximalRectangle(self,m:List[List[str]])->int:
+        a=0
+        h=len(m)
+        w=len(m[0])
+        for i in range(h):
+            for j in range(w):
+                if a>(h-i)*(w-j):
+                    break
+                r=0
+                c=w-j
+                for k in range(i,h):
+                    for l in range(j,j+c):
+                        if m[k][l]<'1':
+                            c=min(c,l-j)
+                            break
+                    r+=1
+                    a=max(a,c*r)
+        return a
+
+class Solution:
+    def maximalRectangle(self,m:List[List[str]])->int:
+        a,h,w=0,len(m),len(m[0]);[(r:=0,c:=w-j,[(c:=next((min(c,l-j)for l in range(j,j+c)if'1'>m[k][l]),c),a:=max(a,c*(r:=r+1)))for k in range(i,h)])for i in range(h)for j in range(w)if(h-i)*(w-j)>=a];return a
+
+# gemini 3 pro
+
+class Solution:
+    def maximalRectangle(self,m:List[List[str]])->int:
+        m=[int("".join(r),2)for r in m];a=0;[(x:=-1,[(x:=x&y,a:=max(a,len(max(f"{x:b}".split('0'),key=len))*(k+1)))for k,y in enumerate(m[i:])])for i in range(len(m))];return a
+
 test('''
 85. Maximal Rectangle
 Hard

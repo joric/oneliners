a143fac9c8140d62b5bcae3f6dd6877c1c4c10f4
joric <joric@users.noreply.github.com>
2026-01-13 09:20:40 +0500

    updated files

---
+++ leetcode/3453.separate-squares-i.py
@@ -0,0 +1,93 @@
+from lc import *
+
+# https://leetcode.com/problems/separate-squares-i/solutions/6504480/on-if-not-for-pythons-sort-being-on-log-0nw18/?envType=daily-question&envId=2026-01-13
+
+class Solution:
+    def separateSquares(self, squares: List[List[int]]) -> float:
+        h, w, a, ta = 0, 0, 0, sum(l*l for _,_,l in squares)
+        for y, l in sorted(e for _,y,l in squares for e in [(y, l), (y+l, -l)]):
+            if(((a := a + w * (y - h)) * 2) >= ta):
+                return y - ((a - (ta / 2)) / w)
+            h, w = y, w + l
+
+class Solution:
+    def separateSquares(self, s: List[List[int]]) -> float:
+        h=w=a=0;t=sum(l*l for _,_,l in s);return next(y-((a-(t/2))/w)for y,l in sorted(e for _,y,l in s for e in [(y,l),(y+l,-l)])if not(t>((a:=a+w*(y-h))*2)!=(h:=y,w:=w+l)))
+
+class Solution:
+    def separateSquares(self, s: List[List[int]]) -> float:
+        h=w=a=0;t=sum(l*l for*_,l in s)/2;return next(y-(a-t)/w for y,l in sorted(e for*_,y,l in s for e in[(y,l),(y+l,-l)])if t<=(a:=a+w*(y-h))or(h:=y,w:=w+l)==0)
+
+test('''
+3453. Separate Squares I
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given a 2D integer array squares. Each squares[i] = [xi, yi, li] represents the coordinates of the bottom-left point and the side length of a square parallel to the x-axis.
+
+Find the minimum y-coordinate value of a horizontal line such that the total area of the squares above the line equals the total area of the squares below the line.
+
+Answers within 10-5 of the actual answer will be accepted.
+
+Note: Squares may overlap. Overlapping areas should be counted multiple times.
+
+ 
+
+Example 1:
+
+Input: squares = [[0,0,1],[2,2,1]]
+
+Output: 1.00000
+
+Explanation:
+
+
+
+Any horizontal line between y = 1 and y = 2 will have 1 square unit above it and 1 square unit below it. The lowest option is 1.
+
+Example 2:
+
+Input: squares = [[0,0,2],[1,1,1]]
+
+Output: 1.16667
+
+Explanation:
+
+
+
+The areas are:
+
+Below the line: 7/6 * 2 (Red) + 1/6 (Blue) = 15/6 = 2.5.
+Above the line: 5/6 * 2 (Red) + 5/6 (Blue) = 15/6 = 2.5.
+Since the areas above and below the line are equal, the output is 7/6 = 1.16667.
+
+ 
+
+Constraints:
+
+1 <= squares.length <= 5 * 104
+squares[i] = [xi, yi, li]
+squares[i].length == 3
+0 <= xi, yi <= 109
+1 <= li <= 109
+The total area of all the squares will not exceed 1012.
+ 
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+36,439/82.7K
+Acceptance Rate
+44.1%
+Topics
+Array
+Binary Search
+Biweekly Contest 150
+icon
+Companies
+Hint 1
+Binary search on the answer.
+''')

37f23c75db718276232d7b409815d7724651e8de
joric <joric@users.noreply.github.com>
2026-01-14 11:26:48 +0500

    updated files

--- leetcode/3454.separate-squares-ii.py
+++ leetcode/3454.separate-squares-ii.py
@@ -36,6 +36,21 @@ class Solution:
 
 class Solution:
     def separateSquares(self, a: List[List[int]]) -> float:
+        e={};c=[];k=[];g=0;
+        for l,d,s in a:
+            for i,t in((1,d),(0,d+s)):
+                e.setdefault(t,[]).append((i,l,l+s))
+        for y,n in pairwise(sorted(e)):
+            for i,*x in e[y]:
+                c.append(x)if i else c.remove(x)
+            u=w=0
+            for x,v in sorted(c):
+                u<v and(w:=w+v-max(u,x),u:=v)
+            k.append((g:=g+(n-y)*w,w,n))
+        return next(n-(t-g/2)/w for t,w,n in k if t>=g/2)
+
+class Solution:
+    def separateSquares(self, a: List[List[int]]) -> float:
         e={};c=[];k=[];g=0;[e.setdefault(t,[]).append((i,l,l+s))for l,d,s in a for i,t in((1,d),(0,d+s))];[([c.append(x)if i else c.remove(x)for i,*x in e[y]],u:=0,w:=0,[u<v and(w:=w+v-max(u,x),u:=v)for x,v in sorted(c)],k.append((g:=g+(n-y)*w,w,n)))for y,n in pairwise(sorted(e))];return next(n-(t-g/2)/w for t,w,n in k if t>=g/2)
 
 test('''

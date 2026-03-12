2867c66361c6b3e9769fc452774db8224a74089a
joric <joric@users.noreply.github.com>
2026-03-13 00:03:56 +0500

    updated files

--- leetcode/3296.minimum-number-of-seconds-to-make-mountain-height-zero.py
+++ leetcode/3296.minimum-number-of-seconds-to-make-mountain-height-zero.py
@@ -28,10 +28,6 @@ class Solution:
 
 class Solution:
     def minNumberOfSeconds(self, m: int, w: List[int]) -> int:
-        return bisect_left(range(m*m*min(w)),m,key=lambda i:sum((sqrt(8*i/x+1)-1)//2for x in w))
-
-class Solution:
-    def minNumberOfSeconds(self, m: int, w: List[int]) -> int:
         return bisect_left(range(m*m*min(w)),m,key=lambda i:sum(((8*i/x+1)**.5-1)//2for x in w))
 
 class Solution:
@@ -40,7 +36,7 @@ class Solution:
 
 class Solution:
     def minNumberOfSeconds(self, m: int, w: List[int]) -> int:
-        return bisect_left(range(9**17),m,key=lambda i:sum(((8*i/x+1)**.5-1)//2for x in w))
+        return bisect_left(range(9**17),m,key=lambda i:sum(isqrt(8*i//x+1)-1>>1for x in w))
 
 test('''
 3296. Minimum Number of Seconds to Make Mountain Height Zero

d1edf66039cccae88be1575eb00e501ca7d5e5b1
joric <joric@users.noreply.github.com>
2026-03-17 21:13:44 +0500

    updated files

--- leetcode/3070.count-submatrices-with-top-left-element-and-sum-less-than-k.py
+++ leetcode/3070.count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -1,10 +1,25 @@
 from lc import *
 
+# https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k
 # Q2 https://leetcode.com/contest/weekly-contest-387/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/
 
 class Solution:
     def countSubmatrices(self, g: List[List[int]], k: int) -> int:
         f=cache(lambda i,j:i>-1<j and g[i][j]+f(i-1,j)+f(i,j-1)-f(i-1,j-1));return sum(f(i,j)<=k for i,r in enumerate(g)for j in range(len(r)))
+
+# POTD 2026-03-17
+
+class Solution:
+    def countSubmatrices(self, g: List[List[int]], k: int) -> int:
+        f=cache(lambda i,j:i>-1<j and g[i][j]+f(i-1,j)+f(i,j-1)-f(i-1,j-1));return sum(k>=f(i,j)for i,r in enumerate(g)for j in range(len(r)))
+
+class Solution:
+    def countSubmatrices(self, g: List[List[int]], k: int) -> int:
+        e=enumerate;[setitem(r,j,r[j]+g[i-1][j]*(i>0)+r[j-1]*(j>0)-g[i-1][j-1]*(i>0<j))for i,r in e(g)for j,v in e(r)];return sum(v<=k for r in g for v in r)
+
+class Solution:
+    def countSubmatrices(self, g: List[List[int]], k: int) -> int:
+        c=__import__('numpy').cumsum;return int((c(c(g,0),1)<=k).sum())
 
 test('''
 3070. Count Submatrices with Top-Left Element and Sum Less Than k

39bbf646cca51e891314c6f15769633dbe7d755f
joric <joric@users.noreply.github.com>
2026-05-07 20:19:35 +0500

    updated files

--- leetcode/509.fibonacci-number.py
+++ leetcode/509.fibonacci-number.py
@@ -77,8 +77,7 @@ class Solution:
     def fib(self, n: int) -> int:
         return pow(x:=2<<n,n+1,x*x+~x)%x
 
-# revisited 2026-05-07, shorter by 1 character
-# (a*x+b)%x = b => (a*x+b)//x = a
+# considering (a*X + b) % X = b => (a*X + b) // X = a
 
 class Solution:
     def fib(self, n: int) -> int:

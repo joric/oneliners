12ea1687172cc9529808fb1923b5eee8ca32301a
joric <joric@users.noreply.github.com>
2026-05-09 21:28:08 +0500

    updated files

---
+++ leetcode/2770.maximum-number-of-jumps-to-reach-the-last-index.py
@@ -0,0 +1,119 @@
+from lc import *
+
+# https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/solutions/6024785/python-4-line-dp-beats-90-runtime-by-dsa-tel1/?envType=daily-question&envId=2026-05-10
+
+class Solution:
+    def maximumJumps(self, nums: List[int], target: int) -> int:
+        dp = [0] + [-inf] * (len(nums) - 1)
+        for i in range(1, len(nums)):
+            dp[i] = max((1 + dp[j] for j in range(i) if abs(nums[i] - nums[j]) <= target), default=dp[i])
+        return max(-1, dp[-1])
+
+# https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/solutions/3744368/python-easy-10-lines-code-by-s_idd10-7h45/?envType=daily-question&envId=2026-05-10
+
+class Solution:
+    def maximumJumps(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        @cache
+        def dfs(idx):
+            if idx == n - 1:
+                return 0
+            ans = -inf
+            for i in range(idx + 1, n):
+                if -target <= nums[idx] - nums[i] <= target:
+                    ans = max(ans, 1 + dfs(i))
+            return ans
+        ans = dfs(0)
+        return ans if ans != -inf else -1
+
+class Solution:
+    def maximumJumps(self, a: List[int], t: int) -> int:
+        return max(-1,(f:=cache(lambda i,n=len(a):~i+n and max([1+f(j)for j in range(i+1,n)if-t<=a[i]-a[j]<=t]+[-inf])))(0))
+
+test('''
+2770. Maximum Number of Jumps to Reach the Last Index
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given a 0-indexed array nums of n integers and an integer target.
+
+You are initially positioned at index 0. In one step, you can jump from index i to any index j such that:
+
+0 <= i < j < n
+-target <= nums[j] - nums[i] <= target
+Return the maximum number of jumps you can make to reach index n - 1.
+
+If there is no way to reach index n - 1, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,6,4,1,2], target = 2
+Output: 3
+Explanation: To go from index 0 to index n - 1 with the maximum number of jumps, you can perform the following jumping sequence:
+- Jump from index 0 to index 1. 
+- Jump from index 1 to index 3.
+- Jump from index 3 to index 5.
+It can be proven that there is no other jumping sequence that goes from 0 to n - 1 with more than 3 jumps. Hence, the answer is 3. 
+Example 2:
+
+Input: nums = [1,3,6,4,1,2], target = 3
+Output: 5
+Explanation: To go from index 0 to index n - 1 with the maximum number of jumps, you can perform the following jumping sequence:
+- Jump from index 0 to index 1.
+- Jump from index 1 to index 2.
+- Jump from index 2 to index 3.
+- Jump from index 3 to index 4.
+- Jump from index 4 to index 5.
+It can be proven that there is no other jumping sequence that goes from 0 to n - 1 with more than 5 jumps. Hence, the answer is 5. 
+Example 3:
+
+Input: nums = [1,3,6,4,1,2], target = 0
+Output: -1
+Explanation: It can be proven that there is no jumping sequence that goes from 0 to n - 1. Hence, the answer is -1. 
+ 
+
+Constraints:
+
+2 <= nums.length == n <= 1000
+-109 <= nums[i] <= 109
+0 <= target <= 2 * 109
+ 
+Seen this question in a real interview before?
+1/6
+Yes
+No
+Accepted
+35,444/107.9K
+Acceptance Rate
+32.8%
+Topics
+Senior
+Array
+Dynamic Programming
+Weekly Contest 353
+icon
+Companies
+Hint 1
+Use a dynamic programming approach.
+Hint 2
+Define a dynamic programming array dp of size n, where dp[i] represents the maximum number of jumps from index 0 to index i.
+Hint 3
+For each j iterate over all i < j. Set dp[j] = max(dp[j], dp[i] + 1) if -target <= nums[j] - nums[i] <= target.
+Similar Questions
+Jump Game II
+Medium
+Frog Jump
+Hard
+Jump Game III
+Medium
+Jump Game IV
+Hard
+Minimum Jumps to Reach Home
+Medium
+Jump Game VII
+Medium
+''')

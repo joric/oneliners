ca387c3e19ed0cf9912380e72e4c322f6d9b01e4
joric <joric@users.noreply.github.com>
2026-05-30 15:00:34 +0500

    updated files

--- leetcode/3161.block-placement-queries.py
+++ leetcode/3161.block-placement-queries.py
@@ -1,5 +1,64 @@
 from lc import *
 
+# https://leetcode.com/problems/block-placement-queries/solutions/8290843/block-placement-queries-by-leetcode-4gh0/?envType=daily-question&envId=2026-05-30
+
+# Segment Tree
+
+class Solution:
+    def getResults(self, queries: List[List[int]]) -> List[bool]:
+        mx = 50000
+
+        st = SortedList([0, mx])
+        for q in queries:
+            if q[0] == 1:
+                st.add(q[1])
+
+        bt = [0] * (mx + 1)
+
+        def update(x: int, v: int) -> None:
+            while x < len(bt):
+                if v > bt[x]:
+                    bt[x] = v
+                x += x & -x
+
+        def query(x: int) -> int:
+            res = 0
+            while x > 0:
+                if bt[x] > res:
+                    res = bt[x]
+                x -= x & -x
+            return res
+
+        pre = 0
+        for x in st:
+            if x == 0:
+                continue
+            update(x, x - pre)
+            pre = x
+
+        ans = []
+        for q in reversed(queries):
+            if q[0] == 2:
+                x, sz = q[1], q[2]
+                idx = st.bisect_left(x)
+                if idx < len(st) and st[idx] == x:
+                    pre_val = x
+                else:
+                    pre_val = st[idx - 1]
+                max_space = query(pre_val)
+                max_space = max(max_space, x - pre_val)
+                ans.append(max_space >= sz)
+            else:
+                x = q[1]
+                idx = st.bisect_left(x)
+                pre_val = st[idx - 1]
+                nxt = st[idx + 1]
+                update(nxt, nxt - pre_val)
+                st.discard(x)
+
+        return ans[::-1]
+
+
 # https://leetcode.com/problems/block-placement-queries/solutions/5295649/block-placement-queries-python3-sol-by-t-90us/?envType=daily-question&envId=2026-05-30
 
 class Solution:

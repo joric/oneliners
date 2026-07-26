8b67e962deaa2307d8cd9e684154d6f0f4a9726d
joric <joric@users.noreply.github.com>
2026-07-26 23:27:58 +0500

    updated files

--- leetcode/1464.maximum-product-of-two-elements-in-an-array.py
+++ leetcode/1464.maximum-product-of-two-elements-in-an-array.py
@@ -25,6 +25,10 @@ class Solution:
     def maxProduct(self, a: List[int]) -> int:
         a.sort();return(a[-1]-1)*(a[-2]-1)
 
+class Solution:
+    def maxProduct(self, a: List[int]) -> int:
+        a.sort();return~-a[-1]*~-a[-2]
+
 test('''
 1464. Maximum Product of Two Elements in an Array
 Easy

5be8ab23f7741a8cace0c941a11990fd20156e0d
joric <joric@users.noreply.github.com>
2026-08-31 03:17:47 +0500

    updated files

--- leetcode/2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ leetcode/2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -69,9 +69,7 @@ class Solution:
     def nodesBetweenCriticalPoints(self, h: Optional[ListNode]) -> List[int]:
         import numpy as n;f=n.diff;e=f(eval(h.serialize(h)));return[min(d:=f(*n.where(e[1:]*e[:-1]<0)).tolist()or[-1]),sum(d)]
 
-class Solution:
-    def nodesBetweenCriticalPoints(self, h: Optional[ListNode]) -> List[int]:
-        p=q=0;c=-2;return min(d:=[c-(c:=0)for b in eval(h.serialize(h))if(c:=c+1)*p*(p:=q-(q:=b))<0][1:]or[-1]),sum(d)
+# POTD 2026-09-01
 
 class Solution:
     def nodesBetweenCriticalPoints(self, h: Optional[ListNode]) -> List[int]:

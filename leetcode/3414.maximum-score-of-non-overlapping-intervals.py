e1a997c17962648c1658eaef6e258c61d6dc468c
joric <joric@users.noreply.github.com>
2026-09-12 11:29:56 +0500

    updated files

--- leetcode/3414.maximum-score-of-non-overlapping-intervals.py
+++ leetcode/3414.maximum-score-of-non-overlapping-intervals.py
@@ -16,7 +16,7 @@ class Solution:
 
 class Solution:
     def maximumWeight(self, a: List[List[int]]) -> List[int]:
-        f=cache(lambda i,k:i*k>0and min(f(i-1,k),((t:=f(bisect_left(d:=sorted([e,s,w,i]for i,(s,e,w)in enumerate(a)),[(x:=d[i-1])[1]]),k-1))[0]-x[2],sorted([x[3]]+t[1])))or(0,[]));return f(len(a),4)[1]
+        d=sorted([e,s,w,i]for i,(s,e,w)in enumerate(a));f=cache(lambda i,k:i*k>0and min(f(i-1,k),((t:=f(bisect_left(d,[(x:=d[i-1])[1]]),k-1))[0]-x[2],sorted([x[3]]+t[1])))or(0,[]));return f(len(a),4)[1]
 
 class Solution:
     def maximumWeight(self, a: List[List[int]]) -> List[int]:

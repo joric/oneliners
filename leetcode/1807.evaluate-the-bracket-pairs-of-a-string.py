d66b39681792e1d028c2ea2d7d62415317fe9e91
joric <joric@users.noreply.github.com>
2026-09-26 01:52:32 +0500

    updated files

--- leetcode/1807.evaluate-the-bracket-pairs-of-a-string.py
+++ leetcode/1807.evaluate-the-bracket-pairs-of-a-string.py
@@ -18,6 +18,10 @@ class Solution:
     def evaluate(self, s: str, d: List[List[str]]) -> str:
         return re.sub(r'\((\w+)\)',lambda m,d=dict(d):d.get(m[1],'?'),s)
 
+class Solution:
+    def evaluate(self, s: str, d: List[List[str]]) -> str:
+        d=dict(d);return re.sub('\((\w+)\)',lambda m:d.get(m[1],'?'),s)
+
 class Solution: # TLE
     def evaluate(self, s: str, d: List[List[str]]) -> str:
         return re.sub('\((\w+)\)',lambda m:dict(d).get(m[1],'?'),s)
